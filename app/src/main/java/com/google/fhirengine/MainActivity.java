a18f4effd011e3e9a54d4357137a45977a418c6d
jingtang10 <jing.t86@gmail.com>
2020-03-21 02:20:35 +0000

    Add sample code to create FhirEngine in the reference app.

--- app/src/main/java/com/google/fhirengine/MainActivity.java
+++ app/src/main/java/com/google/fhirengine/MainActivity.java
@@ -5,9 +5,13 @@ import android.os.Bundle;
 
 public class MainActivity extends Activity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_main);
+
+    // Gets FHIR Engine using Dagger component.
+    FhirEngine fhirEngine = DaggerFhirEngineComponent.builder()
+        .context(this).build().getFhirEngine();
+  }
 }
