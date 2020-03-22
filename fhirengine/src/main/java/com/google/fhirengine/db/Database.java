73f554a8e9d116ae21dac2ae0a04313f196ef983
jingtang10 <jing.t86@gmail.com>
2020-03-22 23:03:35 +0000

    Implement insert and select methods in the database.
    Bump up junit version from 4.12 to 4.13 to use assertThrows.

--- fhirengine/src/main/java/com/google/fhirengine/db/Database.java
+++ fhirengine/src/main/java/com/google/fhirengine/db/Database.java
@@ -7,28 +7,29 @@ public interface Database {
   /**
    * Inserts the {@code resource} into the FHIR resource database.
    *
-   * @param <M> The resource type which should be a subtype of {@link Message}.
+   * @param <M> The resource type
    */
-  <M extends Message> void insert(M resource);
+  <M extends Message> void insert(M resource) throws ResourceAlreadyExistsException;
 
   /**
    * Updates the {@code resource}.
    *
-   * @param <M> The resource type which should be a subtype of {@link Message}.
+   * @param <M> The resource type
    */
   <M extends Message> void update(M resource);
 
   /**
    * Selects the FHIR resource of type {@code clazz} with {@code id}.
    *
-   * @param <M> The resource type which should be a subtype of {@link Message}.
+   * @param <M> The resource type
+   * @throws ResourceNotFoundException if the resource is not found
    */
-  <M extends Message> M select(Class<M> clazz, String id);
+  <M extends Message> M select(Class<M> clazz, String id) throws ResourceNotFoundException;
 
   /**
    * Deletes the FHIR resource of type {@code clazz} with {@code id}.
    *
-   * @param <M> The resource type which should be a subtype of {@link Message}.
+   * @param <M> The resource type
    */
   <M extends Message> void delete(Class<M> clazz, String id);
 }
---
+++ fhirengine/src/test/java/com/google/fhirengine/db/impl/DatabaseImplTest.java
@@ -0,0 +1,75 @@
+package com.google.fhirengine.db.impl;
+
+import android.os.Build;
+
+import androidx.test.core.app.ApplicationProvider;
+
+import com.google.fhir.r4.core.AdministrativeGenderCode;
+import com.google.fhir.r4.core.Id;
+import com.google.fhir.r4.core.Patient;
+import com.google.fhirengine.db.ResourceAlreadyExistsException;
+import com.google.fhirengine.db.ResourceNotFoundException;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.RobolectricTestRunner;
+import org.robolectric.annotation.Config;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThrows;
+
+/** Unit tests for {@link DatabaseImpl}. */
+@RunWith(RobolectricTestRunner.class)
+@Config(sdk = Build.VERSION_CODES.P)
+public class DatabaseImplTest {
+  private static final String TEST_PATIENT_1_ID = "test_patient_1";
+  private static final Patient TEST_PATIENT_1 = Patient.newBuilder()
+      .setId(Id.newBuilder().setValue(TEST_PATIENT_1_ID))
+      .setGender(Patient.GenderCode.newBuilder().setValue(AdministrativeGenderCode.Value.MALE))
+      .build();
+  private static final String TEST_PATIENT_2_ID = "test_patient_2";
+  private static final Patient TEST_PATIENT_2 = Patient.newBuilder()
+      .setId(Id.newBuilder().setValue(TEST_PATIENT_2_ID))
+      .setGender(Patient.GenderCode.newBuilder().setValue(AdministrativeGenderCode.Value.MALE))
+      .build();
+
+  private DatabaseImpl database;
+
+  @Before
+  public void setUp() throws Exception {
+    database = new DatabaseImpl(ApplicationProvider.getApplicationContext());
+    database.insert(TEST_PATIENT_1);
+  }
+
+  @Test
+  public void insert_shouldInsertResource() throws Exception {
+    database.insert(TEST_PATIENT_2);
+    assertEquals(TEST_PATIENT_2, database.select(Patient.class, TEST_PATIENT_2_ID));
+  }
+
+  @Test
+  public void insert_existingResource_shouldThrowResourceAlreadyExistsException() throws Exception {
+    ResourceAlreadyExistsException resourceAlreadyExistsException =
+        assertThrows(ResourceAlreadyExistsException.class, () -> database.insert(TEST_PATIENT_1));
+    assertEquals(
+        "Resource with type " + Patient.class.getName() + " and id " + TEST_PATIENT_1_ID +
+            " already exists!",
+        resourceAlreadyExistsException.getMessage());
+  }
+
+  @Test
+  public void select_nonexistentResource_shouldThrowResourceNotFondException() throws Exception {
+    ResourceNotFoundException resourceNotFoundException =
+        assertThrows(ResourceNotFoundException.class, () ->
+            database.select(Patient.class, "nonexistent_patient"));
+    assertEquals(
+        "Resource not found with type com.google.fhir.r4.core.Patient and id nonexistent_patient!",
+        resourceNotFoundException.getMessage());
+  }
+
+  @Test
+  public void select_shouldReturnResource() throws Exception {
+    assertEquals(TEST_PATIENT_1, database.select(Patient.class, TEST_PATIENT_1_ID));
+  }
+}
