11aed58de1c4234e8dc55be9cf1ec486ac9cf1f8
jingtang10 <jing.t86@gmail.com>
2020-04-16 09:40:23 +0100

    Create a StringIndex class that represents an index recrod for a resource.

---
+++ fhirengine/src/main/java/com/google/fhirengine/index/StringIndex.java
@@ -0,0 +1,20 @@
+package com.google.fhirengine.index;
+
+import com.google.auto.value.AutoValue;
+
+/** An string index for a specific resource. */
+@AutoValue
+public abstract class StringIndex {
+  public static StringIndex create(String name, String path, String value) {
+    return new AutoValue_StringIndex(name, path, value);
+  }
+
+  /** The name of the string index, e.g. given. */
+  public abstract String name();
+
+  /** The path of the string index, e.g. Patient.name.given. */
+  public abstract String path();
+
+  /** The value of the string index, e.g. Tom. */
+  public abstract String value();
+}
