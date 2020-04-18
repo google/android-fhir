12e2b1ebf975ada8887e54df176d201efb412179
jingtang10 <jing.t86@gmail.com>
2020-04-18 22:32:16 +0100

    Fix build flies and test

--- fhirengine/src/main/java/com/google/fhirengine/cql/FhirEngineLibraryLoader.java
+++ fhirengine/src/main/java/com/google/fhirengine/cql/FhirEngineLibraryLoader.java
@@ -5,17 +5,13 @@ import com.google.fhirengine.db.ResourceNotFoundInDbException;
 
 import org.cqframework.cql.elm.execution.Library;
 import org.cqframework.cql.elm.execution.VersionedIdentifier;
-import org.opencds.cqf.cql.elm.execution.ObjectFactoryEx;
 import org.opencds.cqf.cql.execution.JsonCqlLibraryReader;
 import org.opencds.cqf.cql.execution.LibraryLoader;
 
-import java.io.ByteArrayInputStream;
 import java.io.IOException;
-import java.io.InputStream;
 import java.io.StringReader;
 
 import javax.inject.Inject;
-import javax.xml.namespace.QName;
 
 /**
  * FHIR Engine's implementation of {@link LibraryLoader} that loads a CQL/ELM library for the {@link
