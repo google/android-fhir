413f7b45ace873f0b45f38759e2d4eca3d49506f
jingtang10 <jing.t86@gmail.com>
2020-04-20 12:38:36 +0100

    Correct the CodeIndex's constructor

--- fhirengine/src/main/java/com/google/fhirengine/index/CodeIndex.java
+++ fhirengine/src/main/java/com/google/fhirengine/index/CodeIndex.java
@@ -5,8 +5,8 @@ import com.google.auto.value.AutoValue;
 /** An code index for a specific resource. */
 @AutoValue
 public abstract class CodeIndex {
-  public static CodeIndex create(String name, String path, String value) {
-    return new AutoValue_CodeIndex(name, path, value);
+  public static CodeIndex create(String name, String path, String system, String code) {
+    return new AutoValue_CodeIndex(name, path, system, code);
   }
 
   /** The name of the code index, e.g. "code". */
@@ -15,9 +15,9 @@ public abstract class CodeIndex {
   /** The path of the code index, e.g. "Observation.code". */
   public abstract String path();
 
-  /** The code of the code index, e.g. "1427AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA". */
-  public abstract String value();
-
   /** The system of the code index, e.g. "http://openmrs.org/concepts". */
   public abstract String system();
+
+  /** The code of the code index, e.g. "1427AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA". */
+  public abstract String code();
 }
