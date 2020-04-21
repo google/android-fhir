1633968a4523607a5f45dd3a256130ba6ef0f08a
jingtang10 <jing.t86@gmail.com>
2020-04-21 11:56:20 +0100

    Add searchByString and searchByCode database methods. These will need to be reworked at some point.

--- fhirengine/src/main/java/com/google/fhirengine/db/Database.java
+++ fhirengine/src/main/java/com/google/fhirengine/db/Database.java
@@ -47,4 +47,24 @@ public interface Database {
    * particular patient.
    */
   <R extends Resource> List<R> searchByReference(Class<R> clazz, String reference, String value);
+
+  /**
+   * Returns a {@link List} of {@link Resource}s that are of type {@code clazz} and have {@code
+   * string} with {@code value}.
+   * <p>
+   * For example, a search for {@link org.hl7.fhir.r4.model.Patient}s with {@code string} 'given'
+   * and {@code value} 'Tom' will return all patients with a given name Tom.
+   */
+  <R extends Resource> List<R> searchByString(Class<R> clazz, String string, String value);
+
+  /**
+   * Returns a {@link List} of {@link Resource}s that are of type {@code clazz} and have {@code
+   * code} with {@code system} and {@code value}.
+   * <p>
+   * For example, a search for {@link org.hl7.fhir.r4.model.Observation}s with {@code code} 'code',
+   * {@code system} 'http://openmrs.org/concepts' and {@code value} '165395AAAAAAAAAAAAAAAAAAAAAAAAAAAAAA'
+   * will return all observations with that code.
+   */
+  <R extends Resource> List<R> searchByCode(Class<R> clazz, String code, String system,
+      String value);
 }
