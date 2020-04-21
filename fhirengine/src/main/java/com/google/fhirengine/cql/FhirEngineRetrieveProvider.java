b182d37b66b57ad7b6e2f3c002d0bfbc324af901
jingtang10 <jing.t86@gmail.com>
2020-04-21 12:09:46 +0100

    Retrieve resources by code. NOTE: this is an incredibly hacky solution, a database join will need to be performed to use reference as well as code.

--- fhirengine/src/main/java/com/google/fhirengine/cql/FhirEngineRetrieveProvider.java
+++ fhirengine/src/main/java/com/google/fhirengine/cql/FhirEngineRetrieveProvider.java
@@ -23,6 +23,10 @@ import com.google.fhirengine.resource.ResourceUtils;
 import org.opencds.cqf.cql.runtime.Code;
 import org.opencds.cqf.cql.runtime.Interval;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
 import javax.inject.Inject;
 
 /**
@@ -45,8 +49,25 @@ public class FhirEngineRetrieveProvider implements org.opencds.cqf.cql.retrieve.
   public Iterable<Object> retrieve(String context, String contextPath, Object contextValue,
       String dataType, String templateId, String codePath, Iterable<Code> codes, String valueSet,
       String datePath, String dateLowPath, String dateHighPath, Interval dateRange) {
-    return ImmutableList.copyOf(database
-        .searchByReference(ResourceUtils.getResourceClass(dataType), dataType + "." + contextPath,
-            TextUtils.isEmpty((String) contextValue) ? null : context + "/" + contextValue));
+    Iterator<Code> codesit = codes.iterator();
+    List<Code> codeList = new ArrayList<>();
+    while (codesit.hasNext()) {
+      codeList.add(codesit.next());
+    }
+    if (codeList.size() == 0) {
+      ImmutableList<Object> result = ImmutableList.copyOf(database
+          .searchByReference(ResourceUtils.getResourceClass(dataType), dataType + "." + contextPath,
+              TextUtils.isEmpty((String) contextValue) ? null : context + "/" + contextValue));
+      return result;
+    } else if (codeList.size() == 1) {
+      // TODO: create a database method that search by code as well as reference.
+      Code code = codeList.get(0);
+      ImmutableList<Object> result = ImmutableList.copyOf(database
+          .searchByCode(ResourceUtils.getResourceClass(dataType), dataType + "." + codePath,
+              code.getSystem(), code.getCode()));
+      return result;
+    } else {
+      return ImmutableList.of();
+    }
   }
 }
