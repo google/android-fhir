3f37cd1d02779562968022d46a8a0d2de4aea7a0
Yigit Boyar <yboyar@gmail.com>
2020-05-04 14:07:55 -0700

    move to room. Fixes #28 (#21)
    Use Room for persistence.
    
    This replaces database implementation from vanilla sqlite to Room so that we get compile time check for queries + cleaner to read.
    There is more cleanup opportunities and it'll be a followup CL when we replace auto-value w/ data classes.

---
+++ fhirengine/src/main/java/com/google/fhirengine/db/impl/StringIndexEntity.kt
@@ -0,0 +1,54 @@
+// Copyright 2020 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.google.fhirengine.db.impl
+
+import androidx.room.Entity
+import androidx.room.ForeignKey
+import androidx.room.Index
+import androidx.room.PrimaryKey
+import org.hl7.fhir.r4.model.ResourceType
+
+@Entity(
+        indices = [
+            Index(
+                    value = ["resourceType", "indexName", "indexValue"]
+            ),
+            Index(
+                    // keep this index for faster foreign lookup
+                    value = ["resourceId", "resourceType"]
+            )
+        ],
+        foreignKeys = [
+            ForeignKey(
+                    entity = ResourceEntity::class,
+                    parentColumns = ["resourceId", "resourceType"],
+                    childColumns = ["resourceId", "resourceType"],
+                    onDelete = ForeignKey.CASCADE,
+                    onUpdate = ForeignKey.NO_ACTION,
+                    deferred = true
+            )
+        ]
+)
+// TODO merge index entities if it makes sense
+//  https://github.com/jingtang10/fhir-engine/issues/33
+internal data class StringIndexEntity(
+        @PrimaryKey(autoGenerate = true)
+        val id: Long,
+        val resourceType: ResourceType,
+        val indexName: String,
+        val indexPath: String,
+        val indexValue: String,
+        val resourceId: String
+)
