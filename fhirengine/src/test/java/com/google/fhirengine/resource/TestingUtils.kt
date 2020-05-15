fe3fcca995adae4303dc7d9ef6a69f88ad0e6076
Yigit Boyar <yboyar@google.com>
2020-05-14 17:18:50 -0700

    fix style

--- fhirengine/src/test/java/com/google/fhirengine/resource/TestingUtils.kt
+++ fhirengine/src/test/java/com/google/fhirengine/resource/TestingUtils.kt
@@ -5,7 +5,6 @@ import org.hl7.fhir.r4.model.Resource
 import org.junit.Assert.assertEquals
 import javax.inject.Inject
 
-
 /** Utilities for testing.  */
 class TestingUtils @Inject constructor(private val iParser: IParser) {
 
@@ -13,5 +12,4 @@ class TestingUtils @Inject constructor(private val iParser: IParser) {
     fun assertResourceEquals(expected: Resource?, actual: Resource?) {
         assertEquals(iParser.encodeResourceToString(expected), iParser.encodeResourceToString(actual))
     }
-
 }
