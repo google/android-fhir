e4fb153c75cf6bfcc0f53eeef7877e7a647189fe
jingtang10 <jing.t86@gmail.com>
2020-07-10 11:12:51 +0100

    Convert FhirIndexer class from java to kotlin

--- fhirengine/src/main/java/com/google/fhirengine/index/FhirIndexer.kt
+++ fhirengine/src/main/java/com/google/fhirengine/index/FhirIndexer.kt
@@ -14,20 +14,20 @@
  * limitations under the License.
  */
 
-package com.google.fhirengine.index;
+package com.google.fhirengine.index
 
-import org.hl7.fhir.r4.model.Resource;
+import org.hl7.fhir.r4.model.Resource
 
 /**
  * The interface that handles the indexing of FHIR resources.
  *
- * <p>Note: this interface does not handle the actual storage of the indices.
+ * Note: this interface does not handle the actual storage of the indices.
  */
-public interface FhirIndexer {
-  /**
-   * Returns the values to index for a FHIR {@code resource} in the local storage.
-   *
-   * @param <R> The resource type which should be a subtype of {@link Resource}.
-   */
-  <R extends Resource> ResourceIndices index(R resource);
+internal interface FhirIndexer {
+    /**
+     * Returns the values to index for a FHIR `resource` in the local storage.
+     *
+     * @param <R> The resource type which should be a subtype of [Resource].
+     */
+    fun <R : Resource> index(resource: R): ResourceIndices
 }
