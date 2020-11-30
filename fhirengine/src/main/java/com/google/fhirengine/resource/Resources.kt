b2651c4462839da9ff3f37ef9bb132939c29ed0e
jingtang10 <jing.t86@gmail.com>
2020-11-30 09:38:47 +0000

    Add internal to members of Resources.kt

--- fhirengine/src/main/java/com/google/fhirengine/resource/Resources.kt
+++ fhirengine/src/main/java/com/google/fhirengine/resource/Resources.kt
@@ -21,10 +21,10 @@ import org.hl7.fhir.r4.model.Resource
 import org.hl7.fhir.r4.model.ResourceType
 
 /** The HAPI Fhir package prefix for R4 resources. */
-val R4_RESOURCE_PACKAGE_PREFIX = "org.hl7.fhir.r4.model."
+internal val R4_RESOURCE_PACKAGE_PREFIX = "org.hl7.fhir.r4.model."
 
 /** Returns the FHIR resource type. */
-fun <R : Resource> getResourceType(clazz: Class<R>): ResourceType {
+internal fun <R : Resource> getResourceType(clazz: Class<R>): ResourceType {
     try {
         return clazz.getConstructor().newInstance().getResourceType()
     } catch (e: NoSuchMethodException) {
@@ -39,7 +39,7 @@ fun <R : Resource> getResourceType(clazz: Class<R>): ResourceType {
 }
 
 /** Returns the {@link Class} object for the resource type. */
-fun <R : Resource> getResourceClass(resourceType: String): Class<R> {
+internal fun <R : Resource> getResourceClass(resourceType: String): Class<R> {
     // Remove any curly brackets in the resource type string. This is to work around an issue with
     // JSON deserialization in the CQL engine on Android. The resource type string incorrectly
     // includes namespace prefix in curly brackets, e.g. "{http://hl7.org/fhir}Patient" instead of
