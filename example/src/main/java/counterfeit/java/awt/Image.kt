5a1a3260284f853c9ce3c50179eff8df175f81e0
jingtang10 <jing.t86@gmail.com>
2020-12-08 12:18:13 +0000

    Convert Image.java and Transferable.java to Kotlin
    These are classes used to be able to import cql engine

--- example/src/main/java/counterfeit/java/awt/Image.kt
+++ example/src/main/java/counterfeit/java/awt/Image.kt
@@ -14,6 +14,6 @@
  * limitations under the License.
  */
 
-package java.awt.datatransfer;
+package java.awt
 
-public interface Transferable {}
+class Image
--- example/src/main/java/counterfeit/java/awt/datatransfer/Transferable.kt
+++ example/src/main/java/counterfeit/java/awt/datatransfer/Transferable.kt
@@ -14,6 +14,6 @@
  * limitations under the License.
  */
 
-package java.awt;
+package java.awt.datatransfer
 
-public class Image {}
+interface Transferable
