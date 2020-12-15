1240b8fcba31eef26bcb38892a1bc6d4df190a64
jingtang10 <jing.t86@gmail.com>
2020-12-15 15:11:59 +0000

    Create WidgetTypeTest.kt

---
+++ datacapture/src/test/java/com/google/android/fhir/datacapture/WidgetTypeTest.kt
@@ -0,0 +1,26 @@
+package com.google.android.fhir.datacapture
+
+import android.os.Build
+import com.google.common.truth.Truth.assertThat
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.robolectric.RobolectricTestRunner
+import org.robolectric.annotation.Config
+
+/** Unit tests for {@link FhirIndexerImpl}. */
+@RunWith(RobolectricTestRunner::class)
+@Config(sdk = [Build.VERSION_CODES.P])
+class WidgetTypeTest {
+
+    @Test
+    fun numberOfSupportedTypes() {
+        assertThat(WidgetType.values().size).isEqualTo(4)
+    }
+
+    @Test
+    fun fromInt_shouldReturnTheWidgetType() {
+        WidgetType.values().forEach {
+            assertThat(WidgetType.fromInt(it.value)).isEqualTo(it)
+        }
+    }
+}
