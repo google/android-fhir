08c8a1e03c880c659a3055be66b49962959328ae
jingtang10 <jing.t86@gmail.com>
2020-12-17 11:35:20 +0000

    Re-add the data capture gallery module after it's been removed from the branch in order to split PR #97

---
+++ datacapturegallery/src/main/java/com/google/android/fhir/datacapture/gallery/QuestionnaireResponseDialogFragment.kt
@@ -0,0 +1,43 @@
+/*
+ * Copyright 2020 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.google.android.fhir.datacapture.gallery
+
+import android.app.AlertDialog
+import android.app.Dialog
+import android.os.Bundle
+import android.widget.TextView
+import androidx.fragment.app.DialogFragment
+
+class QuestionnaireResponseDialogFragment(val contents: String) : DialogFragment() {
+    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
+        return activity?.let {
+            val view = requireActivity().layoutInflater.inflate(
+                R.layout.questionnaire_response_dialog_contents,
+                null
+            )
+            view.findViewById<TextView>(R.id.contents).text = contents
+
+            AlertDialog.Builder(it)
+                .setView(view)
+                .create()
+        } ?: throw IllegalStateException("Activity cannot be null")
+    }
+
+    companion object {
+        const val TAG = "questionnaire-response-dialog-fragment"
+    }
+}
