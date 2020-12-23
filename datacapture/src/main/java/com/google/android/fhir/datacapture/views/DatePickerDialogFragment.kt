87532c04753920888dc05e5c7ee9af9ca7e26547
jingtang10 <jing.t86@gmail.com>
2020-12-23 18:30:30 +0000

    Use Java8 java.time APIs

--- datacapture/src/main/java/com/google/android/fhir/datacapture/views/DatePickerDialogFragment.kt
+++ datacapture/src/main/java/com/google/android/fhir/datacapture/views/DatePickerDialogFragment.kt
@@ -16,6 +16,7 @@
 
 package com.google.android.fhir.datacapture.views
 
+import android.annotation.SuppressLint
 import android.app.DatePickerDialog
 import android.app.Dialog
 import android.os.Bundle
@@ -23,18 +24,22 @@ import android.widget.DatePicker
 import androidx.core.os.bundleOf
 import androidx.fragment.app.DialogFragment
 import androidx.fragment.app.setFragmentResult
-import java.util.Calendar
+import java.time.LocalDate
 
 class DatePickerFragment : DialogFragment(), DatePickerDialog.OnDateSetListener {
+    @SuppressLint("NewApi") // Suppress warnings for java.time APIs
     override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
         // Use the current date as the default date in the picker
-        val c = Calendar.getInstance()
-        val year = c.get(Calendar.YEAR)
-        val month = c.get(Calendar.MONTH)
-        val day = c.get(Calendar.DAY_OF_MONTH)
+        val today = LocalDate.now()
 
         // Create a new instance of DatePickerDialog and return it
-        return DatePickerDialog(requireContext(), this, year, month, day)
+        return DatePickerDialog(
+            requireContext(),
+            this,
+            today.year,
+            // month values are 1-12 in java.time but DatePickerDialog expects 0-11
+            today.monthValue - 1,
+            today.dayOfMonth)
     }
 
     override fun onDateSet(view: DatePicker?, year: Int, month: Int, dayOfMonth: Int) {
