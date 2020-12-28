e9a619ee7cb18cb0715c21cf17eaa0f8ced60d4a
jingtang10 <jing.t86@gmail.com>
2020-12-28 18:07:51 +0000

    Read date answer for date picker view

--- datacapture/src/main/java/com/google/android/fhir/datacapture/views/QuestionnaireItemDatePickerViewHolderFactory.kt
+++ datacapture/src/main/java/com/google/android/fhir/datacapture/views/QuestionnaireItemDatePickerViewHolderFactory.kt
@@ -16,7 +16,7 @@
 
 package com.google.android.fhir.datacapture.views
 
-import android.os.Build
+import android.annotation.SuppressLint
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -25,7 +25,8 @@ import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.FragmentResultListener
 import com.google.android.fhir.datacapture.R
-import java.util.Calendar
+import java.time.LocalDate
+import java.time.format.DateTimeFormatter
 import org.hl7.fhir.r4.model.DateType
 import org.hl7.fhir.r4.model.QuestionnaireResponse
 
@@ -45,28 +46,27 @@ private class QuestionnaireItemDatePickerViewHolder(
     private val button = itemView.findViewById<TextView>(R.id.button)
     init {
         button.setOnClickListener {
-            // TODO: find a more robust way to do this.
+            // TODO: find a more robust way to do this as it is not guaranteed that the activity is
+            // an AppCompatActivity.
             val context = itemView.context as AppCompatActivity
             DatePickerFragment().show(context.supportFragmentManager, DatePickerFragment.TAG)
             context.supportFragmentManager.setFragmentResultListener(
                 DatePickerFragment.RESULT_REQUEST_KEY,
                 context,
                 object : FragmentResultListener {
+                    @SuppressLint("NewApi") // java.time APIs can be used due to desugaring
                     override fun onFragmentResult(requestKey: String, result: Bundle) {
                         val year = result.getInt(DatePickerFragment.RESULT_BUNDLE_KEY_YEAR)
                         val month = result.getInt(DatePickerFragment.RESULT_BUNDLE_KEY_MONTH)
                         val dayOfMonth =
-                            result.getInt(DatePickerFragment.RESULT_BUNDLE_KEY_DAY_OF_MONTH)
-                        val date = Calendar.getInstance().apply {
-                            set(year, month, dayOfMonth)
-                        }.time
+                          result.getInt(DatePickerFragment.RESULT_BUNDLE_KEY_DAY_OF_MONTH)
+                        input.text = LocalDate.of(
+                          year,
+                          // month values are 1-12 in java.time but 0-11 in DateType (FHIR)
+                          month + 1,
+                          dayOfMonth
+                        ).format(LOCAL_DATE_FORMATTER)
 
-                        // Prefer the ICU library on Android N or above.
-                        input.text = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
-                            android.icu.text.DateFormat.getDateInstance().format(date)
-                        } else {
-                            java.text.DateFormat.getDateInstance().format(date)
-                        }
                         questionnaireItemViewItem.questionnaireResponseItemComponent.answer =
                             listOf(
                                 QuestionnaireResponse.QuestionnaireResponseItemAnswerComponent()
@@ -82,8 +82,28 @@ private class QuestionnaireItemDatePickerViewHolder(
 
     private lateinit var questionnaireItemViewItem: QuestionnaireItemViewItem
 
+    @SuppressLint("NewApi") // java.time APIs can be used due to desugaring
     override fun bind(questionnaireItemViewItem: QuestionnaireItemViewItem) {
         this.questionnaireItemViewItem = questionnaireItemViewItem
         textView.text = questionnaireItemViewItem.questionnaireItemComponent.text
+        questionnaireItemViewItem.questionnaireResponseItemComponent.answer.also {
+            if (it.size == 1 && it[0].hasValueDateType()) {
+                input.text = it[0].valueDateType.let { date ->
+                    LocalDate.of(
+                      date.year,
+                      // month values are 1-12 in java.time but 0-11 in DateType (FHIR)
+                      date.month + 1,
+                      date.day
+                    )
+                }.format(LOCAL_DATE_FORMATTER)
+            } else {
+                input.text = ""
+            }
+        }
+    }
+
+    private companion object {
+        @SuppressLint("NewApi") // java.time APIs can be used due to desugaring
+        val LOCAL_DATE_FORMATTER = DateTimeFormatter.ISO_LOCAL_DATE
     }
 }
