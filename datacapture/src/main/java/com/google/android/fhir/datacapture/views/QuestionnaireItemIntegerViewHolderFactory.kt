6102ce1125b6229b45bfbcc9ab0812fe11276094
Kashyap Jois <kjois@iprdgroup.com>
2021-01-08 17:47:17 +0530

    Added QuestionnaireItemIntegerViewHolderFactory.kt to handle questionnaire item type of integer

---
+++ datacapture/src/main/java/com/google/android/fhir/datacapture/views/QuestionnaireItemIntegerViewHolderFactory.kt
@@ -0,0 +1,63 @@
+/*
+ * Copyright 2020 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.google.android.fhir.datacapture.views
+
+import android.text.Editable
+import android.view.View
+import android.widget.EditText
+import android.widget.TextView
+import androidx.core.widget.doAfterTextChanged
+import com.google.android.fhir.datacapture.R
+import org.hl7.fhir.r4.model.QuestionnaireResponse
+import org.hl7.fhir.r4.model.StringType
+
+object QuestionnaireItemIntegerViewHolderFactory : QuestionnaireItemViewHolderFactory(
+  R.layout.questionnaire_item_integer_view
+) {
+  override fun getQuestionnaireItemViewHolderDelegate() =
+    object : QuestionnaireItemViewHolderDelegate {
+      private lateinit var textView: TextView
+      private lateinit var editText: EditText
+      private lateinit var questionnaireItemViewItem: QuestionnaireItemViewItem
+
+      override fun init(itemView: View) {
+        textView = itemView.findViewById(R.id.text)
+        editText = itemView.findViewById(R.id.input)
+        itemView.findViewById<EditText>(R.id.input)
+          .doAfterTextChanged { editable: Editable? ->
+            questionnaireItemViewItem.singleAnswerOrNull =
+              QuestionnaireResponse.QuestionnaireResponseItemAnswerComponent()
+                .apply {
+                  value = StringType(editable.toString())
+                }
+          }
+      }
+
+      override fun bind(questionnaireItemViewItem: QuestionnaireItemViewItem) {
+        this.questionnaireItemViewItem = questionnaireItemViewItem
+        textView.text = questionnaireItemViewItem.questionnaireItemComponent.text
+        if(questionnaireItemViewItem.singleAnswerOrNull?.valueIntegerType?.value==null){
+            editText.setText("")
+        }else{
+            editText.setText(
+                    (questionnaireItemViewItem.singleAnswerOrNull!!.valueIntegerType.value)
+            )
+        }
+
+      }
+    }
+}
