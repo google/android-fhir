10724624b0549d8a555908378d888333d48890d4
Kashyap Jois <kjois@iprdgroup.com>
2021-01-20 17:59:26 +0530

    Removed submit button and replaced with a function to setFragmentresult with the current questionnaire Response

--- datacapture/src/main/java/com/google/android/fhir/datacapture/QuestionnaireFragment.kt
+++ datacapture/src/main/java/com/google/android/fhir/datacapture/QuestionnaireFragment.kt
@@ -20,7 +20,6 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
 import androidx.core.os.bundleOf
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.setFragmentResult
@@ -41,15 +40,16 @@ class QuestionnaireFragment : Fragment() {
         val recyclerView = view.findViewById<RecyclerView>(R.id.recycler_view)
         val adapter = QuestionnaireItemAdapter(viewModel.questionnaireItemViewItemList)
         recyclerView.adapter = adapter
+    }
 
-        view.findViewById<Button>(R.id.submit).setOnClickListener {
-            val serializedResponse = FhirContext.forR4().newJsonParser()
-                .encodeResourceToString(viewModel.questionnaireResponse)
-            setFragmentResult(
-                QUESTIONNAIRE_RESPONSE_REQUEST_KEY,
-                bundleOf(QUESTIONNAIRE_RESPONSE_BUNDLE_KEY to serializedResponse)
-            )
-        }
+    //Returns the current questionnaire response to parent
+    fun returnQuestionnaireResult(){
+        val serializedResponse = FhirContext.forR4().newJsonParser()
+          .encodeResourceToString(viewModel.questionnaireResponse)
+        setFragmentResult(
+          QUESTIONNAIRE_RESPONSE_REQUEST_KEY,
+          bundleOf(QUESTIONNAIRE_RESPONSE_BUNDLE_KEY to serializedResponse)
+        )
     }
 
     companion object {
