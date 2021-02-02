f4de866aa3bea30134978e1b46c90a560daef3de
Kashyap Jois <kjois@iprdgroup.com>
2021-02-02 17:35:42 +0530

    Added unit test for MoreAnswerOptions

---
+++ datacapture/src/test/java/com/google/android/fhir/datacapture/MoreAnswerOptionsTest.kt
@@ -0,0 +1,81 @@
+/*
+ * Copyright 2020 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.google.android.fhir.datacapture
+
+import android.os.Build
+import com.google.common.truth.Truth.assertThat
+import com.google.fhir.r4.core.Code
+import com.google.fhir.r4.core.Coding
+import com.google.fhir.r4.core.Questionnaire
+import com.google.fhir.r4.core.QuestionnaireResponse
+import com.google.fhir.r4.core.String
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.robolectric.RobolectricTestRunner
+import org.robolectric.annotation.Config
+
+@RunWith(RobolectricTestRunner::class)
+@Config(sdk = [Build.VERSION_CODES.P])
+class MoreAnswerOptionsTest {
+
+    @Test
+    fun getString_choiceItemType_answerOptionShouldReturnValueCoding() {
+        val answerOption = Questionnaire.Item.AnswerOption.newBuilder()
+            .setValue(
+                Questionnaire.Item.AnswerOption.ValueX.newBuilder()
+                    .setCoding(
+                        Coding.newBuilder()
+                            .setCode(
+                                Code.newBuilder()
+                                    .setValue("test-code")
+                            )
+                            .setDisplay(
+                                String.newBuilder().setValue("Test Code"))
+                    )
+
+            ).build()
+        assertThat(answerOption.getDisplayString()).isEqualTo("Test Code")
+    }
+
+    @Test
+    fun getResponseAnswerValueX_ShouldReturnAnswerValueCoding() {
+        val answerOption = Questionnaire.Item.AnswerOption.newBuilder()
+            .setValue(
+                Questionnaire.Item.AnswerOption.ValueX.newBuilder()
+                    .setCoding(
+                        Coding.newBuilder()
+                            .setCode(
+                                Code.newBuilder()
+                                    .setValue("test-code")
+                            )
+                            .setDisplay(
+                                String.newBuilder().setValue("Test Code"))
+                    )
+            ).build()
+        val answerValueX = QuestionnaireResponse.Item.Answer.ValueX.newBuilder()
+            .setCoding(
+                Coding.newBuilder()
+                    .setCode(
+                        Code.newBuilder()
+                            .setValue("test-code")
+                    )
+                    .setDisplay(
+                        String.newBuilder().setValue("Test Code"))
+            ).build()
+        assertThat(answerOption.getResponseAnswerValueX()).isEqualTo(answerValueX)
+    }
+}
