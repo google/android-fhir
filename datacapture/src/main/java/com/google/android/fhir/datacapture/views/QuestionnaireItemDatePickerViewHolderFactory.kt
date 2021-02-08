56de703d0c19f4f03f87627fae6614f83a4f6558
Varun Vaibhav Jha <jha.varun6@gmail.com>
2021-02-08 20:51:14 +0530

    fixed date widget bug

--- datacapture/src/main/java/com/google/android/fhir/datacapture/views/QuestionnaireItemDatePickerViewHolderFactory.kt
+++ datacapture/src/main/java/com/google/android/fhir/datacapture/views/QuestionnaireItemDatePickerViewHolderFactory.kt
@@ -102,6 +102,7 @@ object QuestionnaireItemDatePickerViewHolderFactory : QuestionnaireItemViewHolde
                         context.supportFragmentManager,
                         DatePickerFragment.TAG
                     )
+                    textInputLayout.clearFocus()
                 }
             }
 
