6c5d8511402c5695864af9d3363073a6dfd4af03
Kashyap Jois <kjois@iprdgroup.com>
2021-02-18 15:17:43 +0530

    Added another constructor to QuestionnaireListItem to store the path of the questionnaire Response

--- datacapturegallery/src/main/java/com/google/android/fhir/datacapture/gallery/QuestionnaireListItem.kt
+++ datacapturegallery/src/main/java/com/google/android/fhir/datacapture/gallery/QuestionnaireListItem.kt
@@ -19,20 +19,20 @@ package com.google.android.fhir.datacapture.gallery
 /**
  * @param name the name of the questionnaire to be displayed
  * @param description the description of the questionnaire to be displayed
- * @param questionnairePath the questionnaire's JSON filename in the assets directory
- * @param questionnaireResponsePath the corresponding questionnaireResponse's JSON filename in the assets directory (optional)
+ * @param pathQuestionnaire the questionnaire's JSON filename in the assets directory
+ * @param pathQuestionnaireResponse the corresponding questionnaireResponse's JSON filename in the assets directory (optional)
  */
 data class QuestionnaireListItem(
     val name: String,
     val description: String,
-    val questionnairePath: String,
-    val questionnaireResponsePath: String?
+    val pathQuestionnaire: String,
+    val pathQuestionnaireResponse: String?
 ) {
 
     constructor(
         name: String,
         description: String,
-        questionnairePath: String
+        pathQuestionnaire: String
     ) :
-        this(name, description, questionnairePath, null)
+        this(name, description, pathQuestionnaire, null)
 }
