9a433292b93f8ffde6e7fc85d6574abb9c7ff05f
Kashyap Jois <kjois@iprdgroup.com>
2021-02-18 15:17:00 +0530

    Added an optional String to the view model to load a questionnaire Response from a json file

--- datacapturegallery/src/main/java/com/google/android/fhir/datacapture/gallery/QuestionnaireViewModel.kt
+++ datacapturegallery/src/main/java/com/google/android/fhir/datacapture/gallery/QuestionnaireViewModel.kt
@@ -21,18 +21,26 @@ import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.SavedStateHandle
 
 class QuestionnaireViewModel(application: Application, private val state: SavedStateHandle) :
-  AndroidViewModel(application) {
-  var questionnaireJson: String? = null
-  val questionnaire: String
-    get() {
-      if (questionnaireJson == null) {
-        questionnaireJson =
-          getApplication<Application>()
-            .assets
-            .open(state[QuestionnaireActivity.QUESTIONNAIRE_FILE_PATH_KEY]!!)
-            .bufferedReader()
-            .use { it.readText() }
-      }
-      return questionnaireJson!!
-    }
+    AndroidViewModel(application) {
+    var questionnaireJson: String? = null
+    val questionnaire: String
+        get() {
+            if (questionnaireJson == null) {
+                questionnaireJson = getApplication<Application>().assets
+                    .open(state[QuestionnaireActivity.QUESTIONNAIRE_FILE_PATH_KEY]!!)
+                    .bufferedReader()
+                    .use { it.readText() }
+            }
+            return questionnaireJson!!
+        }
+    var questionnaireResponseJson: String? = null
+        get() {
+            if (state.contains(QuestionnaireActivity.QUESTIONNAIRE_RESPONSE_FILE_PATH_KEY)) {
+                questionnaireResponseJson = getApplication<Application>().assets
+                    .open(state[QuestionnaireActivity.QUESTIONNAIRE_RESPONSE_FILE_PATH_KEY]!!)
+                    .bufferedReader()
+                    .use { it.readText() }
+            }
+            return field
+        }
 }
