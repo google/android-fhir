84079c7b2c3d95d93a59f7400d24900f6a047180
Kashyap Jois <kjois@iprdgroup.com>
2021-03-08 12:40:47 +0530

    Added check to prevent overwriting of questionnaireResponse answer

--- datacapture/src/main/java/com/google/android/fhir/datacapture/views/QuestionnaireItemRadioGroupViewHolderFactory.kt
+++ datacapture/src/main/java/com/google/android/fhir/datacapture/views/QuestionnaireItemRadioGroupViewHolderFactory.kt
@@ -26,52 +26,63 @@ import com.google.android.fhir.datacapture.displayString
 import com.google.android.fhir.datacapture.responseAnswerValueX
 import com.google.fhir.r4.core.QuestionnaireResponse
 
-internal object QuestionnaireItemRadioGroupViewHolderFactory :
-  QuestionnaireItemViewHolderFactory(R.layout.questionnaire_item_radio_group_view) {
-  override fun getQuestionnaireItemViewHolderDelegate() =
-    object : QuestionnaireItemViewHolderDelegate {
-      private lateinit var radioHeader: TextView
-      private lateinit var radioGroup: RadioGroup
-      private lateinit var questionnaireItemViewItem: QuestionnaireItemViewItem
+internal object QuestionnaireItemRadioGroupViewHolderFactory : QuestionnaireItemViewHolderFactory(
+    R.layout.questionnaire_item_radio_group_view
+) {
+    override fun getQuestionnaireItemViewHolderDelegate() =
+        object : QuestionnaireItemViewHolderDelegate {
+            private lateinit var radioHeader: TextView
+            private lateinit var radioGroup: RadioGroup
+            private lateinit var questionnaireItemViewItem: QuestionnaireItemViewItem
 
-      override fun init(itemView: View) {
-        radioGroup = itemView.findViewById(R.id.radio_group)
-        radioHeader = itemView.findViewById(R.id.radio_header)
-      }
-
-      override fun bind(questionnaireItemViewItem: QuestionnaireItemViewItem) {
-        this.questionnaireItemViewItem = questionnaireItemViewItem
-        val questionnaireItem = questionnaireItemViewItem.questionnaireItem
-        val questionnaireResponseItemBuilder =
-          questionnaireItemViewItem.questionnaireResponseItemBuilder
-        val answer = questionnaireResponseItemBuilder.answerList.singleOrNull()?.value?.coding
-        radioHeader.text = questionnaireItem.text.value
-        radioGroup.removeAllViews()
-        var index = 0
-        questionnaireItem.answerOptionList.forEach {
-          radioGroup.addView(
-            RadioButton(radioGroup.context).apply {
-              id = index++ // Use the answer option index as radio button ID
-              text = it.displayString
-              layoutParams =
-                ViewGroup.LayoutParams(
-                  ViewGroup.LayoutParams.MATCH_PARENT,
-                  ViewGroup.LayoutParams.WRAP_CONTENT
-                )
-              this.isChecked = it.value.coding == answer
+            override fun init(itemView: View) {
+                radioGroup = itemView.findViewById(R.id.radio_group)
+                radioHeader = itemView.findViewById(R.id.radio_header)
             }
-          )
+
+            override fun bind(questionnaireItemViewItem: QuestionnaireItemViewItem) {
+                this.questionnaireItemViewItem = questionnaireItemViewItem
+                val questionnaireItem = questionnaireItemViewItem.questionnaireItem
+                val questionnaireResponseItemBuilder =
+                    questionnaireItemViewItem.questionnaireResponseItemBuilder
+                val answer =
+                    questionnaireResponseItemBuilder.answerList.singleOrNull()?.value?.coding
+                radioHeader.text = questionnaireItem.text.value
+                radioGroup.removeAllViews()
+                var index = 0
+                questionnaireItem.answerOptionList.forEach {
+                    radioGroup.addView(RadioButton(radioGroup.context).apply {
+                        id = index++ // Use the answer option index as radio button ID
+                        text = it.displayString
+                        layoutParams = ViewGroup.LayoutParams(
+                            ViewGroup.LayoutParams.MATCH_PARENT,
+                            ViewGroup.LayoutParams.WRAP_CONTENT
+                        )
+                        this.isChecked = it.value.coding == answer
+                    })
+                }
+                radioGroup.setOnCheckedChangeListener { _, checkedId ->
+                    // if-else block to prevent over-writing of "items" nested within "answer"
+                    if(questionnaireResponseItemBuilder.answerCount > 0 ){
+                        val tmpItems = questionnaireResponseItemBuilder.answerList.first().itemList
+                        QuestionnaireResponse.Item.Answer.newBuilder()
+                            .setValue(
+                                questionnaireItem
+                                    .answerOptionList[checkedId].responseAnswerValueX
+                            )
+                            .addAllItem(tmpItems)
+                    }else{
+                        questionnaireResponseItemBuilder.clearAnswer().addAnswer(
+                            QuestionnaireResponse.Item.Answer.newBuilder().apply {
+                                value = questionnaireItem
+                                    .answerOptionList[checkedId].responseAnswerValueX
+                            }
+                        )
+                    }
+
+                    questionnaireItemViewItem.questionnaireResponseItemChangedCallback()
+                }
+
+            }
         }
-        radioGroup.setOnCheckedChangeListener { _, checkedId ->
-          questionnaireResponseItemBuilder
-            .clearAnswer()
-            .addAnswer(
-              QuestionnaireResponse.Item.Answer.newBuilder().apply {
-                value = questionnaireItem.answerOptionList[checkedId].responseAnswerValueX
-              }
-            )
-          questionnaireItemViewItem.questionnaireResponseItemChangedCallback()
-        }
-      }
-    }
 }
