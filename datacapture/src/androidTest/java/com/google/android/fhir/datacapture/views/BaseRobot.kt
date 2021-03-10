9a7eb9ddfbdcc2e03fd83f995b3f597860c1cb6e
s-ayush2903 <ayush_1901cb14@iitp.ac.in>
2021-03-11 03:03:02 +0530

    fix formatting

--- datacapture/src/androidTest/java/com/google/android/fhir/datacapture/views/BaseRobot.kt
+++ datacapture/src/androidTest/java/com/google/android/fhir/datacapture/views/BaseRobot.kt
@@ -15,60 +15,60 @@ const val LONG_WAIT = 20_000L
 const val VERY_LONG_WAIT = 40_000L
 
 abstract class BaseRobot(
-	private val instrumentation: Instrumentation = InstrumentationRegistry.getInstrumentation(),
-	var context: Context = instrumentation.targetContext,
-	val uiDevice: UiDevice = UiDevice.getInstance(instrumentation)
+  private val instrumentation: Instrumentation = InstrumentationRegistry.getInstrumentation(),
+  var context: Context = instrumentation.targetContext,
+  val uiDevice: UiDevice = UiDevice.getInstance(instrumentation)
 ) {
 
-	internal fun clickNegativeDialogButton() {
-		clickOn(Findable.ViewId(R.id.button2))
-	}
+  internal fun clickNegativeDialogButton() {
+    clickOn(Findable.ViewId(R.id.button2))
+  }
 
-	internal fun clickPositiveDialogButton() {
-		clickOn(Findable.ViewId(R.id.button1))
-	}
+  internal fun clickPositiveDialogButton() {
+    clickOn(Findable.ViewId(R.id.button1))
+  }
 
-	internal fun pressBack() {
-		uiDevice.pressBack()
-	}
+  internal fun pressBack() {
+    uiDevice.pressBack()
+  }
 
-	protected fun isVisible(findable: Findable, timeout: Long = DEFAULT_WAIT) =
-		waitFor(findable, timeout) ?: throw RuntimeException(findable.errorMessage(this))
+  protected fun isVisible(findable: Findable, timeout: Long = DEFAULT_WAIT) =
+    waitFor(findable, timeout) ?: throw RuntimeException(findable.errorMessage(this))
 
-	protected fun UiObject2.swipeLeft() {
-		customSwipe(Direction.LEFT)
-	}
+  protected fun UiObject2.swipeLeft() {
+    customSwipe(Direction.LEFT)
+  }
 
-	protected fun UiObject2.swipeRight() {
-		customSwipe(Direction.RIGHT)
-	}
+  protected fun UiObject2.swipeRight() {
+    customSwipe(Direction.RIGHT)
+  }
 
-	protected fun clickOn(findable: Findable, timeout: Long = DEFAULT_WAIT) {
-		isVisible(findable, timeout).click()
-	}
+  protected fun clickOn(findable: Findable, timeout: Long = DEFAULT_WAIT) {
+    isVisible(findable, timeout).click()
+  }
 
-	protected fun longClickOn(findable: Findable) {
-		isVisible(findable).click(1000L)
-	}
+  protected fun longClickOn(findable: Findable) {
+    isVisible(findable).click(1000L)
+  }
 
-	protected fun clickOnTab(textId: Int) {
-		clickOn(ContentDesc(textId), LONG_WAIT)
-	}
+  protected fun clickOnTab(textId: Int) {
+    clickOn(ContentDesc(textId), LONG_WAIT)
+  }
 
-	protected fun waitFor(milliseconds: Long) {
-		waitFor(Findable.Text("WILL_NEVER_EXIST"), milliseconds)
-	}
+  protected fun waitFor(milliseconds: Long) {
+    waitFor(Findable.Text("WILL_NEVER_EXIST"), milliseconds)
+  }
 
-	private fun waitFor(
-		findable: Findable,
-		timeout: Long = DEFAULT_WAIT
-	): UiObject2? =
-		uiDevice.wait(Until.findObject(findable.selector(this)), timeout)
+  private fun waitFor(
+    findable: Findable,
+    timeout: Long = DEFAULT_WAIT
+  ): UiObject2? =
+    uiDevice.wait(Until.findObject(findable.selector(this)), timeout)
 
-	private fun UiObject2.customSwipe(
-		direction: Direction,
-		percent: Float = 0.8f
-	) {
-		swipe(direction, percent)
-	}
+  private fun UiObject2.customSwipe(
+    direction: Direction,
+    percent: Float = 0.8f
+  ) {
+    swipe(direction, percent)
+  }
 }
--- datacapture/src/androidTest/java/com/google/android/fhir/datacapture/views/QuestionnaireItemDatePickerViewHolderFactoryInstrumentedTest.kt
+++ datacapture/src/androidTest/java/com/google/android/fhir/datacapture/views/QuestionnaireItemDatePickerViewHolderFactoryInstrumentedTest.kt
@@ -20,11 +20,6 @@ import android.widget.FrameLayout
 import android.widget.TextView
 import androidx.appcompat.view.ContextThemeWrapper
 import androidx.test.annotation.UiThreadTest
-import androidx.test.espresso.Espresso.onView
-import androidx.test.espresso.action.ViewActions
-import androidx.test.espresso.assertion.ViewAssertions
-import androidx.test.espresso.matcher.ViewMatchers.withClassName
-import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.platform.app.InstrumentationRegistry
 import com.google.android.fhir.datacapture.R
@@ -32,7 +27,6 @@ import com.google.common.truth.Truth.assertThat
 import com.google.fhir.r4.core.Date
 import com.google.fhir.r4.core.Questionnaire
 import com.google.fhir.r4.core.QuestionnaireResponse
-import org.hamcrest.Matchers.containsString
 import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -41,92 +35,92 @@ import java.time.ZoneId
 
 @RunWith(AndroidJUnit4::class)
 class QuestionnaireItemDatePickerViewHolderFactoryInstrumentedTest {
-	private lateinit var context: ContextThemeWrapper
-	private lateinit var parent: FrameLayout
-	private lateinit var viewHolder: QuestionnaireItemViewHolder
+  private lateinit var context: ContextThemeWrapper
+  private lateinit var parent: FrameLayout
+  private lateinit var viewHolder: QuestionnaireItemViewHolder
 
-	@Before
-	fun setUp() {
-		context =
-			ContextThemeWrapper(
-				InstrumentationRegistry.getInstrumentation().targetContext,
-				R.style.Theme_MaterialComponents
-			)
-		parent = FrameLayout(context)
-		viewHolder = QuestionnaireItemDatePickerViewHolderFactory.create(parent)
-	}
+  @Before
+  fun setUp() {
+    context =
+      ContextThemeWrapper(
+        InstrumentationRegistry.getInstrumentation().targetContext,
+        R.style.Theme_MaterialComponents
+      )
+    parent = FrameLayout(context)
+    viewHolder = QuestionnaireItemDatePickerViewHolderFactory.create(parent)
+  }
 
-	@Test
-	fun shouldSetTextInputLayoutHint() {
-		viewHolder.bind(
-			QuestionnaireItemViewItem(
-				Questionnaire.Item.newBuilder()
-					.apply {
-						text = com.google.fhir.r4.core.String.newBuilder().setValue("Question?").build()
-					}
-					.build(),
-				QuestionnaireResponse.Item.newBuilder()
-			) {}
-		)
+  @Test
+  fun shouldSetTextInputLayoutHint() {
+    viewHolder.bind(
+      QuestionnaireItemViewItem(
+        Questionnaire.Item.newBuilder()
+          .apply {
+            text = com.google.fhir.r4.core.String.newBuilder().setValue("Question?").build()
+          }
+          .build(),
+        QuestionnaireResponse.Item.newBuilder()
+      ) {}
+    )
 
-		assertThat(viewHolder.itemView.findViewById<TextView>(R.id.question).text)
-			.isEqualTo("Question?")
-	}
+    assertThat(viewHolder.itemView.findViewById<TextView>(R.id.question).text)
+      .isEqualTo("Question?")
+  }
 
-	@Test
-	@UiThreadTest
-	fun shouldSetEmptyDateInput() {
-		viewHolder.bind(
-			QuestionnaireItemViewItem(
-				Questionnaire.Item.newBuilder()
-					.apply {
-						text = com.google.fhir.r4.core.String.newBuilder().setValue("Question?").build()
-					}
-					.build(),
-				QuestionnaireResponse.Item.newBuilder()
-			) {}
-		)
+  @Test
+  @UiThreadTest
+  fun shouldSetEmptyDateInput() {
+    viewHolder.bind(
+      QuestionnaireItemViewItem(
+        Questionnaire.Item.newBuilder()
+          .apply {
+            text = com.google.fhir.r4.core.String.newBuilder().setValue("Question?").build()
+          }
+          .build(),
+        QuestionnaireResponse.Item.newBuilder()
+      ) {}
+    )
 
-		assertThat(viewHolder.itemView.findViewById<TextView>(R.id.textInputEditText).text.toString())
-			.isEqualTo("")
-	}
+    assertThat(viewHolder.itemView.findViewById<TextView>(R.id.textInputEditText).text.toString())
+      .isEqualTo("")
+  }
 
-	@Test
-	@UiThreadTest
-	fun shouldSetDateInput() {
-		viewHolder.bind(
-			QuestionnaireItemViewItem(
-				Questionnaire.Item.newBuilder()
-					.apply {
-						text = com.google.fhir.r4.core.String.newBuilder().setValue("Question?").build()
-					}
-					.build(),
-				QuestionnaireResponse.Item.newBuilder()
-					.addAnswer(
-						QuestionnaireResponse.Item.Answer.newBuilder().apply {
-							value =
-								QuestionnaireResponse.Item.Answer.ValueX.newBuilder()
-									.setDate(
-										Date.newBuilder()
-											.setValueUs(
-												LocalDate.of(2020, 1, 1)
-													.atStartOfDay()
-													.atZone(ZoneId.systemDefault())
-													.toEpochSecond() * NUMBER_OF_MICROSECONDS_PER_SECOND
-											)
-											.setPrecision(Date.Precision.DAY)
-											.setTimezone(ZoneId.systemDefault().id)
-									)
-									.build()
-						}
-					)
-			) {}
-		)
+  @Test
+  @UiThreadTest
+  fun shouldSetDateInput() {
+    viewHolder.bind(
+      QuestionnaireItemViewItem(
+        Questionnaire.Item.newBuilder()
+          .apply {
+            text = com.google.fhir.r4.core.String.newBuilder().setValue("Question?").build()
+          }
+          .build(),
+        QuestionnaireResponse.Item.newBuilder()
+          .addAnswer(
+            QuestionnaireResponse.Item.Answer.newBuilder().apply {
+              value =
+                QuestionnaireResponse.Item.Answer.ValueX.newBuilder()
+                  .setDate(
+                    Date.newBuilder()
+                      .setValueUs(
+                        LocalDate.of(2020, 1, 1)
+                          .atStartOfDay()
+                          .atZone(ZoneId.systemDefault())
+                          .toEpochSecond() * NUMBER_OF_MICROSECONDS_PER_SECOND
+                      )
+                      .setPrecision(Date.Precision.DAY)
+                      .setTimezone(ZoneId.systemDefault().id)
+                  )
+                  .build()
+            }
+          )
+      ) {}
+    )
 
-		assertThat(
-			viewHolder.itemView.findViewById<TextView>(R.id.textInputEditText).text.toString()
-		).isEqualTo("2020-01-01")
-	}
+    assertThat(
+      viewHolder.itemView.findViewById<TextView>(R.id.textInputEditText).text.toString()
+    ).isEqualTo("2020-01-01")
+  }
 
 //	@Test
 //	@UiThreadTest
