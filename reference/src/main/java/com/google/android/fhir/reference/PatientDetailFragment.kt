5fc335ab2d8d906f536a390934ac311afd36c657
jingtang10 <jing.t86@gmail.com>
2021-03-12 15:27:46 +0000

    Remove unnecessary imports

--- reference/src/main/java/com/google/android/fhir/reference/PatientDetailFragment.kt
+++ reference/src/main/java/com/google/android/fhir/reference/PatientDetailFragment.kt
@@ -23,11 +23,7 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.core.text.HtmlCompat
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.Observer
-import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.RecyclerView
-import com.google.android.fhir.FhirEngine
-import com.google.android.material.appbar.CollapsingToolbarLayout
 
 /**
  * A fragment representing a single Patient detail screen. This fragment is contained in a
