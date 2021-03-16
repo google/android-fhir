53b4d7181a0d2feed8eff6dd304109c06f0dcdb4
Rodgers Andati <andatirodgers@gmail.com>
2021-03-16 12:45:53 +0300

    remove wildcart import

--- datacapture/src/test/java/com/google/android/fhir/datacapture/mapping/ResourceMapperHAPITest.kt
+++ datacapture/src/test/java/com/google/android/fhir/datacapture/mapping/ResourceMapperHAPITest.kt
@@ -1,24 +1,40 @@
+/*
+ * Copyright 2020 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.google.android.fhir.datacapture.mapping
 
 import android.os.Build
 import ca.uhn.fhir.context.FhirContext
 import ca.uhn.fhir.parser.IParser
 import com.google.android.fhir.datacapture.model.Patient
+import com.google.common.truth.Truth.assertThat
+import java.util.Date
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.robolectric.RobolectricTestRunner
 import org.robolectric.annotation.Config
 
-import com.google.common.truth.Truth.assertThat
-import java.util.*
-
 @RunWith(RobolectricTestRunner::class)
 @Config(sdk = [Build.VERSION_CODES.P])
 class ResourceMapperHAPITest {
-    @Test
-    fun extract() {
-        // https://developer.commure.com/docs/apis/sdc/examples#definition-based-extraction
-        val questionnaireJson = """
+  @Test
+  fun extract() {
+    // https://developer.commure.com/docs/apis/sdc/examples#definition-based-extraction
+    val questionnaireJson =
+      """
             {
               "resourceType": "Questionnaire",
               "id": "client-registration-sample",
@@ -95,7 +111,8 @@ class ResourceMapperHAPITest {
             }
         """.trimIndent()
 
-        val questionnaireResponseJson = """
+    val questionnaireResponseJson =
+      """
             {
               "resourceType": "QuestionnaireResponse",
               "questionnaire": "client-registration-sample",
@@ -154,21 +171,23 @@ class ResourceMapperHAPITest {
             }
         """.trimIndent()
 
-        val iParser: IParser = FhirContext.forR4().newJsonParser()
+    val iParser: IParser = FhirContext.forR4().newJsonParser()
 
-        val uriTestQuestionnaire = iParser.parseResource(org.hl7.fhir.r4.model.Questionnaire::class.java,
-                questionnaireJson) as org.hl7.fhir.r4.model.Questionnaire
-        val uriTestQuestionnaireResponse = iParser.parseResource(org.hl7.fhir.r4.model.QuestionnaireResponse::class.java,
-                questionnaireResponseJson) as org.hl7.fhir.r4.model.QuestionnaireResponse
+    val uriTestQuestionnaire =
+      iParser.parseResource(org.hl7.fhir.r4.model.Questionnaire::class.java, questionnaireJson) as
+        org.hl7.fhir.r4.model.Questionnaire
+    val uriTestQuestionnaireResponse =
+      iParser.parseResource(
+        org.hl7.fhir.r4.model.QuestionnaireResponse::class.java,
+        questionnaireResponseJson
+      ) as
+        org.hl7.fhir.r4.model.QuestionnaireResponse
 
-        val patient = ResourceMapperHAPI.extract(uriTestQuestionnaire,
-                uriTestQuestionnaireResponse)
+    val patient = ResourceMapperHAPI.extract(uriTestQuestionnaire, uriTestQuestionnaireResponse)
 
-        assertThat((patient as Patient).birthDate).isEqualTo(
-                Date(1609448400000)
-        )
-        assertThat(patient.active).isTrue()
-        assertThat(patient.name.first().given.first().toString()).isEqualTo("Rodgers")
-        assertThat(patient.name.first().family).isEqualTo("Andati")
-    }
+    assertThat((patient as Patient).birthDate).isEqualTo(Date(1609448400000))
+    assertThat(patient.active).isTrue()
+    assertThat(patient.name.first().given.first().toString()).isEqualTo("Rodgers")
+    assertThat(patient.name.first().family).isEqualTo("Andati")
+  }
 }
