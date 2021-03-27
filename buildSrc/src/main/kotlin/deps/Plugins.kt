d8900045db3485e8dd8a3a3f15cad5fc7e2685e5
s-ayush2903 <ayush_1901cb14@iitp.ac.in>
2021-03-27 14:26:08 +0530

    Add license in deps kt files

--- buildSrc/src/main/kotlin/deps/Plugins.kt
+++ buildSrc/src/main/kotlin/deps/Plugins.kt
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2020 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package deps
 
 object Plugins {
@@ -5,7 +21,7 @@ object Plugins {
   const val kotlinGradlePlugin = "org.jetbrains.kotlin:kotlin-gradle-plugin:${Dependencies.Versions.Kotlin.stdlib}"
   const val spotlessGradlePlugin = "com.diffplug.spotless:spotless-plugin-gradle:${Versions.spotlessGradlePlugin}"
   const val navSafeArgsGradlePlugin = "androidx.navigation:navigation-safe-args-gradle-plugin:${Dependencies.Versions.Androidx.navigation}"
-  
+
   object Versions {
     const val androidGradlePlugin = "4.2.0-beta02"
     const val spotlessGradlePlugin = "5.11.0"
