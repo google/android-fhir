4f1aabdd6b9716a1bcc9d45793175ba104f8f938
epicadk <adityakurkure@gmail.com>
2021-04-20 12:36:56 +0530

    move integration test after unit tests

--- engine/src/test/java/com/google/android/fhir/index/ResourceIndexerTest.kt
+++ engine/src/test/java/com/google/android/fhir/index/ResourceIndexerTest.kt
@@ -84,236 +84,6 @@ class ResourceIndexerTest {
     testLocation = testingUtils.readFromFile(Location::class.java, "/location-example-hl7hq.json")
   }
 
-  /** Integration tests for ResourceIndexer. */
-  @Test
-  fun index_invoice() {
-    val resourceIndices = ResourceIndexer.index(testInvoice)
-
-    assertThat(resourceIndices.resourceId).isEqualTo(testInvoice.logicalId)
-    assertThat(resourceIndices.resourceType).isEqualTo(testInvoice.resourceType)
-
-    assertThat(resourceIndices.quantityIndices)
-      .containsExactly(
-        // Search parameter names flatten camel case so "totalGross" becomes "totalgross"
-        QuantityIndex(
-          "totalgross",
-          "Invoice.totalGross",
-          FHIR_CURRENCY_SYSTEM,
-          testInvoice.totalGross.currency,
-          testInvoice.totalGross.value
-        ),
-        QuantityIndex(
-          "totalnet",
-          "Invoice.totalNet",
-          FHIR_CURRENCY_SYSTEM,
-          testInvoice.totalNet.currency,
-          testInvoice.totalNet.value
-        )
-      )
-
-    assertThat(resourceIndices.numberIndices).isEmpty()
-
-    assertThat(resourceIndices.tokenIndices)
-      .containsExactly(
-        TokenIndex(
-          "identifier",
-          "Invoice.identifier",
-          testInvoice.identifierFirstRep.system,
-          testInvoice.identifierFirstRep.value
-        ),
-        TokenIndex(
-          "participant-role",
-          "Invoice.participant.role",
-          testInvoice.participantFirstRep.role.codingFirstRep.system,
-          testInvoice.participantFirstRep.role.codingFirstRep.code
-        )
-      )
-
-    assertThat(resourceIndices.uriIndices).isEmpty()
-
-    assertThat(resourceIndices.dateIndices)
-      .contains(
-        DateIndex(
-          "date",
-          "Invoice.date",
-          testInvoice.date.time,
-          testInvoice.dateElement.precision.add(testInvoice.date, 1).time - 1
-        )
-      )
-
-    assertThat(resourceIndices.referenceIndices)
-      .containsExactly(
-        ReferenceIndex("subject", "Invoice.subject", testInvoice.subject.reference),
-        ReferenceIndex(
-          "participant",
-          "Invoice.participant.actor",
-          testInvoice.participantFirstRep.actor.reference
-        ),
-        ReferenceIndex("account", "Invoice.account", testInvoice.account.reference)
-      )
-
-    assertThat(resourceIndices.stringIndices).isEmpty()
-
-    assertThat(resourceIndices.positionIndices).isEmpty()
-  }
-
-  @Test
-  fun index_questionnaire() {
-    val resourceIndices = ResourceIndexer.index(testQuestionnaire)
-
-    assertThat(resourceIndices.resourceType).isEqualTo(testQuestionnaire.resourceType)
-    assertThat(resourceIndices.resourceId).isEqualTo(testQuestionnaire.logicalId)
-
-    assertThat(resourceIndices.uriIndices)
-      .containsExactly(
-        UriIndex("url", "Questionnaire.url", "http://hl7.org/fhir/Questionnaire/3141")
-      )
-
-    assertThat(resourceIndices.numberIndices).isEmpty()
-
-    assertThat(resourceIndices.tokenIndices).isEmpty()
-
-    assertThat(resourceIndices.dateIndices)
-      .containsExactly(
-        DateIndex(
-          "date",
-          "Questionnaire.date",
-          testQuestionnaire.date.time,
-          testQuestionnaire.dateElement.precision.add(testQuestionnaire.date, 1).time - 1
-        )
-      )
-
-    assertThat(resourceIndices.referenceIndices).isEmpty()
-
-    assertThat(resourceIndices.stringIndices)
-      .containsExactly(StringIndex("title", "Questionnaire.title", testQuestionnaire.title))
-
-    assertThat(resourceIndices.positionIndices).isEmpty()
-
-    assertThat(resourceIndices.quantityIndices).isEmpty()
-  }
-
-  @Test
-  fun index_patient() {
-    val resourceIndices = ResourceIndexer.index(testPatient)
-
-    assertThat(resourceIndices.resourceType).isEqualTo(testPatient.resourceType)
-    assertThat(resourceIndices.resourceId).isEqualTo(testPatient.logicalId)
-
-    assertThat(resourceIndices.dateIndices)
-      .containsExactly(
-        DateIndex(
-          "birthdate",
-          "Patient.birthDate",
-          testPatient.birthDateElement.value.time,
-          testPatient.birthDateElement.precision.add(testPatient.birthDateElement.value, 1).time - 1
-        )
-      )
-
-    assertThat(resourceIndices.numberIndices).isEmpty()
-
-    assertThat(resourceIndices.tokenIndices)
-      .containsExactly(
-        TokenIndex(
-          "identifier",
-          "Patient.identifier",
-          testPatient.identifierFirstRep.system,
-          testPatient.identifierFirstRep.value
-        ),
-        TokenIndex(
-          "deceased",
-          "Patient.deceased.exists() and Patient.deceased != false",
-          null,
-          testPatient.deceased.primitiveValue()
-        ),
-        TokenIndex("active", "Patient.active", null, testPatient.active.toString()),
-        TokenIndex(
-          "language",
-          "Patient.communication.language",
-          testPatient.communicationFirstRep.language.codingFirstRep.system,
-          testPatient.communicationFirstRep.language.codingFirstRep.code
-        )
-      )
-
-    assertThat(resourceIndices.uriIndices).isEmpty()
-
-    assertThat(resourceIndices.quantityIndices).isEmpty()
-
-    assertThat(resourceIndices.referenceIndices)
-      .containsExactly(
-        ReferenceIndex(
-          "organization",
-          "Patient.managingOrganization",
-          testPatient.managingOrganization.reference
-        )
-      )
-
-    assertThat(resourceIndices.stringIndices)
-      .containsExactly(
-        StringIndex("given", "Patient.name.given", testPatient.nameFirstRep.givenAsSingleString),
-        StringIndex("address", "Patient.address", testPatient.addressFirstRep.toString()),
-        StringIndex(
-          "address-postalcode",
-          "Patient.address.postalCode",
-          testPatient.addressFirstRep.postalCode
-        ),
-        StringIndex(
-          "address-country",
-          "Patient.address.country",
-          testPatient.addressFirstRep.country
-        ),
-        StringIndex("phonetic", "Patient.name", testPatient.nameFirstRep.toString()),
-        StringIndex("name", "Patient.name", testPatient.nameFirstRep.toString()),
-        StringIndex("family", "Patient.name.family", testPatient.nameFirstRep.family),
-        StringIndex("address-city", "Patient.address.city", testPatient.addressFirstRep.city)
-      )
-
-    assertThat(resourceIndices.positionIndices).isEmpty()
-  }
-
-  @Test
-  fun index_location() {
-    val resourceIndices = ResourceIndexer.index(testLocation)
-    assertThat(resourceIndices.resourceType).isEqualTo(testLocation.resourceType)
-    assertThat(resourceIndices.resourceId).isEqualTo(testLocation.logicalId)
-
-    assertThat(resourceIndices.positionIndices).containsExactly(PositionIndex(-83.69471, 42.2565))
-
-    assertThat(resourceIndices.numberIndices).isEmpty()
-
-    assertThat(resourceIndices.tokenIndices)
-      .containsExactly(
-        TokenIndex(
-          "type",
-          "Location.type",
-          testLocation.typeFirstRep.codingFirstRep.system,
-          testLocation.typeFirstRep.codingFirstRep.code
-        )
-      )
-
-    assertThat(resourceIndices.uriIndices).isEmpty()
-
-    assertThat(resourceIndices.dateIndices).isEmpty()
-
-    assertThat(resourceIndices.referenceIndices).isEmpty()
-
-    assertThat(resourceIndices.quantityIndices).isEmpty()
-
-    assertThat(resourceIndices.stringIndices)
-      .containsExactly(
-        StringIndex("address", "Location.address", testLocation.address.toString()),
-        StringIndex("address-state", "Location.address.state", testLocation.address.state),
-        StringIndex(
-          "address-postalcode",
-          "Location.address.postalCode",
-          testLocation.address.postalCode
-        ),
-        StringIndex("name", "Location.name | Location.alias", testLocation.name),
-        StringIndex("address-country", "Location.address.country", testLocation.address.country),
-        StringIndex("address-city", "Location.address.city", testLocation.address.city)
-      )
-  }
-
   /** Unit tests for resource indexer */
   @Test
   fun index_Patient_shouldIndexLastUpdatedDate() {
@@ -801,6 +571,236 @@ class ResourceIndexerTest {
       }
     val resourceIndices = ResourceIndexer.index(location)
     assertThat(resourceIndices.positionIndices).isEmpty()
+  }
+
+  /** Integration tests for ResourceIndexer. */
+  @Test
+  fun index_invoice() {
+    val resourceIndices = ResourceIndexer.index(testInvoice)
+
+    assertThat(resourceIndices.resourceId).isEqualTo(testInvoice.logicalId)
+    assertThat(resourceIndices.resourceType).isEqualTo(testInvoice.resourceType)
+
+    assertThat(resourceIndices.quantityIndices)
+      .containsExactly(
+        // Search parameter names flatten camel case so "totalGross" becomes "totalgross"
+        QuantityIndex(
+          "totalgross",
+          "Invoice.totalGross",
+          FHIR_CURRENCY_SYSTEM,
+          testInvoice.totalGross.currency,
+          testInvoice.totalGross.value
+        ),
+        QuantityIndex(
+          "totalnet",
+          "Invoice.totalNet",
+          FHIR_CURRENCY_SYSTEM,
+          testInvoice.totalNet.currency,
+          testInvoice.totalNet.value
+        )
+      )
+
+    assertThat(resourceIndices.numberIndices).isEmpty()
+
+    assertThat(resourceIndices.tokenIndices)
+      .containsExactly(
+        TokenIndex(
+          "identifier",
+          "Invoice.identifier",
+          testInvoice.identifierFirstRep.system,
+          testInvoice.identifierFirstRep.value
+        ),
+        TokenIndex(
+          "participant-role",
+          "Invoice.participant.role",
+          testInvoice.participantFirstRep.role.codingFirstRep.system,
+          testInvoice.participantFirstRep.role.codingFirstRep.code
+        )
+      )
+
+    assertThat(resourceIndices.uriIndices).isEmpty()
+
+    assertThat(resourceIndices.dateIndices)
+      .contains(
+        DateIndex(
+          "date",
+          "Invoice.date",
+          testInvoice.date.time,
+          testInvoice.dateElement.precision.add(testInvoice.date, 1).time - 1
+        )
+      )
+
+    assertThat(resourceIndices.referenceIndices)
+      .containsExactly(
+        ReferenceIndex("subject", "Invoice.subject", testInvoice.subject.reference),
+        ReferenceIndex(
+          "participant",
+          "Invoice.participant.actor",
+          testInvoice.participantFirstRep.actor.reference
+        ),
+        ReferenceIndex("account", "Invoice.account", testInvoice.account.reference)
+      )
+
+    assertThat(resourceIndices.stringIndices).isEmpty()
+
+    assertThat(resourceIndices.positionIndices).isEmpty()
+  }
+
+  @Test
+  fun index_questionnaire() {
+    val resourceIndices = ResourceIndexer.index(testQuestionnaire)
+
+    assertThat(resourceIndices.resourceType).isEqualTo(testQuestionnaire.resourceType)
+    assertThat(resourceIndices.resourceId).isEqualTo(testQuestionnaire.logicalId)
+
+    assertThat(resourceIndices.uriIndices)
+      .containsExactly(
+        UriIndex("url", "Questionnaire.url", "http://hl7.org/fhir/Questionnaire/3141")
+      )
+
+    assertThat(resourceIndices.numberIndices).isEmpty()
+
+    assertThat(resourceIndices.tokenIndices).isEmpty()
+
+    assertThat(resourceIndices.dateIndices)
+      .containsExactly(
+        DateIndex(
+          "date",
+          "Questionnaire.date",
+          testQuestionnaire.date.time,
+          testQuestionnaire.dateElement.precision.add(testQuestionnaire.date, 1).time - 1
+        )
+      )
+
+    assertThat(resourceIndices.referenceIndices).isEmpty()
+
+    assertThat(resourceIndices.stringIndices)
+      .containsExactly(StringIndex("title", "Questionnaire.title", testQuestionnaire.title))
+
+    assertThat(resourceIndices.positionIndices).isEmpty()
+
+    assertThat(resourceIndices.quantityIndices).isEmpty()
+  }
+
+  @Test
+  fun index_patient() {
+    val resourceIndices = ResourceIndexer.index(testPatient)
+
+    assertThat(resourceIndices.resourceType).isEqualTo(testPatient.resourceType)
+    assertThat(resourceIndices.resourceId).isEqualTo(testPatient.logicalId)
+
+    assertThat(resourceIndices.dateIndices)
+      .containsExactly(
+        DateIndex(
+          "birthdate",
+          "Patient.birthDate",
+          testPatient.birthDateElement.value.time,
+          testPatient.birthDateElement.precision.add(testPatient.birthDateElement.value, 1).time - 1
+        )
+      )
+
+    assertThat(resourceIndices.numberIndices).isEmpty()
+
+    assertThat(resourceIndices.tokenIndices)
+      .containsExactly(
+        TokenIndex(
+          "identifier",
+          "Patient.identifier",
+          testPatient.identifierFirstRep.system,
+          testPatient.identifierFirstRep.value
+        ),
+        TokenIndex(
+          "deceased",
+          "Patient.deceased.exists() and Patient.deceased != false",
+          null,
+          testPatient.deceased.primitiveValue()
+        ),
+        TokenIndex("active", "Patient.active", null, testPatient.active.toString()),
+        TokenIndex(
+          "language",
+          "Patient.communication.language",
+          testPatient.communicationFirstRep.language.codingFirstRep.system,
+          testPatient.communicationFirstRep.language.codingFirstRep.code
+        )
+      )
+
+    assertThat(resourceIndices.uriIndices).isEmpty()
+
+    assertThat(resourceIndices.quantityIndices).isEmpty()
+
+    assertThat(resourceIndices.referenceIndices)
+      .containsExactly(
+        ReferenceIndex(
+          "organization",
+          "Patient.managingOrganization",
+          testPatient.managingOrganization.reference
+        )
+      )
+
+    assertThat(resourceIndices.stringIndices)
+      .containsExactly(
+        StringIndex("given", "Patient.name.given", testPatient.nameFirstRep.givenAsSingleString),
+        StringIndex("address", "Patient.address", testPatient.addressFirstRep.toString()),
+        StringIndex(
+          "address-postalcode",
+          "Patient.address.postalCode",
+          testPatient.addressFirstRep.postalCode
+        ),
+        StringIndex(
+          "address-country",
+          "Patient.address.country",
+          testPatient.addressFirstRep.country
+        ),
+        StringIndex("phonetic", "Patient.name", testPatient.nameFirstRep.toString()),
+        StringIndex("name", "Patient.name", testPatient.nameFirstRep.toString()),
+        StringIndex("family", "Patient.name.family", testPatient.nameFirstRep.family),
+        StringIndex("address-city", "Patient.address.city", testPatient.addressFirstRep.city)
+      )
+
+    assertThat(resourceIndices.positionIndices).isEmpty()
+  }
+
+  @Test
+  fun index_location() {
+    val resourceIndices = ResourceIndexer.index(testLocation)
+    assertThat(resourceIndices.resourceType).isEqualTo(testLocation.resourceType)
+    assertThat(resourceIndices.resourceId).isEqualTo(testLocation.logicalId)
+
+    assertThat(resourceIndices.positionIndices).containsExactly(PositionIndex(-83.69471, 42.2565))
+
+    assertThat(resourceIndices.numberIndices).isEmpty()
+
+    assertThat(resourceIndices.tokenIndices)
+      .containsExactly(
+        TokenIndex(
+          "type",
+          "Location.type",
+          testLocation.typeFirstRep.codingFirstRep.system,
+          testLocation.typeFirstRep.codingFirstRep.code
+        )
+      )
+
+    assertThat(resourceIndices.uriIndices).isEmpty()
+
+    assertThat(resourceIndices.dateIndices).isEmpty()
+
+    assertThat(resourceIndices.referenceIndices).isEmpty()
+
+    assertThat(resourceIndices.quantityIndices).isEmpty()
+
+    assertThat(resourceIndices.stringIndices)
+      .containsExactly(
+        StringIndex("address", "Location.address", testLocation.address.toString()),
+        StringIndex("address-state", "Location.address.state", testLocation.address.state),
+        StringIndex(
+          "address-postalcode",
+          "Location.address.postalCode",
+          testLocation.address.postalCode
+        ),
+        StringIndex("name", "Location.name | Location.alias", testLocation.name),
+        StringIndex("address-country", "Location.address.country", testLocation.address.country),
+        StringIndex("address-city", "Location.address.city", testLocation.address.city)
+      )
   }
 
   // TODO: improve the tests further.
