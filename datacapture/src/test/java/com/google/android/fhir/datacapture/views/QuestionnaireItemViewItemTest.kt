acb23355e5987160ff537cd5078938b8b38bb752
Kashyap Jois <kjois@iprdgroup.com>
2021-05-26 22:54:24 +0530

    Added unit tests for addAnswer, removeAnswer and hasAnswerOption functions

--- datacapture/src/test/java/com/google/android/fhir/datacapture/views/QuestionnaireItemViewItemTest.kt
+++ datacapture/src/test/java/com/google/android/fhir/datacapture/views/QuestionnaireItemViewItemTest.kt
@@ -18,7 +18,9 @@ package com.google.android.fhir.datacapture.views
 
 import android.os.Build
 import com.google.common.truth.Truth.assertThat
+import kotlin.test.assertFailsWith
 import org.hl7.fhir.r4.model.BooleanType
+import org.hl7.fhir.r4.model.Coding
 import org.hl7.fhir.r4.model.Questionnaire
 import org.hl7.fhir.r4.model.QuestionnaireResponse
 import org.junit.Test
@@ -70,5 +72,168 @@ class QuestionnaireItemViewItemTest {
         }
       ) {}
     assertThat(questionnaireItemViewItem.singleAnswerOrNull).isNull()
+  }
+
+  @Test
+  fun addAnswer_questionnaireItemDoesNotRepeat_shouldThrowIllegalArgument() {
+    val questionnaireItemViewItem =
+      QuestionnaireItemViewItem(
+        Questionnaire.QuestionnaireItemComponent().apply { linkId = "a-question" },
+        QuestionnaireResponse.QuestionnaireResponseItemComponent().apply {
+          addAnswer(
+            QuestionnaireResponse.QuestionnaireResponseItemAnswerComponent()
+              .setValue(BooleanType(true))
+          )
+        }
+      ) {}
+
+    val errorMessage =
+      assertFailsWith<IllegalArgumentException> {
+          questionnaireItemViewItem.addAnswer(
+            QuestionnaireResponse.QuestionnaireResponseItemAnswerComponent()
+              .setValue(BooleanType(true))
+          )
+        }
+        .localizedMessage
+
+    assertThat(errorMessage)
+      .isEqualTo("Questionnaire item with linkId a-question does not allow repeated answers")
+  }
+
+  @Test
+  fun addAnswer_questionnaireItemRepeats_shouldAddQuestionnaireResponseItemAnswerComponent() {
+    val questionnaireItemViewItem =
+      QuestionnaireItemViewItem(
+        Questionnaire.QuestionnaireItemComponent().apply {
+          repeats = true
+          linkId = "a-question"
+        },
+        QuestionnaireResponse.QuestionnaireResponseItemComponent().apply {
+          addAnswer(
+            QuestionnaireResponse.QuestionnaireResponseItemAnswerComponent()
+              .setValue(BooleanType(true))
+          )
+        }
+      ) {}
+
+    questionnaireItemViewItem.addAnswer(
+      QuestionnaireResponse.QuestionnaireResponseItemAnswerComponent().setValue(BooleanType(true))
+    )
+
+    assertThat(questionnaireItemViewItem.questionnaireResponseItem.answer.size).isEqualTo(2)
+  }
+
+  @Test
+  fun removeAnswer_questionnaireItemDoesNotRepeat_shouldThrowIllegalArgument() {
+    val questionnaireItemViewItem =
+      QuestionnaireItemViewItem(
+        Questionnaire.QuestionnaireItemComponent().apply { linkId = "a-question" },
+        QuestionnaireResponse.QuestionnaireResponseItemComponent().apply {
+          addAnswer(
+            QuestionnaireResponse.QuestionnaireResponseItemAnswerComponent()
+              .setValue(BooleanType(true))
+          )
+          addAnswer(
+            QuestionnaireResponse.QuestionnaireResponseItemAnswerComponent()
+              .setValue(BooleanType(true))
+          )
+        }
+      ) {}
+
+    val errorMessage =
+      assertFailsWith<IllegalArgumentException> {
+          questionnaireItemViewItem.removeAnswer(
+            QuestionnaireResponse.QuestionnaireResponseItemAnswerComponent()
+              .setValue(BooleanType(true))
+          )
+        }
+        .localizedMessage
+
+    assertThat(errorMessage)
+      .isEqualTo("Questionnaire item with linkId a-question does not allow repeated answers")
+  }
+
+  @Test
+  fun removeAnswer_questionnaireItemRepeats_shouldRemoveQuestionnaireResponseItemAnswerComponent() {
+    val questionnaireItemViewItem =
+      QuestionnaireItemViewItem(
+        Questionnaire.QuestionnaireItemComponent().apply {
+          repeats = true
+          linkId = "a-question"
+        },
+        QuestionnaireResponse.QuestionnaireResponseItemComponent().apply {
+          addAnswer(
+            QuestionnaireResponse.QuestionnaireResponseItemAnswerComponent()
+              .setValue(BooleanType(true))
+          )
+          addAnswer(
+            QuestionnaireResponse.QuestionnaireResponseItemAnswerComponent()
+              .setValue(BooleanType(false))
+          )
+          addAnswer(
+            QuestionnaireResponse.QuestionnaireResponseItemAnswerComponent()
+              .setValue(BooleanType(false))
+          )
+        }
+      ) {}
+
+    questionnaireItemViewItem.removeAnswer(
+      QuestionnaireResponse.QuestionnaireResponseItemAnswerComponent().setValue(BooleanType(false))
+    )
+
+    assertThat(questionnaireItemViewItem.questionnaireResponseItem.answer.size).isEqualTo(1)
+  }
+
+  @Test
+  fun hasAnswerOption_questionnaireItemRepeats_shouldReturnTrue() {
+    val questionnaireItemViewItem =
+      QuestionnaireItemViewItem(
+        Questionnaire.QuestionnaireItemComponent().apply {
+          repeats = true
+          linkId = "a-question"
+          addAnswerOption(
+            Questionnaire.QuestionnaireItemAnswerOptionComponent().setValue(Coding("sample-system", "sample-code1", "Sample Code1"))
+          )
+          addAnswerOption(
+            Questionnaire.QuestionnaireItemAnswerOptionComponent().setValue(Coding("sample-system", "sample-code2", "Sample Code2"))
+          )
+        },
+        QuestionnaireResponse.QuestionnaireResponseItemComponent().apply {
+          addAnswer(
+            QuestionnaireResponse.QuestionnaireResponseItemAnswerComponent()
+              .setValue(Coding("sample-system", "sample-code2", "Sample Code2"))
+          )
+        }
+      ) {}
+
+    assertThat(
+      questionnaireItemViewItem.hasAnswerOption(
+        Questionnaire.QuestionnaireItemAnswerOptionComponent().setValue(Coding("sample-system", "sample-code2", "Sample Code2"))
+      )
+    ).isTrue()
+  }
+
+  @Test
+  fun hasAnswerOption_questionnaireItemRepeats_shouldReturnFalse() {
+    val questionnaireItemViewItem =
+      QuestionnaireItemViewItem(
+        Questionnaire.QuestionnaireItemComponent().apply {
+          repeats = true
+          linkId = "a-question"
+          addAnswerOption(
+            Questionnaire.QuestionnaireItemAnswerOptionComponent().setValue(Coding("sample-system", "sample-code1", "Sample Code1"))
+          )
+          addAnswerOption(
+            Questionnaire.QuestionnaireItemAnswerOptionComponent().setValue(Coding("sample-system", "sample-code2", "Sample Code2"))
+          )
+        },
+        QuestionnaireResponse.QuestionnaireResponseItemComponent()
+      ) {}
+
+    assertThat(
+      questionnaireItemViewItem.hasAnswerOption(
+        Questionnaire.QuestionnaireItemAnswerOptionComponent().setValue(Coding("sample-system", "sample-code2", "Sample Code2"))
+      )
+    ).isFalse()
   }
 }
