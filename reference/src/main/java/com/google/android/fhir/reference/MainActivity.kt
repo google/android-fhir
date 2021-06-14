125a979b38b2267da1fee044ac501cfde406c10b
aditya-07 <aditya.pu2010@gmail.com>
2021-06-14 17:29:16 +0530

    Added Navigation library to Reference app.  (#533)
    * Added Navigation library to Reference app. Also updated the patient details page
    
    * Review comment changes
    
    * Spotless apply

---
+++ reference/src/main/java/com/google/android/fhir/reference/MainActivity.kt
@@ -0,0 +1,49 @@
+/*
+ * Copyright 2020 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.google.android.fhir.reference
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
+import androidx.work.Constraints
+import com.google.android.fhir.reference.data.FhirPeriodicSyncWorker
+import com.google.android.fhir.sync.PeriodicSyncConfiguration
+import com.google.android.fhir.sync.RepeatInterval
+import com.google.android.fhir.sync.Sync
+import java.util.concurrent.TimeUnit
+
+const val MAX_RESOURCE_COUNT = 20
+
+class MainActivity : AppCompatActivity() {
+
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    setContentView(R.layout.activity_main)
+
+    val toolbar = findViewById<Toolbar>(R.id.toolbar)
+    setSupportActionBar(toolbar)
+    toolbar.title = title
+
+    Sync.periodicSync<FhirPeriodicSyncWorker>(
+      this,
+      PeriodicSyncConfiguration(
+        syncConstraints = Constraints.Builder().build(),
+        repeat = RepeatInterval(interval = 1, timeUnit = TimeUnit.MINUTES)
+      )
+    )
+  }
+}
