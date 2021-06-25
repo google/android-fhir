defaebcea803aeea7e2506f1325c38ecbbec5c5d
epicadk <adityakurkure@gmail.com>
2021-06-26 00:31:55 +0530

    Add some tests

--- hapiprotoconverter/src/test/java/com/google/android/fhir/hapiprotoconverter/EnumConverterTest.kt
+++ hapiprotoconverter/src/test/java/com/google/android/fhir/hapiprotoconverter/EnumConverterTest.kt
@@ -21,33 +21,31 @@ import com.google.fhir.r4.core.AddressTypeCode
 import com.google.fhir.r4.core.AddressUseCode
 import com.google.fhir.r4.core.AdministrativeGenderCode
 import com.google.fhir.r4.core.BindingStrengthCode
+import com.google.fhir.r4.core.CapabilityStatement
 import com.google.fhir.r4.core.CapabilityStatementKindCode
-import com.google.fhir.r4.core.ConditionalDeleteStatusCode
-import com.google.fhir.r4.core.ConditionalReadStatusCode
-import com.google.fhir.r4.core.ConstraintSeverityCode
+import com.google.fhir.r4.core.ContactPoint
 import com.google.fhir.r4.core.ContactPointSystemCode
 import com.google.fhir.r4.core.ContactPointUseCode
-import com.google.fhir.r4.core.DataAbsentReasonCode
-import com.google.fhir.r4.core.DaysOfWeekCode
-import com.google.fhir.r4.core.DiscriminatorTypeCode
-import com.google.fhir.r4.core.DocumentModeCode
-import com.google.fhir.r4.core.NameUseCode
+import com.google.fhir.r4.core.OperationDefinition
+import com.google.fhir.r4.core.Person
+import com.google.fhir.shaded.protobuf.GeneratedMessageV3
 import com.google.fhir.shaded.protobuf.ProtocolMessageEnum
+import java.io.Serializable
+import org.hl7.fhir.instance.model.api.IBaseEnumeration
 import org.hl7.fhir.r4.model.Address
+import org.hl7.fhir.r4.model.Enumeration
 import org.hl7.fhir.r4.model.codesystems.AddressType
+import org.hl7.fhir.r4.model.codesystems.AddressTypeEnumFactory
 import org.hl7.fhir.r4.model.codesystems.AdministrativeGender
+import org.hl7.fhir.r4.model.codesystems.AdministrativeGenderEnumFactory
 import org.hl7.fhir.r4.model.codesystems.BindingStrength
+import org.hl7.fhir.r4.model.codesystems.BindingStrengthEnumFactory
 import org.hl7.fhir.r4.model.codesystems.CapabilityStatementKind
-import org.hl7.fhir.r4.model.codesystems.ConditionalDeleteStatus
-import org.hl7.fhir.r4.model.codesystems.ConditionalReadStatus
-import org.hl7.fhir.r4.model.codesystems.ConstraintSeverity
+import org.hl7.fhir.r4.model.codesystems.CapabilityStatementKindEnumFactory
 import org.hl7.fhir.r4.model.codesystems.ContactPointSystem
+import org.hl7.fhir.r4.model.codesystems.ContactPointSystemEnumFactory
 import org.hl7.fhir.r4.model.codesystems.ContactPointUse
-import org.hl7.fhir.r4.model.codesystems.DataAbsentReason
-import org.hl7.fhir.r4.model.codesystems.DaysOfWeek
-import org.hl7.fhir.r4.model.codesystems.DiscriminatorType
-import org.hl7.fhir.r4.model.codesystems.DocumentMode
-import org.hl7.fhir.r4.model.codesystems.NameUse
+import org.hl7.fhir.r4.model.codesystems.ContactPointUseEnumFactory
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.junit.runners.Parameterized
@@ -55,111 +53,100 @@ import org.junit.runners.Parameterized
 @RunWith(Parameterized::class)
 class EnumConverterTest {
 
-  @Parameterized.Parameter(0) lateinit var hapi: Enum<*>
-  @Parameterized.Parameter(1) lateinit var proto: ProtocolMessageEnum
+  @Parameterized.Parameter(0) lateinit var hapi: IBaseEnumeration<*>
+  @Parameterized.Parameter(1) lateinit var proto: GeneratedMessageV3
 
   @Test
-  fun test_enum_converter_test() {
+  fun test_enum_converter_test_outter() {
     Truth.assertThat(convert(hapi, proto::class.java)).isEqualTo(proto)
-    Truth.assertThat(convert(proto, hapi::class.java)).isEqualTo(hapi)
+    Truth.assertThat(convert(proto, hapi::class.java, hapi.value::class.java).value)
+      .isEquivalentAccordingToCompareTo(hapi.value)
   }
 
+  @Test
+  @Suppress("UNCHECKED_CAST")
+  fun test_enum_converter_test_inner() {
+    // Probably not the best idea to use reflection is tests , we could just pass more parameters to
+    // the Test Class
+    val protoEnumValue =
+      proto::class.java.getDeclaredMethod("getValue").invoke(proto) as ProtocolMessageEnum
+    Truth.assertThat(convert(hapi.value, protoEnumValue::class.java)).isEqualTo(protoEnumValue)
+    Truth.assertThat(convert(protoEnumValue, hapi.value::class.java))
+      .isEquivalentAccordingToCompareTo(hapi.value)
+  }
   private companion object {
     @Parameterized.Parameters
     @JvmStatic
 
     // TODO add more tests
-    fun testParams(): Collection<Array<Enum<*>>> {
-      return listOf(
-        arrayOf(AdministrativeGender.FEMALE, AdministrativeGenderCode.Value.FEMALE),
-        arrayOf(NameUse.NICKNAME, NameUseCode.Value.NICKNAME),
-      ) +
-        AddressType.values().filter { it.name != "NULL" }.map {
-          arrayOf(it, AddressTypeCode.Value.valueOf(it.toCode().replace('-', '_').toUpperCase()))
-        } +
+    fun testParams(): List<Array<out Serializable>> {
+      return AddressType.values().filter { it.name != "NULL" }.map {
+        arrayOf(
+          Enumeration(AddressTypeEnumFactory(), it),
+          com.google.fhir.r4.core.Address.TypeCode.newBuilder()
+            .setValue(AddressTypeCode.Value.valueOf(it.toCode().replace('-', '_').toUpperCase()))
+            .build()
+        )
+      } +
         Address.AddressUse.values().filter { it.name != "NULL" }.map {
-          arrayOf(it, AddressUseCode.Value.valueOf(it.toCode().replace('-', '_').toUpperCase()))
+          arrayOf(
+            Enumeration(Address.AddressUseEnumFactory(), it),
+            com.google.fhir.r4.core.Address.UseCode.newBuilder()
+              .setValue(AddressUseCode.Value.valueOf(it.toCode().replace('-', '_').toUpperCase()))
+              .build()
+          )
         } +
         AdministrativeGender.values().filter { it.name != "NULL" }.map {
           arrayOf(
-            it,
-            AdministrativeGenderCode.Value.valueOf(it.toCode().replace('-', '_').toUpperCase())
+            Enumeration(AdministrativeGenderEnumFactory(), it),
+            Person.GenderCode.newBuilder()
+              .setValue(
+                AdministrativeGenderCode.Value.valueOf(it.toCode().replace('-', '_').toUpperCase())
+              )
+              .build()
           )
         } +
         BindingStrength.values().filter { it.name != "NULL" }.map {
           arrayOf(
-            it,
-            BindingStrengthCode.Value.valueOf(it.toCode().replace('-', '_').toUpperCase())
+            Enumeration(BindingStrengthEnumFactory(), it),
+            OperationDefinition.Parameter.Binding.StrengthCode.newBuilder()
+              .setValue(
+                BindingStrengthCode.Value.valueOf(it.toCode().replace('-', '_').toUpperCase())
+              )
+              .build()
           )
         } +
-        //        BundleType.TRANSACTIONRESPONSE.values().filter { it.name != "NULL" }.map {
-        //          arrayOf(it,
-        // BundleTypeCode.ValuevalueOf(it.toCode().replace('-','_').toUpperCase()))
-        //        } +
         CapabilityStatementKind.values().filter { it.name != "NULL" }.map {
           arrayOf(
-            it,
-            CapabilityStatementKindCode.Value.valueOf(it.toCode().replace('-', '_').toUpperCase())
-          )
-        } +
-        //        CodeSystem.CodeSystemContentMode.NOTPRESENT.toCode.values().filter { it.name !=
-        // "NULL" }.map {
-        //          arrayOf(it,
-        // CodeSystemContentModeCode.Value.NOT_PRESENT.valueOf(it.toCode().replace('-','_').toUpperCase()))
-        //        } +
-        ConditionalDeleteStatus.values().filter { it.name != "NULL" }.map {
-          arrayOf(
-            it,
-            ConditionalDeleteStatusCode.Value.valueOf(it.toCode().replace('-', '_').toUpperCase())
-          )
-        } +
-        ConditionalReadStatus.values().filter { it.name != "NULL" }.map {
-          arrayOf(
-            it,
-            ConditionalReadStatusCode.Value.valueOf(it.toCode().replace('-', '_').toUpperCase())
-          )
-        } +
-        ConstraintSeverity.values().filter { it.name != "NULL" }.map {
-          arrayOf(
-            it,
-            ConstraintSeverityCode.Value.valueOf(it.toCode().replace('-', '_').toUpperCase())
+            Enumeration(CapabilityStatementKindEnumFactory(), it),
+            CapabilityStatement.KindCode.newBuilder()
+              .setValue(
+                CapabilityStatementKindCode.Value.valueOf(
+                  it.toCode().replace('-', '_').toUpperCase()
+                )
+              )
+              .build()
           )
         } +
         ContactPointSystem.values().filter { it.name != "NULL" }.map {
           arrayOf(
-            it,
-            ContactPointSystemCode.Value.valueOf(it.toCode().replace('-', '_').toUpperCase())
+            Enumeration(ContactPointSystemEnumFactory(), it),
+            ContactPoint.SystemCode.newBuilder()
+              .setValue(
+                ContactPointSystemCode.Value.valueOf(it.toCode().replace('-', '_').toUpperCase())
+              )
+              .build()
           )
         } +
         ContactPointUse.values().filter { it.name != "NULL" }.map {
           arrayOf(
-            it,
-            ContactPointUseCode.Value.valueOf(it.toCode().replace('-', '_').toUpperCase())
+            Enumeration(ContactPointUseEnumFactory(), it),
+            ContactPoint.UseCode.newBuilder()
+              .setValue(
+                ContactPointUseCode.Value.valueOf(it.toCode().replace('-', '_').toUpperCase())
+              )
+              .build()
           )
-        } +
-        DataAbsentReason.values().filter { it.name != "NULL" }.map {
-          arrayOf(
-            it,
-            DataAbsentReasonCode.Value.valueOf(it.toCode().replace('-', '_').toUpperCase())
-          )
-        } +
-        DaysOfWeek.values().filter { it.name != "NULL" }.map {
-          arrayOf(it, DaysOfWeekCode.Value.valueOf(it.toCode().replace('-', '_').toUpperCase()))
-        } +
-        // TODO handle this case
-
-        //        Enumerations.FHIRDefinedType.values().filter { it.name != "NULL" }.map {
-        //          arrayOf(it,
-        // FHIRDefinedTypeCode.Value.valueOf(it.toCode().replace('-','_').toUpperCase()))
-        //        } +
-        DiscriminatorType.values().filter { it.name != "NULL" }.map {
-          arrayOf(
-            it,
-            DiscriminatorTypeCode.Value.valueOf(it.toCode().replace('-', '_').toUpperCase())
-          )
-        } +
-        DocumentMode.values().filter { it.name != "NULL" }.map {
-          arrayOf(it, DocumentModeCode.Value.valueOf(it.toCode().replace('-', '_').toUpperCase()))
         }
     }
   }
