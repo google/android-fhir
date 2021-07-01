cce998ab7653e89d231d8fc36d6bf1badeed1259
epicadk <adityakurkure@gmail.com>
2021-07-01 15:09:21 +0530

    Run spotless

--- hapiprotoconverter/src/main/java/com/google/android/fhir/hapiprotoconverter/PrimitiveCodegenMain.kt
+++ hapiprotoconverter/src/main/java/com/google/android/fhir/hapiprotoconverter/PrimitiveCodegenMain.kt
@@ -1,35 +1,60 @@
+/*
+ * Copyright 2020 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.google.android.fhir.hapiprotoconverter
 
 import com.google.fhir.r4.core.Id
 import com.google.fhir.r4.core.StructureDefinition
+import com.google.fhir.r4.core.StructureDefinitionKindCode
 import java.io.File
 
 fun main() {
-    for (x in listOf(
-
-        "base64Binary",
-        "boolean",
-        "canonical",
-        "code",
-        "date",
-        "dateTime",
-         "decimal",
-          "id",
-        "instant",
-        "integer",
-        "markdown",
-        "oid",
-        "positiveInt",
-        "string",
-        "time",
-        "unsignedInt",
-        "uri",
-        "url",
-        "uuid"
-
+  for (x in
+    listOf(
+      "base64Binary",
+      "boolean",
+      "canonical",
+      "code",
+      "date",
+      "dateTime",
+      "decimal",
+      "id",
+      "instant",
+      "integer",
+      "markdown",
+      "oid",
+      "positiveInt",
+      "string",
+      "time",
+      "unsignedInt",
+      "uri",
+      "url",
+      "uuid"
     )) {
-        val file =
-            File("hapiprotoconverter\\src\\main\\java")
-        PrimitiveCodegen.generate(StructureDefinition.newBuilder().setId(Id.newBuilder().setValue(x)).build(),outLocation = file)
-    }
+    val file = File("hapiprotoconverter\\src\\main\\java")
+    // This is temp will parse files
+    PrimitiveCodegen.generate(
+      StructureDefinition.newBuilder()
+        .setId(Id.newBuilder().setValue(x))
+        .setKind(
+          StructureDefinition.KindCode.newBuilder()
+            .setValue(StructureDefinitionKindCode.Value.PRIMITIVE_TYPE)
+        )
+        .build(),
+      outLocation = file
+    )
+  }
 }
--- hapiprotoconverter/src/main/java/com/google/android/fhir/hapiprotoconverter/generated/DateConverter.kt
+++ hapiprotoconverter/src/main/java/com/google/android/fhir/hapiprotoconverter/generated/DateConverter.kt
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2020 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.google.android.fhir.hapiprotoconverter.generated
 
 import ca.uhn.fhir.model.api.TemporalPrecisionEnum
@@ -8,11 +24,12 @@ import org.hl7.fhir.r4.model.DateType
 
 public object DateConverter {
   public fun DateType.toProto(): Date {
-    val protoValue = Date.newBuilder()
-    .setTimezone(timeZone.id)
-    .setValueUs(value.time)
-    .setPrecision(precision.toProtoPrecision())
-    .build()
+    val protoValue =
+      Date.newBuilder()
+        .setTimezone(timeZone.id)
+        .setValueUs(value.time)
+        .setPrecision(precision.toProtoPrecision())
+        .build()
     return protoValue
   }
 
@@ -23,17 +40,19 @@ public object DateConverter {
     return hapiValue
   }
 
-  private fun TemporalPrecisionEnum.toProtoPrecision(): Date.Precision = when(this) {
-    TemporalPrecisionEnum.YEAR -> Date.Precision.YEAR
-    TemporalPrecisionEnum.MONTH -> Date.Precision.MONTH
-    TemporalPrecisionEnum.DAY -> Date.Precision.DAY
-    else ->Date.Precision.PRECISION_UNSPECIFIED
-  }
+  private fun TemporalPrecisionEnum.toProtoPrecision(): Date.Precision =
+    when (this) {
+      TemporalPrecisionEnum.YEAR -> Date.Precision.YEAR
+      TemporalPrecisionEnum.MONTH -> Date.Precision.MONTH
+      TemporalPrecisionEnum.DAY -> Date.Precision.DAY
+      else -> Date.Precision.PRECISION_UNSPECIFIED
+    }
 
-  private fun Date.Precision.toHapiPrecision(): TemporalPrecisionEnum = when(this) {
-    Date.Precision.YEAR -> TemporalPrecisionEnum.YEAR
-    Date.Precision.MONTH -> TemporalPrecisionEnum.MONTH
-    Date.Precision.DAY -> TemporalPrecisionEnum.DAY
-    else ->TemporalPrecisionEnum.MILLI
-  }
+  private fun Date.Precision.toHapiPrecision(): TemporalPrecisionEnum =
+    when (this) {
+      Date.Precision.YEAR -> TemporalPrecisionEnum.YEAR
+      Date.Precision.MONTH -> TemporalPrecisionEnum.MONTH
+      Date.Precision.DAY -> TemporalPrecisionEnum.DAY
+      else -> TemporalPrecisionEnum.MILLI
+    }
 }
