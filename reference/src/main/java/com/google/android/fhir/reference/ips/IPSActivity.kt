83e66af2a128480190be8124538fbbddb339788f
Anushka Chakraborty <anushkachakra@gmail.com>
2021-07-09 17:41:19 +0530

    shift fragments to activity using nav graph different

--- reference/src/main/java/com/google/android/fhir/reference/ips/IPSActivity.kt
+++ reference/src/main/java/com/google/android/fhir/reference/ips/IPSActivity.kt
@@ -9,6 +9,5 @@ class IPSActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_ips)
-
     }
 }
