04790f696390ecc91b57f11ab85f069988809a30
maanuanubha999 <maanuanubhav999@gmail.com>
2021-07-26 12:06:26 +0530

    add test

---
+++ datacapture/src/androidTest/java/com/google/android/fhir/datacapture/views/QuestionnaireItemBooleanTypePickerViewHolderFactoryInstrumentedTest.kt
@@ -0,0 +1,141 @@
+/*
+ * Copyright 2020 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.google.android.fhir.datacapture.views
+
+import android.widget.FrameLayout
+import android.widget.RadioButton
+import android.widget.TextView
+import androidx.core.view.isVisible
+import androidx.test.annotation.UiThreadTest
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.platform.app.InstrumentationRegistry
+import com.google.android.fhir.datacapture.R
+import com.google.common.truth.Truth
+import org.hl7.fhir.r4.model.Questionnaire
+import org.hl7.fhir.r4.model.QuestionnaireResponse
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)
+class QuestionnaireItemBooleanTypePickerViewHolderFactoryInstrumentedTest {
+  private val parent = FrameLayout(InstrumentationRegistry.getInstrumentation().context)
+  private val viewHolder = QuestionnaireItemBooleanTypePickerViewHolderFactory.create(parent)
+
+  @Test
+  fun shouldShowPrefixText() {
+    viewHolder.bind(
+      QuestionnaireItemViewItem(
+        Questionnaire.QuestionnaireItemComponent().apply { prefix = "Prefix?" },
+        QuestionnaireResponse.QuestionnaireResponseItemComponent()
+      ) {}
+    )
+
+    Truth.assertThat(viewHolder.itemView.findViewById<TextView>(R.id.prefix).isVisible).isTrue()
+    Truth.assertThat(viewHolder.itemView.findViewById<TextView>(R.id.prefix).text)
+      .isEqualTo("Prefix?")
+  }
+
+  @Test
+  fun shouldHidePrefixText() {
+    viewHolder.bind(
+      QuestionnaireItemViewItem(
+        Questionnaire.QuestionnaireItemComponent().apply { prefix = "" },
+        QuestionnaireResponse.QuestionnaireResponseItemComponent()
+      ) {}
+    )
+
+    Truth.assertThat(viewHolder.itemView.findViewById<TextView>(R.id.prefix).isVisible).isFalse()
+  }
+
+  @Test
+  fun bind_shouldSetHeaderText() {
+    viewHolder.bind(
+      QuestionnaireItemViewItem(
+        Questionnaire.QuestionnaireItemComponent().apply {
+          repeats = true
+          text = "Question?"
+        },
+        QuestionnaireResponse.QuestionnaireResponseItemComponent()
+      ) {}
+    )
+
+    Truth.assertThat(viewHolder.itemView.findViewById<TextView>(R.id.bool_header).text)
+      .isEqualTo("Question?")
+  }
+
+  @Test
+  fun bind_shouldCreateRadioButtonsForBooleanType() {
+    viewHolder.bind(
+      QuestionnaireItemViewItem(
+        Questionnaire.QuestionnaireItemComponent().apply { text = "Question?" },
+        QuestionnaireResponse.QuestionnaireResponseItemComponent()
+      ) {}
+    )
+
+    Truth.assertThat(viewHolder.itemView.findViewById<TextView>(R.id.bool_header).text)
+      .isEqualTo("Questiond")
+    Truth.assertThat(
+        viewHolder.itemView.findViewById<RadioButton>(R.id.boolean_type_yes).isClickable
+      )
+      .isTrue()
+  }
+
+  @Test
+  @UiThreadTest
+  fun click_shouldSetAnswerTrue() {
+    val questionnaireItemViewItem =
+      QuestionnaireItemViewItem(
+        Questionnaire.QuestionnaireItemComponent().apply { text = "Question?" },
+        QuestionnaireResponse.QuestionnaireResponseItemComponent()
+      ) {}
+    viewHolder.bind(questionnaireItemViewItem)
+    viewHolder.itemView.findViewById<RadioButton>(R.id.boolean_type_yes).performClick()
+
+    val answer = questionnaireItemViewItem.questionnaireResponseItem.answer
+    Truth.assertThat(answer[0].valueBooleanType.value).isTrue()
+  }
+
+  @Test
+  @UiThreadTest
+  fun click_shouldSetAnswerFalse() {
+    val questionnaireItemViewItem =
+      QuestionnaireItemViewItem(
+        Questionnaire.QuestionnaireItemComponent().apply { text = "Question?" },
+        QuestionnaireResponse.QuestionnaireResponseItemComponent()
+      ) {}
+    viewHolder.bind(questionnaireItemViewItem)
+    viewHolder.itemView.findViewById<RadioButton>(R.id.boolean_type_no).performClick()
+
+    val answer = questionnaireItemViewItem.questionnaireResponseItem.answer
+    Truth.assertThat(answer[0].valueBooleanType.value).isFalse()
+  }
+
+  @Test
+  @UiThreadTest
+  fun click_shouldClearAnswer() {
+    val questionnaireItemViewItem =
+      QuestionnaireItemViewItem(
+        Questionnaire.QuestionnaireItemComponent().apply { text = "Question?" },
+        QuestionnaireResponse.QuestionnaireResponseItemComponent()
+      ) {}
+    viewHolder.bind(questionnaireItemViewItem)
+    viewHolder.itemView.findViewById<RadioButton>(R.id.boolean_type_empty).performClick()
+
+    val answer = questionnaireItemViewItem.questionnaireResponseItem.answer
+    Truth.assertThat(answer[0].isEmpty)
+  }
+}
--- datacapture/src/main/java/com/google/android/fhir/datacapture/views/QuestionnaireItemBooleanTypePickerViewHolderFactory.kt
+++ datacapture/src/main/java/com/google/android/fhir/datacapture/views/QuestionnaireItemBooleanTypePickerViewHolderFactory.kt
@@ -1,6 +1,21 @@
+/*
+ * Copyright 2020 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.google.android.fhir.datacapture.views
 
-import android.util.Log
 import android.view.View
 import android.widget.RadioButton
 import android.widget.RadioGroup
@@ -11,65 +26,65 @@ import com.google.android.fhir.datacapture.localizedText
 import org.hl7.fhir.r4.model.BooleanType
 import org.hl7.fhir.r4.model.QuestionnaireResponse
 
+internal object QuestionnaireItemBooleanTypePickerViewHolderFactory :
+  QuestionnaireItemViewHolderFactory(R.layout.questionnaire_item_boolean_type_picker_view) {
+  override fun getQuestionnaireItemViewHolderDelegate() =
+    object : QuestionnaireItemViewHolderDelegate {
+      private lateinit var prefixTextView: TextView
+      private lateinit var questionnaireItemViewItem: QuestionnaireItemViewItem
+      private lateinit var boolTypeHeader: TextView
+      private lateinit var boolTypeYes: RadioButton
+      private lateinit var boolTypeNo: RadioButton
+      private lateinit var boolTypeEmpty: RadioButton
+      private lateinit var radioGroup: RadioGroup
 
-internal object QuestionnaireItemBooleanTypePickerViewHolderFactory: QuestionnaireItemViewHolderFactory(R.layout.questionnaire_item_boolean_type_picker_view) {
-  override fun getQuestionnaireItemViewHolderDelegate()= object : QuestionnaireItemViewHolderDelegate {
-    private lateinit var prefixTextView: TextView
-    private lateinit var questionnaireItemViewItem: QuestionnaireItemViewItem
-    private lateinit var boolTypeHeader : TextView
-    private lateinit var boolTypeYes: RadioButton
-    private lateinit var boolTypeNo: RadioButton
-    private lateinit var boolTypeEmpty: RadioButton
-    private lateinit var radioGroup: RadioGroup
-
-
-    override fun init(itemView: View) {
-      boolTypeYes = itemView.findViewById(R.id.boolean_type_yes)
-      boolTypeNo = itemView.findViewById(R.id.boolean_type_no)
-      boolTypeEmpty = itemView.findViewById(R.id.boolean_type_empty)
-      prefixTextView = itemView.findViewById(R.id.prefix)
-      radioGroup = itemView.findViewById(R.id.radio_group_main)
-      boolTypeHeader = itemView.findViewById(R.id.bool_header)
-
-    }
-
-    override fun bind(questionnaireItemViewItem: QuestionnaireItemViewItem) {
-      this.questionnaireItemViewItem = questionnaireItemViewItem
-
-      if (!questionnaireItemViewItem.questionnaireItem.prefix.isNullOrEmpty()) {
-        prefixTextView.visibility = View.VISIBLE
-        prefixTextView.text = questionnaireItemViewItem.questionnaireItem.localizedPrefix
-      } else {
-        prefixTextView.visibility = View.GONE
+      override fun init(itemView: View) {
+        boolTypeYes = itemView.findViewById(R.id.boolean_type_yes)
+        boolTypeNo = itemView.findViewById(R.id.boolean_type_no)
+        boolTypeEmpty = itemView.findViewById(R.id.boolean_type_empty)
+        prefixTextView = itemView.findViewById(R.id.prefix)
+        radioGroup = itemView.findViewById(R.id.radio_group_main)
+        boolTypeHeader = itemView.findViewById(R.id.bool_header)
       }
-      val (questionnaireItem, questionnaireResponseItem) = questionnaireItemViewItem
-      val answer = questionnaireResponseItem.answer.singleOrNull()?.valueCoding
-      boolTypeHeader.text = questionnaireItem.localizedText
 
+      override fun bind(questionnaireItemViewItem: QuestionnaireItemViewItem) {
+        this.questionnaireItemViewItem = questionnaireItemViewItem
 
-      radioGroup.setOnCheckedChangeListener { _, i ->
-
-        when (i) {
-          R.id.boolean_type_yes -> {
-            questionnaireResponseItem.answer.clear()
-            questionnaireResponseItem.answer.add(QuestionnaireResponse.QuestionnaireResponseItemAnswerComponent().apply {
-              value = BooleanType(true)
-            })
-          }
-          R.id.boolean_type_no -> {
-            questionnaireResponseItem.answer.clear()
-            questionnaireResponseItem.answer.add(QuestionnaireResponse.QuestionnaireResponseItemAnswerComponent().apply {
-              value = BooleanType(false)
-            })
-          }
-          R.id.boolean_type_empty -> {
-            questionnaireResponseItem.answer.clear()
-          }
+        if (!questionnaireItemViewItem.questionnaireItem.prefix.isNullOrEmpty()) {
+          prefixTextView.visibility = View.VISIBLE
+          prefixTextView.text = questionnaireItemViewItem.questionnaireItem.localizedPrefix
+        } else {
+          prefixTextView.visibility = View.GONE
         }
+        val (questionnaireItem, questionnaireResponseItem) = questionnaireItemViewItem
+        val answer = questionnaireResponseItem.answer.singleOrNull()?.valueCoding
+        boolTypeHeader.text = questionnaireItem.localizedText
 
-        questionnaireItemViewItem.questionnaireResponseItemChangedCallback()
+        radioGroup.setOnCheckedChangeListener { _, i ->
+          when (i) {
+            R.id.boolean_type_yes -> {
+              questionnaireResponseItem.answer.clear()
+              questionnaireResponseItem.answer.add(
+                QuestionnaireResponse.QuestionnaireResponseItemAnswerComponent().apply {
+                  value = BooleanType(true)
+                }
+              )
+            }
+            R.id.boolean_type_no -> {
+              questionnaireResponseItem.answer.clear()
+              questionnaireResponseItem.answer.add(
+                QuestionnaireResponse.QuestionnaireResponseItemAnswerComponent().apply {
+                  value = BooleanType(false)
+                }
+              )
+            }
+            R.id.boolean_type_empty -> {
+              questionnaireResponseItem.answer.clear()
+            }
+          }
 
+          questionnaireItemViewItem.questionnaireResponseItemChangedCallback()
+        }
       }
     }
-  }
 }
