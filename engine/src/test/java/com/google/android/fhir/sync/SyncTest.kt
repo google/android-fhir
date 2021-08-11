fe7e238a8edb34615f3588514174844b5e4e8884
maimoonak <4829880+maimoonak@users.noreply.github.com>
2021-08-11 19:09:14 +0500

    554 Flow API for one-time and periodic tasks (#648)
    * Polling with given delay implemented
    
    * Flow for FhirSyncronizer
    
    * Flow for FhirSyncronizer states and tests
    
    * Testing work manager
    
    * Working WorkManager with flow
    
    * spotless-apply
    
    * Resolving feedback on PR
    
    * test and update dependencies
    
    * Resolved feedback and added the toast on ui as well
    
    * Resolved comments
    
    * Testing WorkInfo with containsAtLeast
    
    * FhirWorkerTest fix

--- engine/src/test/java/com/google/android/fhir/sync/SyncTest.kt
+++ engine/src/test/java/com/google/android/fhir/sync/SyncTest.kt
@@ -20,16 +20,9 @@ import android.content.Context
 import androidx.work.BackoffPolicy
 import androidx.work.WorkerParameters
 import com.google.android.fhir.FhirEngine
-import com.google.android.fhir.SyncDownloadContext
-import com.google.android.fhir.db.impl.dao.LocalChangeToken
-import com.google.android.fhir.db.impl.dao.SquashedLocalChange
-import com.google.android.fhir.search.Search
+import com.google.android.fhir.resource.TestingUtils
 import com.google.common.truth.Truth.assertThat
 import java.util.concurrent.TimeUnit
-import org.hl7.fhir.r4.model.Bundle
-import org.hl7.fhir.r4.model.Observation
-import org.hl7.fhir.r4.model.OperationOutcome
-import org.hl7.fhir.r4.model.Resource
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.robolectric.RobolectricTestRunner
@@ -40,62 +33,9 @@ import org.robolectric.annotation.Config
 class SyncTest {
   class PassingPeriodicSyncWorker(appContext: Context, workerParams: WorkerParameters) :
     FhirSyncWorker(appContext, workerParams) {
-    private var engine =
-      object : FhirEngine {
-        override suspend fun <R : Resource> save(vararg resource: R) {}
 
-        override suspend fun <R : Resource> update(resource: R) {}
-
-        override suspend fun <R : Resource> load(clazz: Class<R>, id: String): R {
-          return clazz.newInstance()
-        }
-
-        override suspend fun <R : Resource> remove(clazz: Class<R>, id: String) {}
-
-        override suspend fun <R : Resource> search(search: Search): List<R> {
-          return emptyList()
-        }
-
-        override suspend fun syncUpload(
-          upload: suspend (List<SquashedLocalChange>) -> List<LocalChangeToken>
-        ) {}
-
-        override suspend fun syncDownload(
-          download: suspend (SyncDownloadContext) -> List<Resource>
-        ) {}
-
-        override suspend fun count(search: Search): Long {
-          return 0
-        }
-      }
-    private var source =
-      object : DataSource {
-        override suspend fun loadData(path: String): Bundle {
-          return Bundle()
-        }
-
-        override suspend fun insert(
-          resourceType: String,
-          resourceId: String,
-          payload: String
-        ): Resource {
-          return Observation()
-        }
-
-        override suspend fun update(
-          resourceType: String,
-          resourceId: String,
-          payload: String
-        ): OperationOutcome {
-          return OperationOutcome()
-        }
-
-        override suspend fun delete(resourceType: String, resourceId: String): OperationOutcome {
-          return OperationOutcome()
-        }
-      }
-    override fun getFhirEngine(): FhirEngine = engine
-    override fun getDataSource(): DataSource = source
+    override fun getFhirEngine(): FhirEngine = TestingUtils.TestFhirEngineImpl
+    override fun getDataSource(): DataSource = TestingUtils.TestDataSourceImpl
     override fun getSyncData(): ResourceSyncParams = mapOf()
   }
 
