75a50060afc2a6c451335011108b19c193e9feda
stevenckngaa <68665119+stevenckngaa@users.noreply.github.com>
2021-09-15 23:30:06 +0100

    Apply spotless

--- engine/src/main/java/com/google/android/fhir/security/StorageKeyProvider.kt
+++ engine/src/main/java/com/google/android/fhir/security/StorageKeyProvider.kt
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2020 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.google.android.fhir.security
 
 import android.content.Context
@@ -21,13 +37,14 @@ object StorageKeyProvider {
     keyLength: Int = STORAGE_KEY_LENGTH
   ): ByteArray {
     val mainKeyAlias = MasterKeys.getOrCreate(MasterKeys.AES256_GCM_SPEC)
-    val encryptedSharedPreferences = EncryptedSharedPreferences.create(
-      STORAGE_KEY_PREFERENCES_NAME,
-      mainKeyAlias,
-      context,
-      EncryptedSharedPreferences.PrefKeyEncryptionScheme.AES256_SIV,
-      EncryptedSharedPreferences.PrefValueEncryptionScheme.AES256_GCM
-    )
+    val encryptedSharedPreferences =
+      EncryptedSharedPreferences.create(
+        STORAGE_KEY_PREFERENCES_NAME,
+        mainKeyAlias,
+        context,
+        EncryptedSharedPreferences.PrefKeyEncryptionScheme.AES256_SIV,
+        EncryptedSharedPreferences.PrefValueEncryptionScheme.AES256_GCM
+      )
     encryptedSharedPreferences.getString(passphraseName, null)?.decodeHex()?.let {
       return it
     }
@@ -59,7 +76,5 @@ fun ByteArray.toHexString() = joinToString("") { "%02x".format(it) }
 
 fun String.decodeHex(): ByteArray {
   check(length % 2 == 0) { "Must have an even length" }
-  return chunked(2)
-    .map { it.toInt(16).toByte() }
-    .toByteArray()
+  return chunked(2).map { it.toInt(16).toByte() }.toByteArray()
 }
