8af3f2df73c9fd9335d00f5f9d2e17f829950e63
Deepankar Bhardwaj <bdeepankar@google.com>
2021-09-23 11:05:35 +0530

    Remove the Java Junit Jacoco plugin
    * Plugin was not being used due to misconfiguration in ci.yml
    * Plugin generated reports are different from AGP invoked jacoco reports, needs further investigation
    * Excluded reference module from coverage

--- reference/build.gradle.kts
+++ reference/build.gradle.kts
@@ -23,7 +23,6 @@ android {
       isMinifyEnabled = false
       proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
     }
-    getByName("debug") { isTestCoverageEnabled = true }
   }
   buildFeatures { viewBinding = true }
   compileOptions {
