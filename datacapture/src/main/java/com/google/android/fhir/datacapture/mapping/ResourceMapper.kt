5aa6b1228079cf63b4796fbfb3cbf4b68cb7be14
santosh-pingle <santosh.pingle.21@gmail.com>
2021-09-24 17:42:05 +0530

    Refactoring : Unify group type check in ResourceMapper extractField method.

--- datacapture/src/main/java/com/google/android/fhir/datacapture/mapping/ResourceMapper.kt
+++ datacapture/src/main/java/com/google/android/fhir/datacapture/mapping/ResourceMapper.kt
@@ -235,59 +235,65 @@ object ResourceMapper {
     questionnaireResponseItem: QuestionnaireResponse.QuestionnaireResponseItemComponent
   ) {
     if (questionnaireItem.type == Questionnaire.QuestionnaireItemType.GROUP) {
-      if (questionnaireItem.itemContextNameToExpressionMap.values.isNotEmpty()) {
-        val extractedResource =
-          (Class.forName(
-              "org.hl7.fhir.r4.model.${questionnaireItem.itemContextNameToExpressionMap.values.first()}"
-            )
-            .newInstance() as
-            Base)
-        if (extractedResource is Resource) {
-          extractedResource.apply {
+      extractResource(bundle, questionnaireItem, questionnaireResponseItem)
+      if (questionnaireItem.definition == null) {
+        extractFields(bundle, questionnaireItem.item, questionnaireResponseItem.item)
+        return
+      }
+      val targetFieldName = questionnaireItem.definitionFieldName ?: return
+      if (targetFieldName.isEmpty()) {
+        return
+      }
+      val definitionField = questionnaireItem.getDefinitionField ?: return
+      if (questionnaireItem.isChoiceElement(choiceTypeFieldIndex = 1)) {
+        val value: Base =
+          questionnaireItem.createBase().apply {
             extractFields(bundle, questionnaireItem.item, questionnaireResponseItem.item)
           }
-          bundle.apply { addEntry().apply { resource = extractedResource as Resource } }
-        }
+        updateField(definitionField, value)
+        return
       }
+      val value: Base =
+        (definitionField.nonParameterizedType.newInstance() as Base).apply {
+          extractFields(bundle, questionnaireItem.item, questionnaireResponseItem.item)
+        }
+      updateField(definitionField, value)
+      return
     }
+
     if (questionnaireItem.definition == null) {
       extractFields(bundle, questionnaireItem.item, questionnaireResponseItem.item)
       return
     }
-
     val targetFieldName = questionnaireItem.definitionFieldName ?: return
     if (targetFieldName.isEmpty()) {
       return
     }
-
     val definitionField = questionnaireItem.getDefinitionField ?: return
-    if (questionnaireItem.type == Questionnaire.QuestionnaireItemType.GROUP) {
-      if (questionnaireItem.isChoiceElement(choiceTypeFieldIndex = 1)) {
-        val value: Base =
-          (Class.forName(
-                "org.hl7.fhir.r4.model.${questionnaireItem.itemContextNameToExpressionMap.values.first()}"
-              )
-              .newInstance() as
-              Base)
-            .apply { extractFields(bundle, questionnaireItem.item, questionnaireResponseItem.item) }
-        updateField(definitionField, value)
-      } else {
-        val value: Base =
-          (definitionField.nonParameterizedType.newInstance() as Base).apply {
-            extractFields(bundle, questionnaireItem.item, questionnaireResponseItem.item)
-          }
-        updateField(definitionField, value)
-      }
-    } else {
-      if (questionnaireResponseItem.answer.isEmpty()) return
-      if (!definitionField.nonParameterizedType.isEnum) {
-        // this is a low level type e.g. StringType
-        updateField(definitionField, questionnaireResponseItem.answer)
-      } else {
-        // this is a high level type e.g. AdministrativeGender
-        updateFieldWithEnum(definitionField, questionnaireResponseItem.answer.first().value)
-      }
+    if (questionnaireResponseItem.answer.isEmpty()) return
+    if (!definitionField.nonParameterizedType.isEnum) {
+      // this is a low level type e.g. StringType
+      updateField(definitionField, questionnaireResponseItem.answer)
+      return
     }
+    // this is a high level type e.g. AdministrativeGender
+    updateFieldWithEnum(definitionField, questionnaireResponseItem.answer.first().value)
+  }
+
+  private suspend fun extractResource(
+    bundle: Bundle,
+    questionnaireItem: Questionnaire.QuestionnaireItemComponent,
+    questionnaireResponseItem: QuestionnaireResponse.QuestionnaireResponseItemComponent
+  ) {
+    if (questionnaireItem.itemContextNameToExpressionMap.values.isEmpty()) {
+      return
+    }
+    val resource = questionnaireItem.createBase()
+    if (resource !is Resource) {
+      return
+    }
+    resource.apply { extractFields(bundle, questionnaireItem.item, questionnaireResponseItem.item) }
+    bundle.apply { addEntry().apply { this.resource = resource } }
   }
 }
 
@@ -535,6 +541,11 @@ private fun Questionnaire.QuestionnaireItemComponent.getFieldOrNull(
     resourceClass.getFieldOrNull(path[fieldIndex])
   }
 }
+
+private fun Questionnaire.QuestionnaireItemComponent.createBase(): Base =
+  (Class.forName("org.hl7.fhir.r4.model.${itemContextNameToExpressionMap.values.first()}")
+    .newInstance() as
+    Base)
 
 /**
  * See
