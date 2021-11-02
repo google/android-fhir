f657d1e7b29c7538680dd47e9a1be4afe702499b
Aditya Kurkure <56596662+epicadk@users.noreply.github.com>
2021-11-02 09:30:09 +0530

    Add gradle task to generate code at runtime (#851)
    * add gradle task to generate code at runtime
    
    * run before engine build rather than codegen build
    
    * run spotless after generating file.
    
    Co-authored-by: Jing Tang <jingtang@google.com>

--- codegen/build.gradle.kts
+++ codegen/build.gradle.kts
@@ -13,3 +13,17 @@ dependencies {
 
   implementation(Dependencies.kotlinPoet)
 }
+
+tasks.create<JavaExec>("runCodeGenerator") {
+  group = "kotlinpoet"
+  classpath = sourceSets.getByName("main").runtimeClasspath
+  mainClass.set("com.google.android.fhir.codegen.SearchParameterRepositoryGeneratorKt")
+  run {
+    args =
+      mutableListOf(
+        project.rootDir.absolutePath + "/codegen/src/main/res/search-parameters.json",
+        project.rootDir.absolutePath + "/engine/src/main/java"
+      )
+  }
+  finalizedBy(":spotlessGenerated")
+}
