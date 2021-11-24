a1f65ba29ee458a15099234344e8abc9ba5c3474
Anita Vadverao <anita.vadverao@gmail.com>
2021-11-24 06:37:37 +0000

    FHIR-Engine:API Unit Test: Save, Update and Remove Patient

--- engine/src/test/java/com/google/android/fhir/impl/FhirEngineImplTest.kt
+++ engine/src/test/java/com/google/android/fhir/impl/FhirEngineImplTest.kt
@@ -17,10 +17,12 @@
 package com.google.android.fhir.impl
 
 import androidx.test.core.app.ApplicationProvider
+import ca.uhn.fhir.context.FhirContext
 import com.google.android.fhir.FhirServices.Companion.builder
 import com.google.android.fhir.db.ResourceNotFoundException
 import com.google.android.fhir.db.impl.dao.SquashedLocalChange
 import com.google.android.fhir.db.impl.entities.LocalChangeEntity
+import com.google.android.fhir.logicalId
 import com.google.android.fhir.resource.TestingUtils
 import com.google.common.truth.Truth.assertThat
 import java.util.Date
@@ -112,7 +114,7 @@ class FhirEngineImplTest {
 
   @Test
   fun syncUpload_uploadLocalChange() = runBlocking {
-    var localChanges = mutableListOf<SquashedLocalChange>()
+    val localChanges = mutableListOf<SquashedLocalChange>()
     fhirEngine.syncUpload { it ->
       localChanges.addAll(it)
       return@syncUpload it.map { it.token }
@@ -137,6 +139,60 @@ class FhirEngineImplTest {
       TEST_PATIENT_2,
       fhirEngine.load(Patient::class.java, TEST_PATIENT_2_ID)
     )
+  }
+  @Test
+  fun save_shouldSavePatientResource() = runBlocking {
+    val patient: Patient = testingUtils.readFromFile(Patient::class.java, "/New_patient.json")
+    fhirEngine.save(patient)
+    val json = FhirContext.forR4().newJsonParser().encodeResourceToString(patient)
+    println(json)
+
+    testingUtils.assertResourceEquals(
+      patient,
+      fhirEngine.load(Patient::class.java, patient.logicalId)
+    )
+    val loadedPatient = fhirEngine.load(Patient::class.java, patient.logicalId)
+    assertThat(loadedPatient.name[0].family).isEqualTo("Ross")
+  }
+
+  @Test
+  fun updateShouldReturnUpdatedResource() = runBlocking {
+    val patient: Patient = testingUtils.readFromFile(Patient::class.java, "/New_patient.json")
+    fhirEngine.save(patient)
+    val newPatientJson = FhirContext.forR4().newJsonParser().encodeResourceToString(patient)
+    println(newPatientJson)
+    val updatedPatient: Patient =
+      testingUtils.readFromFile(Patient::class.java, "/Update_patient.json")
+    fhirEngine.update(updatedPatient)
+    val updatedPatientJson =
+      FhirContext.forR4().newJsonParser().encodeResourceToString(updatedPatient)
+    print(updatedPatientJson)
+
+    testingUtils.assertResourceEquals(
+      updatedPatient,
+      fhirEngine.load(Patient::class.java, updatedPatient.logicalId)
+    )
+    val loadedPatient = fhirEngine.load(Patient::class.java, updatedPatient.logicalId)
+    assertThat(loadedPatient.name[0].family).isEqualTo("Spector")
+    assertThat(loadedPatient.nameFirstRep.given.first().valueAsString).isEqualTo("Michel")
+  }
+
+  @Test
+  fun remove_shouldDeleteResource() = runBlocking {
+    val patient: Patient = testingUtils.readFromFile(Patient::class.java, "/New_patient.json")
+    fhirEngine.save(patient)
+    val newPatientJson = FhirContext.forR4().newJsonParser().encodeResourceToString(patient)
+    println(newPatientJson)
+    fhirEngine.remove(Patient::class.java, patient.logicalId)
+
+    val resourceNotFoundException =
+      assertThrows(ResourceNotFoundException::class.java) {
+        runBlocking { fhirEngine.load(Patient::class.java, patient.logicalId) }
+      }
+    assertThat(resourceNotFoundException.message)
+      .isEqualTo(
+        "Resource not found with type ${ResourceType.Patient.name} and id ${patient.logicalId}!"
+      )
   }
 
   companion object {
