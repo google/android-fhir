881acf2ba8ded074359ad9bafeef714358e5642e
Fikri Milano <fikrimilano1@gmail.com>
2021-12-28 19:15:40 +0700

    Add custom style attributes (#633)
    * Add custom style for Group Header questionnaire
    
    * Add custom style for Single CheckBox and CheckBox Group questionnaire
    
    * Change style name for Group Header
    
    * Clean up
    
    * Add custom style for CheckBox Group header
    
    * Move to data capture gallery
    
    * Add custom style for Radio Button questionnaire
    
    * Run spotlessApply
    
    * Add custom style for Radio Group header
    
    * Remove unused code
    
    * Add custom style for DropDown questionnaire
    
    * Add custom style for DropDown header
    
    * Add custom style for DropDown selected
    
    * Add custom style for DropDown widget
    
    * Add custom style for EditText header
    
    * Add custom style for EditText layout
    
    * Add custom style for EditText input
    
    * Add custom style for AutoComplete dropdown list
    
    * Add custom style for AutoComplete text input
    
    * Add custom style for AutoComplete text input layout
    
    * Add custom style for AutoComplete header
    
    * Add custom style for AutoComplete chip
    
    * Add custom style for Display type
    
    * Add custom style for DatePicker type
    
    * Add custom style for DateTimePicker type
    
    * Use only 1 header TextAppearance
    
    * Add header style for slider
    
    * Re-name styles
    
    * Add custom style for prefix
    
    * spotlessApply
    
    * Add more attributes
    
    * Move style to top
    
    * Remove unused resource
    
    * Support dark theme
    
    Co-authored-by: Jing Tang <jing.t86@gmail.com>

--- datacapture/src/main/java/com/google/android/fhir/datacapture/views/QuestionnaireItemAutoCompleteViewHolderFactory.kt
+++ datacapture/src/main/java/com/google/android/fhir/datacapture/views/QuestionnaireItemAutoCompleteViewHolderFactory.kt
@@ -149,7 +149,7 @@ internal object QuestionnaireItemAutoCompleteViewHolderFactory :
         val adapter =
           ArrayAdapter(
             chipContainer.context,
-            android.R.layout.simple_dropdown_item_1line,
+            R.layout.questionnaire_item_drop_down_list,
             answerOptionString
           )
         autoCompleteTextView.setAdapter(adapter)
@@ -216,7 +216,7 @@ internal object QuestionnaireItemAutoCompleteViewHolderFactory :
       ): Boolean {
         if (chipIsAlreadyPresent(answer)) return false
 
-        val chip = Chip(chipContainer.context)
+        val chip = Chip(chipContainer.context, null, R.attr.chipStyleQuestionnaire)
         chip.text = answer.valueCoding.display
         chip.isCloseIconVisible = true
         chip.isClickable = true
