c129e572ebe7607ca2112e47a922e4df20da63e6
santosh-pingle <santosh.pingle.21@gmail.com>
2022-01-03 10:54:09 +0530

    Update components and layouts vector icons.

--- datacapturegallery/src/main/java/com/google/android/fhir/datacapture/gallery/ComponentsLayoutsViewModel.kt
+++ datacapturegallery/src/main/java/com/google/android/fhir/datacapture/gallery/ComponentsLayoutsViewModel.kt
@@ -35,26 +35,26 @@ class ComponentsLayoutsViewModel(application: Application, private val state: Sa
 
   // TODO update components icons
   enum class Components(@DrawableRes val iconId: Int, @StringRes val textId: Int) {
-    MULTIPLE_CHOICE(R.drawable.ic_layout_icon, R.string.multiple_choice),
-    SINGLE_CHOICE(R.drawable.ic_components_icon, R.string.single_choice),
-    OPEN_CHOICE(R.drawable.ic_layout_icon, R.string.open_choice),
-    TEXT_FIELD(R.drawable.ic_components_icon, R.string.text_field),
-    DATE_PICKER(R.drawable.ic_layout_icon, R.string.date_picker),
-    TIME_PICKER(R.drawable.ic_components_icon, R.string.time_picker),
-    MODAL(R.drawable.ic_layout_icon, R.string.modal),
-    SLIDER(R.drawable.ic_components_icon, R.string.slider),
-    DROPDOWN(R.drawable.ic_layout_icon, R.string.dropdown),
-    IMAGE(R.drawable.ic_components_icon, R.string.image),
-    DATE_OF_BIRTH(R.drawable.ic_layout_icon, R.string.date_of_Birth),
-    DATE_RANGE_PICKER(R.drawable.ic_components_icon, R.string.date_range_picker),
-    UNIT_OPTIONS(R.drawable.ic_components_icon, R.string.unit_options),
+    MULTIPLE_CHOICE(R.drawable.ic_multiplechoice, R.string.multiple_choice),
+    SINGLE_CHOICE(R.drawable.ic_singlechoice, R.string.single_choice),
+    OPEN_CHOICE(R.drawable.ic_openchoice, R.string.open_choice),
+    TEXT_FIELD(R.drawable.ic_textfield, R.string.text_field),
+    DATE_PICKER(R.drawable.ic_datepicker, R.string.date_picker),
+    TIME_PICKER(R.drawable.ic_timepicker, R.string.time_picker),
+    MODAL(R.drawable.ic_modal, R.string.modal),
+    SLIDER(R.drawable.ic_slider, R.string.slider),
+    DROPDOWN(R.drawable.ic_group_1278, R.string.dropdown),
+    IMAGE(R.drawable.ic_image, R.string.image),
+    DATE_OF_BIRTH(R.drawable.ic_dateofbirth, R.string.date_of_Birth),
+    DATE_RANGE_PICKER(R.drawable.ic_rangepicker, R.string.date_range_picker),
+    UNIT_OPTIONS(R.drawable.ic_unitoptions, R.string.unit_options),
   }
 
   // TODO update layouts icons
   enum class SdcLayouts(@DrawableRes val iconId: Int, @StringRes val textId: Int) {
-    DEFAULT(R.drawable.ic_layout_icon, R.string.default_text),
-    PAGINATED(R.drawable.ic_components_icon, R.string.paginated),
-    REVIEW(R.drawable.ic_layout_icon, R.string.review),
-    READ_ONLY(R.drawable.ic_components_icon, R.string.read_only),
+    DEFAULT(R.drawable.ic_defaultlayout, R.string.default_text),
+    PAGINATED(R.drawable.ic_paginatedlayout, R.string.paginated),
+    REVIEW(R.drawable.ic_reviewlayout, R.string.review),
+    READ_ONLY(R.drawable.ic_readonlylayout, R.string.read_only),
   }
 }
