c6876e37e6263ffe2482bda0f37163c241176766
santosh-pingle <santosh.pingle.21@gmail.com>
2022-01-25 13:38:11 +0530

    slider component workflow.

--- catalog/src/main/java/com/google/android/fhir/catalog/ComponentListViewModel.kt
+++ catalog/src/main/java/com/google/android/fhir/catalog/ComponentListViewModel.kt
@@ -32,6 +32,7 @@ class ComponentListViewModel(application: Application, private val state: SavedS
   fun getQuestionnaire(component: Component): String {
     return when (component) {
       Component.BOOLEAN_CHOICE -> "single_choice_questionnaire_boolean.json"
+      Component.SLIDER -> "slider_questionnaire.json"
       else -> "" // TODO remove else when all components cases are added to the when.
     // https://github.com/google/android-fhir/issues/1076
     }
