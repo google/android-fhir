1f490c5f770d673e62df6f443c179dd314b27c1e
Aditya Khajuria <adityakhajuria@google.com>
2022-01-28 13:44:52 +0530

    spotless

--- engine/src/test/java/com/google/android/fhir/impl/FhirEngineImplTest.kt
+++ engine/src/test/java/com/google/android/fhir/impl/FhirEngineImplTest.kt
@@ -24,7 +24,6 @@ import com.google.android.fhir.db.impl.entities.LocalChangeEntity
 import com.google.android.fhir.resource.TestingUtils
 import com.google.common.truth.Truth.assertThat
 import java.util.Date
-import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.flow.flowOf
 import kotlinx.coroutines.runBlocking
 import org.hl7.fhir.r4.model.Enumerations
@@ -131,7 +130,7 @@ class FhirEngineImplTest {
 
   @Test
   fun syncDownload_downloadResources() = runBlocking {
-    fhirEngine.syncDownload { flowOf((listOf(TEST_PATIENT_2)))}
+    fhirEngine.syncDownload { flowOf((listOf(TEST_PATIENT_2))) }
 
     testingUtils.assertResourceEquals(
       TEST_PATIENT_2,
