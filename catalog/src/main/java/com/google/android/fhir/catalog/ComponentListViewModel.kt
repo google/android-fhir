55b9f1ed19894c309cc95c33690f528ad10e12da
santosh-pingle <86107848+santosh-pingle@users.noreply.github.com>
2022-02-08 22:00:27 +0530

    Catalog, Modal component workflow. (#1042)
    * Sdc gallery app landing page
    1. Show Bottom navigation bar with Components and Layouts menus.
    2. Implements components screen.
    3. Implements layouts screen.
    4. Handles bottom navigation clicks.
    5. Update actionbar.
    
    * Rename file name.
    
    * Update components and layouts vector icons.
    
    * Sdc gallery app single choice workflow.
    
    * Update it as boolean choice.
    
    * Address review comments.
    
    * Boolean choice component icon.
    
    * Spotless apply
    
    * Build failures
    
    * Delete gallery drawables.
    
    * fix package name in nav graph
    
    * Spotless apply.
    
    * Delete gallery color resource
    
    * theme attributes for color value.
    
    * bottom view margin
    
    * modal workflow.
    
    * Remove unwanted file.

--- catalog/src/main/java/com/google/android/fhir/catalog/ComponentListViewModel.kt
+++ catalog/src/main/java/com/google/android/fhir/catalog/ComponentListViewModel.kt
@@ -54,7 +54,7 @@ class ComponentListViewModel(application: Application, private val state: SavedS
       R.string.component_name_dropdown,
       "dropdown-questionnaire.json"
     ),
-    MODAL(R.drawable.ic_modal, R.string.component_name_modal, ""),
+    MODAL(R.drawable.ic_modal, R.string.component_name_modal, "modal-questionnaire.json"),
     OPEN_CHOICE(
       R.drawable.ic_openchoice,
       R.string.component_name_open_choice,
