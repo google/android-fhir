25ae6e770ec2a5834e9093095ea2bb5c4ceb4dc5
anshul <lci2020020@iiitl.ac.in>
2022-02-10 22:46:47 +0530

    Removed all parameterised quantities except number

--- engine/src/test/java/com/google/android/fhir/search/NumberSearch.kt
+++ engine/src/test/java/com/google/android/fhir/search/NumberSearch.kt
@@ -13,34 +13,17 @@ import com.google.common.truth.Truth.assertThat
 
 @RunWith(Parameterized::class)
 class NumberSearch(
-  private val num: BigDecimal,
-  private val expected1: String,
-  private val expected2: String
+  private val num: BigDecimal
 ) {
     companion object {
     @JvmStatic
     @Parameterized.Parameters
     fun data() : Collection<Array<Any>> {
-      val str=""" 
-        SELECT a.serializedResource
-        FROM ResourceEntity a
-        WHERE a.resourceType = ?
-        AND a.resourceId IN (
-        SELECT resourceId FROM NumberIndexEntity
-        WHERE resourceType = ? AND index_name = ? AND index_value <= ?
-        )
-        """.trimIndent()
-      val list=listOf(
-        ResourceType.RiskAssessment.name,
-        ResourceType.RiskAssessment.name,
-        RiskAssessment.PROBABILITY.paramName,
-        BigDecimal("100.00").toDouble()
-      )
       return listOf(
-        arrayOf(BigDecimal("100.00"),str,list),
-        arrayOf(BigDecimal("100"),str,list),
-        arrayOf(BigDecimal("1e-1"),str,list),
-        arrayOf(BigDecimal("1e2"), str,list)
+        arrayOf(BigDecimal("100.00")),
+        arrayOf(BigDecimal("100")),
+        arrayOf(BigDecimal("1e-1")),
+        arrayOf(BigDecimal("1e2"))
       )
     }
   }
@@ -58,8 +41,21 @@ class NumberSearch(
           )
         }
         .getQuery()
-    assertThat(query.query).isEqualTo(expected1)
-    assertThat(query.args).isEqualTo(expected2)
+    assertThat(query.query).isEqualTo(""" 
+        SELECT a.serializedResource
+        FROM ResourceEntity a
+        WHERE a.resourceType = ?
+        AND a.resourceId IN (
+        SELECT resourceId FROM NumberIndexEntity
+        WHERE resourceType = ? AND index_name = ? AND index_value <= ?
+        )
+        """.trimIndent())
+    assertThat(query.args).isEqualTo(listOf(
+      ResourceType.RiskAssessment.name,
+      ResourceType.RiskAssessment.name,
+      RiskAssessment.PROBABILITY.paramName,
+      BigDecimal("100.00").toDouble()
+    ))
   }
 }
 
