1d5a948e324fb01ff499652e29f77e0a580dc0eb
Aurangzaib Umer <aurangzaib.umer@venturedive.com>
2022-02-22 14:14:35 +0500

    test case fixed

--- datacapture/src/main/java/com/google/android/fhir/datacapture/DataCapture.kt
+++ datacapture/src/main/java/com/google/android/fhir/datacapture/DataCapture.kt
@@ -17,12 +17,10 @@
 package com.google.android.fhir.datacapture
 
 import android.content.Context
-import androidx.annotation.VisibleForTesting
-import androidx.annotation.VisibleForTesting.PRIVATE
 
 /** Stores config and global state of the Structured Data Capture Library. */
 internal object DataCapture {
-  @VisibleForTesting(otherwise = PRIVATE) internal lateinit var configuration: DataCaptureConfig
+  private lateinit var configuration: DataCaptureConfig
 
   /**
    * If client has set a configuration by implementing [DataCaptureConfig.Provider], then it returns
