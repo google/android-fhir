38b61972d372502a8f825a8e0f39b31cccd8bc3e
Raazia Tariq <89384079+RaaziaTarique@users.noreply.github.com>
2022-04-12 09:47:37 +0200

    my changes2

--- datacapture/src/main/java/com/google/android/fhir/datacapture/views/RepeatViewHolderFactory.kt
+++ datacapture/src/main/java/com/google/android/fhir/datacapture/views/RepeatViewHolderFactory.kt
@@ -16,50 +16,90 @@
 
 package com.google.android.fhir.datacapture.views
 
-import android.view.ViewGroup
+import android.view.View
+import android.widget.Button
 import com.google.android.fhir.datacapture.QuestionnaireItemViewHolderType
+import com.google.android.fhir.datacapture.R
 import com.google.android.fhir.datacapture.contrib.views.QuestionnaireItemPhoneNumberViewHolderFactory
+import com.google.android.fhir.datacapture.localizedPrefixSpanned
+import com.google.android.fhir.datacapture.validation.ValidationResult
 
-internal object RepeatViewHolderFactory {
+object RepeatViewHolderFactory : QuestionnaireItemViewHolderFactory(R.layout.questionnaire_item_repeats) {
 
-  fun create(parent: ViewGroup, viewType: Int): QuestionnaireItemViewHolder {
-    val viewHolderFactory =
-      when (QuestionnaireItemViewHolderType.fromInt(viewType)) {
-        QuestionnaireItemViewHolderType.GROUP -> QuestionnaireItemGroupViewHolderFactory
+
+
+  private fun getQuestionnaireDelegate(): QuestionnaireItemViewHolderDelegate {
+      //TODO pass viewType as a parameter
+    return when (QuestionnaireItemViewHolderType.fromInt(newViewType)) {
+        QuestionnaireItemViewHolderType.GROUP -> QuestionnaireItemGroupViewHolderFactory.getQuestionnaireItemViewHolderDelegate()
         QuestionnaireItemViewHolderType.BOOLEAN_TYPE_PICKER ->
-          QuestionnaireItemBooleanTypePickerViewHolderFactory
-        QuestionnaireItemViewHolderType.DATE_PICKER -> QuestionnaireItemDatePickerViewHolderFactory
+            QuestionnaireItemBooleanTypePickerViewHolderFactory.getQuestionnaireItemViewHolderDelegate()
+        QuestionnaireItemViewHolderType.DATE_PICKER ->
+            QuestionnaireItemDatePickerViewHolderFactory.getQuestionnaireItemViewHolderDelegate()
         QuestionnaireItemViewHolderType.DATE_TIME_PICKER ->
-          QuestionnaireItemDateTimePickerViewHolderFactory
+            QuestionnaireItemDateTimePickerViewHolderFactory.getQuestionnaireItemViewHolderDelegate()
         QuestionnaireItemViewHolderType.EDIT_TEXT_SINGLE_LINE ->
-          QuestionnaireItemEditTextSingleLineViewHolderFactory
+            QuestionnaireItemEditTextSingleLineViewHolderFactory.getQuestionnaireItemViewHolderDelegate()
         QuestionnaireItemViewHolderType.EDIT_TEXT_MULTI_LINE ->
-          QuestionnaireItemEditTextMultiLineViewHolderFactory
+            QuestionnaireItemEditTextMultiLineViewHolderFactory.getQuestionnaireItemViewHolderDelegate()
         QuestionnaireItemViewHolderType.EDIT_TEXT_INTEGER ->
-          QuestionnaireItemEditTextIntegerViewHolderFactory
+            QuestionnaireItemEditTextIntegerViewHolderFactory.getQuestionnaireItemViewHolderDelegate()
         QuestionnaireItemViewHolderType.EDIT_TEXT_DECIMAL ->
-          QuestionnaireItemEditTextDecimalViewHolderFactory
-        QuestionnaireItemViewHolderType.RADIO_GROUP -> QuestionnaireItemRadioGroupViewHolderFactory
-        QuestionnaireItemViewHolderType.DROP_DOWN -> QuestionnaireItemDropDownViewHolderFactory
-        QuestionnaireItemViewHolderType.DISPLAY -> QuestionnaireItemDisplayViewHolderFactory
+            QuestionnaireItemEditTextDecimalViewHolderFactory.getQuestionnaireItemViewHolderDelegate()
+        QuestionnaireItemViewHolderType.RADIO_GROUP ->
+            QuestionnaireItemRadioGroupViewHolderFactory.getQuestionnaireItemViewHolderDelegate()
+        QuestionnaireItemViewHolderType.DROP_DOWN -> QuestionnaireItemDropDownViewHolderFactory.getQuestionnaireItemViewHolderDelegate()
+        QuestionnaireItemViewHolderType.DISPLAY -> QuestionnaireItemDisplayViewHolderFactory.getQuestionnaireItemViewHolderDelegate()
         QuestionnaireItemViewHolderType.QUANTITY ->
-          QuestionnaireItemEditTextQuantityViewHolderFactory
+            QuestionnaireItemEditTextQuantityViewHolderFactory.getQuestionnaireItemViewHolderDelegate()
         QuestionnaireItemViewHolderType.CHECK_BOX_GROUP ->
-          QuestionnaireItemCheckBoxGroupViewHolderFactory
+            QuestionnaireItemCheckBoxGroupViewHolderFactory.getQuestionnaireItemViewHolderDelegate()
         QuestionnaireItemViewHolderType.AUTO_COMPLETE ->
-          QuestionnaireItemAutoCompleteViewHolderFactory
+            QuestionnaireItemAutoCompleteViewHolderFactory.getQuestionnaireItemViewHolderDelegate()
         QuestionnaireItemViewHolderType.DIALOG_SELECT ->
-          QuestionnaireItemDialogSelectViewHolderFactory
-        QuestionnaireItemViewHolderType.SLIDER -> QuestionnaireItemSliderViewHolderFactory
+            QuestionnaireItemDialogSelectViewHolderFactory.getQuestionnaireItemViewHolderDelegate()
+        QuestionnaireItemViewHolderType.SLIDER -> QuestionnaireItemSliderViewHolderFactory.getQuestionnaireItemViewHolderDelegate()
         QuestionnaireItemViewHolderType.PHONE_NUMBER ->
-          QuestionnaireItemPhoneNumberViewHolderFactory
-      }
-
-    // Add the - button
-    if (parent.tag == "first") {
-      // Add the + button
+            QuestionnaireItemPhoneNumberViewHolderFactory.getQuestionnaireItemViewHolderDelegate()
     }
-
-    return viewHolderFactory.create(parent)
   }
+
+    override fun getQuestionnaireItemViewHolderDelegate(): QuestionnaireItemViewHolderDelegate = object : QuestionnaireItemViewHolderDelegate {
+
+            var childDelegate : QuestionnaireItemViewHolderDelegate = getQuestionnaireDelegate()
+            var addButton : Button? = null
+            var removeButton: Button? = null
+            override var questionnaireItemViewItem: QuestionnaireItemViewItem
+                get() {
+                    return  childDelegate.questionnaireItemViewItem
+                }
+                set(value) {}
+
+
+        override fun init(itemView: View) {
+                childDelegate.init(itemView)
+                addButton = itemView.findViewById(R.id.btn_add_view)
+            }
+
+            override fun bind(questionnaireItemViewItem: QuestionnaireItemViewItem, position: Int) {
+                addButton!!.text = questionnaireItemViewItem.questionnaireItem.localizedPrefixSpanned
+                addButton!!.setOnClickListener {
+                    itemAdapter.addItem(questionnaireItemViewItem, position)
+                }
+                // When add is clicked, add copy the item with the same id as the next
+
+//                removeButton?.setOnClickListener()
+
+                childDelegate.bind(questionnaireItemViewItem, position)
+            }
+
+            override fun displayValidationResult(validationResult: ValidationResult) {
+
+                childDelegate.displayValidationResult(validationResult)
+            }
+
+            override fun setReadOnly(isReadOnly: Boolean) {
+                childDelegate.setReadOnly(isReadOnly)
+            }
+        }
 }
--- datacapture/src/main/java/com/google/android/fhir/datacapture/views/ViewProvider.kt
+++ datacapture/src/main/java/com/google/android/fhir/datacapture/views/ViewProvider.kt
@@ -5,8 +5,14 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.annotation.LayoutRes
 
+/**
+ * Created by Ephraim Kigamba - nek.eam@gmail.com on 06-04-2022.
+ */
 object ViewProvider {
-fun getView(parent: ViewGroup,@LayoutRes resId: Int): View {
- return  LayoutInflater.from(parent.context).inflate(resId, parent, false)
-}
+
+
+    fun getView(parent: ViewGroup, @LayoutRes resId: Int) : View {
+        return LayoutInflater.from(parent.context).inflate(resId, parent, false)
+    }
+
 }
