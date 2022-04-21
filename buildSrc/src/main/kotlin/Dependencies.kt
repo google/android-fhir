cde106f25b81c29ed6e67c35e6c403e6fdeae993
santosh-pingle <santosh.pingle.21@gmail.com>
2022-04-21 15:01:55 +0530

    Upgrade fragment and activity ktx version

--- buildSrc/src/main/kotlin/Dependencies.kt
+++ buildSrc/src/main/kotlin/Dependencies.kt
@@ -146,7 +146,7 @@ object Dependencies {
       const val constraintLayout = "2.1.1"
       const val coreKtx = "1.2.0"
       const val datastorePref = "1.0.0"
-      const val fragmentKtx = "1.3.1"
+      const val fragmentKtx = "1.4.1"
       const val lifecycle = "2.2.0"
       const val navigation = "2.3.4"
       const val recyclerView = "1.1.0"
