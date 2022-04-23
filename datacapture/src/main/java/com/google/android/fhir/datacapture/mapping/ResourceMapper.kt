45a86afad6d78b3dee70529605f92aa60380aaff
Raazia Tariq <89384079+RaaziaTarique@users.noreply.github.com>
2022-04-23 07:23:04 +0200

    Fix issue with empty context with initial expression (#1228)
    * - today() support added in initial expression
    
    * - spotless apply
    
    * - Enhance initialExpression support
    
    * - Enhance initialExpression support modifications
    
    * - Use ExpressionNode to evaluate expression
    
    * - Reduce step one of expression evaluation to use only one expression evaluate method.
    
    * - Simplify ResourceMapper.kt
    - Add test cases in ResourceMapperTest.kt
    
    * - Simply Code
    
    * - Remove extract answers from expressions using definition code.
    
    * - Resolve requested changes.
    
    * - Resolve requested changes.
    
    * - Fix failing test cases.
    
    * - Address requested changes
    
    * - Code refactor
    
    * - Resolve requested changes.
    
    * - Resolve requested changes.
    
    * Refactor ResourceMapper.kt to make it more concise.
    
    * Add documentation.
    
    * Make ITEM_INITIAL_EXPRESSION_URL internal
    
    * Use Kotlin scope functions to replace if..else...
    
    Co-authored-by: Francis Odhiambo Otieno <4540684+f-odhiambo@users.noreply.github.com>
    Co-authored-by: Jing Tang <jingtang@google.com>

--- datacapture/src/main/java/com/google/android/fhir/datacapture/mapping/ResourceMapper.kt
+++ datacapture/src/main/java/com/google/android/fhir/datacapture/mapping/ResourceMapper.kt
@@ -46,7 +46,6 @@ import org.hl7.fhir.r4.model.Questionnaire
 import org.hl7.fhir.r4.model.QuestionnaireResponse
 import org.hl7.fhir.r4.model.Resource
 import org.hl7.fhir.r4.model.StringType
-import org.hl7.fhir.r4.model.StructureMap
 import org.hl7.fhir.r4.model.Type
 import org.hl7.fhir.r4.model.UriType
 import org.hl7.fhir.r4.utils.FHIRPathEngine
@@ -196,28 +195,45 @@ object ResourceMapper {
     questionnaireItem: Questionnaire.QuestionnaireItemComponent,
     vararg resources: Resource
   ) {
-    if (questionnaireItem.type != Questionnaire.QuestionnaireItemType.GROUP) {
-      questionnaireItem.fetchExpression?.let { exp ->
-        val resourceType = exp.expression.substringBefore(".").removePrefix("%")
-
-        // Match the first resource of the same type
-        val contextResource =
-          resources.firstOrNull { it.resourceType.name.equals(resourceType) } ?: return
-
-        val answerExtracted = fhirPathEngine.evaluate(contextResource, exp.expression)
-        answerExtracted.firstOrNull()?.let { answer ->
-          questionnaireItem.initial =
-            mutableListOf(
-              Questionnaire.QuestionnaireItemInitialComponent().setValue(answer.asExpectedType())
-            )
-        }
+    questionnaireItem.initialExpression
+      ?.let {
+        fhirPathEngine
+          .evaluate(
+            selectPopulationContext(resources.asList(), it),
+            it.expression.removePrefix("%")
+          )
+          .singleOrNull()
       }
-    }
+      ?.let {
+        // Set initial value for the questionnaire item. Questionnaire items should not have both
+        // initial value and initial expression.
+        questionnaireItem.initial =
+          mutableListOf(
+            Questionnaire.QuestionnaireItemInitialComponent().setValue(it.asExpectedType())
+          )
+      }
 
     populateInitialValues(questionnaireItem.item, *resources)
   }
 
-  private val Questionnaire.QuestionnaireItemComponent.fetchExpression: Expression?
+  /**
+   * Returns the population context for the questionnaire/group.
+   *
+   * The resource of the same type as the expected type of the initial expression will be selected
+   * first. Otherwise, the first resource in the list will be selected.
+   *
+   * TODO: rewrite this using the launch context and population context.
+   */
+  private fun selectPopulationContext(
+    resources: List<Resource>,
+    initialExpression: Expression
+  ): Resource? {
+    val resourceType = initialExpression.expression.substringBefore(".").removePrefix("%")
+    return resources.singleOrNull { it.resourceType.name.lowercase() == resourceType.lowercase() }
+      ?: resources.firstOrNull()
+  }
+
+  private val Questionnaire.QuestionnaireItemComponent.initialExpression: Expression?
     get() {
       return this.extension.firstOrNull { it.url == ITEM_INITIAL_EXPRESSION_URL }?.let {
         it.value as Expression
@@ -514,7 +530,7 @@ private fun wrapAnswerInFieldType(answer: Base, fieldType: Field): Base {
   return answer
 }
 
-private const val ITEM_INITIAL_EXPRESSION_URL: String =
+internal const val ITEM_INITIAL_EXPRESSION_URL: String =
   "http://hl7.org/fhir/uv/sdc/StructureDefinition/sdc-questionnaire-initialExpression"
 
 private val Field.isList: Boolean
