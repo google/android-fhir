ff57f7be3a65c34419d06e7660b4a0d5ed93ae49
epicadk <adityakurkure@gmail.com>
2022-05-01 14:13:10 +0530

    spotless

--- workflow/src/test/java/com/google/android/fhir/workflow/FhirEngineDalTest.kt
+++ workflow/src/test/java/com/google/android/fhir/workflow/FhirEngineDalTest.kt
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2021 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.google.android.fhir.workflow
 
 import androidx.test.core.app.ApplicationProvider
@@ -21,64 +37,61 @@ import org.robolectric.RobolectricTestRunner
 @RunWith(RobolectricTestRunner::class)
 class FhirEngineDalTest {
 
-    @get:Rule
-    val fhirEngineProviderRule = FhirEngineProviderTestRule()
-    private lateinit var fhirEngine : FhirEngine
+  @get:Rule val fhirEngineProviderRule = FhirEngineProviderTestRule()
+  private lateinit var fhirEngine: FhirEngine
 
-    private lateinit var fhirEngineDal : FhirEngineDal
+  private lateinit var fhirEngineDal: FhirEngineDal
 
-    @Before
-    fun setupTest() {
-        fhirEngine = FhirEngineProvider.getInstance(ApplicationProvider.getApplicationContext())
-        fhirEngineDal = FhirEngineDal(fhirEngine)
-        runBlocking { fhirEngine.create(testPatient) }
+  @Before
+  fun setupTest() {
+    fhirEngine = FhirEngineProvider.getInstance(ApplicationProvider.getApplicationContext())
+    fhirEngineDal = FhirEngineDal(fhirEngine)
+    runBlocking { fhirEngine.create(testPatient) }
+  }
 
-    }
+  @Test
+  fun testDalRead() = runBlocking {
+    val result = fhirEngineDal.read(IdType("Patient/${testPatient.id}"))
+    Truth.assertThat(result).isInstanceOf(Patient::class.java)
+    Truth.assertThat((result as Patient).nameFirstRep.givenAsSingleString)
+      .isEqualTo(testPatient.nameFirstRep.givenAsSingleString)
+  }
 
-    @Test
-    fun testDalRead() = runBlocking {
-        val result = fhirEngineDal.read(IdType("Patient/${testPatient.id}"))
-        Truth.assertThat(result).isInstanceOf(Patient::class.java)
-        Truth.assertThat((result as Patient).nameFirstRep.givenAsSingleString)
-            .isEqualTo(testPatient.nameFirstRep.givenAsSingleString)
-    }
+  @Test
+  fun testDalCreate() = runBlocking {
+    val patient =
+      Patient().apply {
+        id = "Patient/2"
+        addName(HumanName().apply { addGiven("John") })
+      }
+    fhirEngineDal.create(patient)
+    val result = fhirEngine.get(ResourceType.Patient, "2") as Patient
+    Truth.assertThat(result.nameFirstRep.givenAsSingleString)
+      .isEqualTo(patient.nameFirstRep.givenAsSingleString)
+  }
 
-    @Test
-    fun testDalCreate() = runBlocking {
-        val patient =
-            Patient().apply {
-                id = "Patient/2"
-                addName(HumanName().apply { addGiven("John") })
-            }
-        fhirEngineDal.create(patient)
-        val result = fhirEngine.get(ResourceType.Patient, "2") as Patient
-        Truth.assertThat(result.nameFirstRep.givenAsSingleString)
-            .isEqualTo(patient.nameFirstRep.givenAsSingleString)
-    }
+  @Test
+  fun testDalUpdate() = runBlocking {
+    testPatient.name = mutableListOf(HumanName().apply { addGiven("Eve") })
+    fhirEngineDal.update(testPatient)
+    val result = fhirEngine.search<Patient> {}.single()
+    Truth.assertThat(result.nameFirstRep.givenAsSingleString).isEqualTo("Eve")
+  }
 
-    @Test
-    fun testDalUpdate() = runBlocking {
-        testPatient.name = mutableListOf(HumanName().apply { addGiven("Eve") })
-        fhirEngineDal.update(testPatient)
-        val result = fhirEngine.search<Patient> {}.single()
-        Truth.assertThat(result.nameFirstRep.givenAsSingleString).isEqualTo("Eve")
-    }
+  @Test
+  fun testDalDelete() = runBlocking {
+    fhirEngineDal.delete(testPatient.idElement)
+    val result = fhirEngine.search<Patient> {}
+    Truth.assertThat(result).isEmpty()
+  }
 
-    @Test
-    fun testDalDelete() = runBlocking {
-        fhirEngineDal.delete(testPatient.idElement)
-        val result = fhirEngine.search<Patient> {}
-        Truth.assertThat(result).isEmpty()
-    }
+  @After fun fhirEngine() = runBlocking { fhirEngine.delete(ResourceType.Patient, "Patient/1") }
 
-    @After
-    fun fhirEngine() = runBlocking { fhirEngine.delete(ResourceType.Patient, "Patient/1") }
-
-    companion object {
-        val testPatient =
-            Patient().apply {
-                id = "Patient/1"
-                addName(HumanName().apply { addGiven("Jane") })
-            }
-    }
+  companion object {
+    val testPatient =
+      Patient().apply {
+        id = "Patient/1"
+        addName(HumanName().apply { addGiven("Jane") })
+      }
+  }
 }
