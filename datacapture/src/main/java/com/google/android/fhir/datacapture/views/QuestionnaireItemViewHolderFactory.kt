0c7c34e3b6eb36e9782b1f41ab8f3fcac00dc399
aurangzaibumer <35099184+aurangzaibumer@users.noreply.github.com>
2022-05-23 18:33:44 +0500

    Update datacapture/src/main/java/com/google/android/fhir/datacapture/views/QuestionnaireItemViewHolderFactory.kt
    Co-authored-by: Jing Tang <jingtang@google.com>

--- datacapture/src/main/java/com/google/android/fhir/datacapture/views/QuestionnaireItemViewHolderFactory.kt
+++ datacapture/src/main/java/com/google/android/fhir/datacapture/views/QuestionnaireItemViewHolderFactory.kt
@@ -62,9 +62,8 @@ open class QuestionnaireItemViewHolder(
     delegate.questionnaireItemViewItem = questionnaireItemViewItem
     delegate.bind(questionnaireItemViewItem)
     delegate.setReadOnly(questionnaireItemViewItem.questionnaireItem.readOnly)
-    // Only validate questionnaire items when user has modified it or there's any existing answers.
-    // This will prevent showing validation errors at the start of the form, to avoid UI cluttering
-    // upon opening questionnaire.
+    // Only validate the questionnaire item after the user has modified it or if there's any existing answers.
+    // This will prevent cluttering the UI with validation errors when the user opens an empty questionnaire.
     if (delegate.questionnaireItemViewItem.modified ||
         delegate.questionnaireItemViewItem.questionnaireResponseItem.answer.size > 0
     ) {
