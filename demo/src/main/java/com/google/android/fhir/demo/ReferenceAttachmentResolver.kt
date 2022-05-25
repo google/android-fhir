b26b103ba4f35f5d21d05cef1e87c4073c90b12c
fikrimilano <fikrimilano1@gmail.com>
2022-05-25 20:58:52 +0700

    Fix failing test

--- demo/src/main/java/com/google/android/fhir/demo/ReferenceAttachmentResolver.kt
+++ demo/src/main/java/com/google/android/fhir/demo/ReferenceAttachmentResolver.kt
@@ -19,13 +19,14 @@ package com.google.android.fhir.demo
 import android.content.Context
 import android.graphics.Bitmap
 import com.google.android.fhir.datacapture.AttachmentResolver
+import com.google.android.fhir.get
 import org.hl7.fhir.r4.model.Binary
 
 class ReferenceAttachmentResolver(val context: Context) : AttachmentResolver {
 
   override suspend fun resolveBinaryResource(uri: String): Binary? {
     return uri.substringAfter("Binary/").substringBefore("/").run {
-      FhirApplication.fhirEngine(context).load(Binary::class.java, this)
+      FhirApplication.fhirEngine(context).get(this)
     }
   }
 
