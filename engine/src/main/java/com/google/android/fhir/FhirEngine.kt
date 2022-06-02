3e1efbb9f97278a0b21b3806eac89bfa48ef1af8
Raazia Tariq <89384079+RaaziaTarique@users.noreply.github.com>
2022-06-02 10:36:05 +0200

    - Moved FhirXQuery method to FhirEngineImpl

--- engine/src/main/java/com/google/android/fhir/FhirEngine.kt
+++ engine/src/main/java/com/google/android/fhir/FhirEngine.kt
@@ -49,6 +49,12 @@ interface FhirEngine {
   suspend fun <R : Resource> search(search: Search): List<R>
 
   /**
+   * Searches the database and returns a list resources according to the [fhirXQueryModel]
+   * specifications.
+   */
+  suspend fun <R : Resource> searchByFhirXQueryModel(fhirXQueryModel: FhirXQueryModel): List<R>
+
+  /**
    * Synchronizes the [upload] result in the database. [upload] operation may result in multiple
    * calls to the server to upload the data. Result of each call will be emitted by [upload] and the
    * api caller should [Flow.collect] it.
