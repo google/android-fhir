4e31216f099a8a20fb772a9b4988f651d3389db5
Aurangzaib Umer <aurangzaib.umer@venturedive.com>
2022-06-23 02:39:10 +0500

    added instrumentation test for DatePickerViewFactory

---
+++ datacapture/src/androidTest/java/com/google/android/fhir/datacapture/views/QuestionnaireItemDatePickerViewHolderFactoryEspressoTest.kt
@@ -0,0 +1,108 @@
+/*
+ * Copyright 2021 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.google.android.fhir.datacapture.views
+
+import android.view.View
+import android.widget.FrameLayout
+import android.widget.TextView
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.RootMatchers.isDialog
+import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
+import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.espresso.matcher.ViewMatchers.withText
+import androidx.test.ext.junit.rules.ActivityScenarioRule
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.platform.app.InstrumentationRegistry
+import com.google.android.fhir.datacapture.R
+import com.google.android.fhir.datacapture.TestActivity
+import com.google.android.fhir.datacapture.utilities.clickIcon
+import com.google.common.truth.Truth.assertThat
+import org.hamcrest.CoreMatchers.allOf
+import org.hl7.fhir.r4.model.DateType
+import org.hl7.fhir.r4.model.Questionnaire
+import org.hl7.fhir.r4.model.QuestionnaireResponse
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)
+class QuestionnaireItemDatePickerViewHolderFactoryEspressoTest {
+
+  @Rule
+  @JvmField
+  var activityScenarioRule: ActivityScenarioRule<TestActivity> =
+    ActivityScenarioRule(TestActivity::class.java)
+
+  private lateinit var parent: FrameLayout
+  private lateinit var viewHolder: QuestionnaireItemViewHolder
+  @Before
+  fun setup() {
+    activityScenarioRule.scenario.onActivity { activity -> parent = FrameLayout(activity) }
+    viewHolder = QuestionnaireItemDatePickerViewHolderFactory.create(parent)
+    setTestLayout(viewHolder.itemView)
+  }
+
+  @Test
+  fun shouldSetDateInput() {
+    val questionnaireItemView =
+      QuestionnaireItemViewItem(
+        Questionnaire.QuestionnaireItemComponent().apply {
+          addExtension().apply {
+            url = "http://hl7.org/fhir/StructureDefinition/minValue"
+            setValue(DateType(2020, 0, 1))
+          }
+          addExtension().apply {
+            url = "http://hl7.org/fhir/StructureDefinition/maxValue"
+            setValue(DateType(2025, 0, 1))
+          }
+        },
+        QuestionnaireResponse.QuestionnaireResponseItemComponent()
+      ) {}
+
+    runOnUI { viewHolder.bind(questionnaireItemView) }
+
+    assertThat(
+        viewHolder.itemView.findViewById<TextView>(R.id.text_input_edit_text).text.toString()
+      )
+      .isEmpty()
+
+    onView(withId(R.id.text_input_layout)).perform(clickIcon(true))
+    onView(allOf(withText("OK")))
+      .inRoot(isDialog())
+      .check(matches(isDisplayed()))
+      .perform(ViewActions.click())
+
+    assertThat(
+        viewHolder.itemView.findViewById<TextView>(R.id.text_input_edit_text).text.toString()
+      )
+      .isNotEmpty()
+  }
+
+  /** Method to run code snippet on UI/main thread */
+  private fun runOnUI(action: () -> Unit) {
+    activityScenarioRule.scenario.onActivity { activity -> action() }
+  }
+
+  /** Method to set content view for test activity */
+  private fun setTestLayout(view: View) {
+    activityScenarioRule.scenario.onActivity { activity -> activity.setContentView(view) }
+    InstrumentationRegistry.getInstrumentation().waitForIdleSync()
+  }
+}
