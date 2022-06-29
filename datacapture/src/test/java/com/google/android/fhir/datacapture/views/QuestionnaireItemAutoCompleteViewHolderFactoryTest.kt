3811cedd40f96e40b713c1749b4fc86dfff08966
Aditya Khajuria <adityakhajuria@google.com>
2022-06-29 14:18:31 +0530

    Fixed failing tests

--- datacapture/src/test/java/com/google/android/fhir/datacapture/views/QuestionnaireItemAutoCompleteViewHolderFactoryTest.kt
+++ datacapture/src/test/java/com/google/android/fhir/datacapture/views/QuestionnaireItemAutoCompleteViewHolderFactoryTest.kt
@@ -39,7 +39,7 @@ import org.robolectric.RuntimeEnvironment
 class QuestionnaireItemAutoCompleteViewHolderFactoryInstrumentedTest {
   private val parent =
     FrameLayout(
-      RuntimeEnvironment.getApplication().apply { setTheme(R.style.Theme_MaterialComponents) }
+      RuntimeEnvironment.getApplication().apply { setTheme(R.style.Theme_Material3_DayNight) }
     )
   private val viewHolder = QuestionnaireItemAutoCompleteViewHolderFactory.create(parent)
 
