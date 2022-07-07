7c17c10262fdab02a790f46fdc1e21b0a02b370c
Yigit Boyar <yboyar@google.com>
2022-07-07 10:33:41 -0700

    add enterprise plugin

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,3 +1,17 @@
+plugins {
+    id("com.gradle.enterprise") version("3.10")
+}
+
+gradleEnterprise {
+    buildScan {
+        termsOfServiceUrl = "https://gradle.com/terms-of-service"
+        termsOfServiceAgree = "yes"
+        capture {
+            isTaskInputFiles = true
+        }
+    }
+}
+
 include(":catalog")
 
 include(":codegen")
