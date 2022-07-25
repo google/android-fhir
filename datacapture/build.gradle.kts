d715db42a8d270a8ca33290c6f3a95a8a79fccf1
williamito <williamito@google.com>
2022-07-26 00:59:22 +0530

    Dokka (#1466)
    * Add Dokka tasks.
    
    * Add Dokka tasks.
    
    * Apply Spotless formatting fix
    
    * Change output directories to be docs directory directly
    
    * Remove line from bad merge

--- datacapture/build.gradle.kts
+++ datacapture/build.gradle.kts
@@ -1,8 +1,12 @@
+import java.net.URL
+
 plugins {
   id(Plugins.BuildPlugins.androidLib)
   id(Plugins.BuildPlugins.kotlinAndroid)
   id(Plugins.BuildPlugins.mavenPublish)
   jacoco
+  // Use Dokka 1.6.10 until https://github.com/Kotlin/dokka/issues/2452 is resolved.
+  id(Plugins.BuildPlugins.dokka).version(Plugins.Versions.dokka)
 }
 
 publishArtifact(Releases.DataCapture)
@@ -86,4 +90,22 @@ dependencies {
   testImplementation(Dependencies.robolectric)
   testImplementation(Dependencies.truth)
   androidTestImplementation(Dependencies.Espresso.espressoCore)
+}
+
+tasks.dokkaHtml.configure {
+  outputDirectory.set(file("../docs/${Releases.DataCapture.artifactId}"))
+  suppressInheritedMembers.set(true)
+  dokkaSourceSets {
+    named("main") {
+      moduleName.set(Releases.DataCapture.artifactId)
+      moduleVersion.set(Releases.DataCapture.version)
+      noAndroidSdkLink.set(false)
+      externalDocumentationLink {
+        url.set(URL("https://hapifhir.io/hapi-fhir/apidocs/hapi-fhir-structures-r4/"))
+        packageListUrl.set(
+          URL("https://hapifhir.io/hapi-fhir/apidocs/hapi-fhir-structures-r4/element-list")
+        )
+      }
+    }
+  }
 }
