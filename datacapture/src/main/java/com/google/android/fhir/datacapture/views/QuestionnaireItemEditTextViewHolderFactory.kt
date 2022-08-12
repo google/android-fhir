dad74e7f5bafeeafb6672a0b6bd6fc13056cb683
Jing Tang <jingtang@google.com>
2022-08-12 14:40:30 +0100

    Refactor validation result to be a sealed class (#1553)
    * Refactor the validation result to a sealed class
    
    * Fix rebase error
    
    * Fix tests for radio group
    
    * Fix build issue in demo app
    
    * Run spotless
    
    * Fix compile errors
    
    * Fix bar code instrumentation test

--- datacapture/src/main/java/com/google/android/fhir/datacapture/views/QuestionnaireItemEditTextViewHolderFactory.kt
+++ datacapture/src/main/java/com/google/android/fhir/datacapture/views/QuestionnaireItemEditTextViewHolderFactory.kt
@@ -27,8 +27,10 @@ import androidx.annotation.LayoutRes
 import androidx.core.widget.doAfterTextChanged
 import com.google.android.fhir.datacapture.R
 import com.google.android.fhir.datacapture.localizedFlyoverSpanned
+import com.google.android.fhir.datacapture.validation.Invalid
+import com.google.android.fhir.datacapture.validation.NotValidated
+import com.google.android.fhir.datacapture.validation.Valid
 import com.google.android.fhir.datacapture.validation.ValidationResult
-import com.google.android.fhir.datacapture.validation.getSingleStringValidationMessage
 import com.google.android.material.textfield.TextInputEditText
 import com.google.android.material.textfield.TextInputLayout
 import org.hl7.fhir.r4.model.QuestionnaireResponse
@@ -100,8 +102,10 @@ abstract class QuestionnaireItemEditTextViewHolderDelegate(private val rawInputT
 
   override fun displayValidationResult(validationResult: ValidationResult) {
     textInputLayout.error =
-      if (validationResult.getSingleStringValidationMessage() == "") null
-      else validationResult.getSingleStringValidationMessage()
+      when (validationResult) {
+        is NotValidated, Valid -> null
+        is Invalid -> validationResult.getSingleStringValidationMessage()
+      }
   }
 
   override fun setReadOnly(isReadOnly: Boolean) {
