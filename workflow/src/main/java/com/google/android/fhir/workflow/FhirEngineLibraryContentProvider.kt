1e2a775c1068e962a49270bd195388a3586a01e3
Vitor Pamplona <vitor@vitorpamplona.com>
2022-08-23 15:43:21 -0400

    Fixing obscure NullPointerException when loading and compiling dependent libraries of a library. The LibraryContentProvider must return null if the library isn't found (as opposed to triggering an NPE) to allow next providers to continue the process.

--- workflow/src/main/java/com/google/android/fhir/workflow/FhirEngineLibraryContentProvider.kt
+++ workflow/src/main/java/com/google/android/fhir/workflow/FhirEngineLibraryContentProvider.kt
@@ -26,7 +26,7 @@ class FhirEngineLibraryContentProvider(adapterFactory: AdapterFactory) :
   BaseFhirLibraryContentProvider(adapterFactory) {
   val libs = mutableMapOf<String, Library>()
 
-  override fun getLibrary(libraryIdentifier: VersionedIdentifier): IBaseResource {
-    return libs[libraryIdentifier.id]!!
+  override fun getLibrary(libraryIdentifier: VersionedIdentifier): IBaseResource? {
+    return libs[libraryIdentifier.id]
   }
 }
