6fbc25166cadf18c270526bd347156510e4b88c3
maimoonak <maimoonak.mk@gmail.com>
2022-09-23 11:50:11 +0500

    Fhir data retrieve changes

--- workflow/src/main/java/com/google/android/fhir/workflow/FhirEngineRetrieveProvider.kt
+++ workflow/src/main/java/com/google/android/fhir/workflow/FhirEngineRetrieveProvider.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright 2021 Google LLC
+ * Copyright 2022 Google LLC
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -16,6 +16,7 @@
 
 package com.google.android.fhir.workflow
 
+import ca.uhn.fhir.context.FhirContext
 import ca.uhn.fhir.rest.gclient.ReferenceClientParam
 import ca.uhn.fhir.rest.gclient.TokenClientParam
 import com.google.android.fhir.FhirEngine
@@ -23,14 +24,19 @@ import com.google.android.fhir.get
 import com.google.android.fhir.search.Search
 import com.google.android.fhir.search.search
 import kotlinx.coroutines.runBlocking
+import org.hl7.fhir.r4.model.CodeableConcept
+import org.hl7.fhir.r4.model.Coding
 import org.hl7.fhir.r4.model.Group
 import org.hl7.fhir.r4.model.ResourceType
+import org.hl7.fhir.r4.model.Task
 import org.opencds.cqf.cql.engine.retrieve.TerminologyAwareRetrieveProvider
 import org.opencds.cqf.cql.engine.runtime.Code
 import org.opencds.cqf.cql.engine.runtime.Interval
 
 class FhirEngineRetrieveProvider(private val fhirEngine: FhirEngine) :
   TerminologyAwareRetrieveProvider() {
+  val fhirContext = FhirContext.forR4Cached()
+
   override fun retrieve(
     context: String?,
     contextPath: String?,
@@ -50,17 +56,35 @@ class FhirEngineRetrieveProvider(private val fhirEngine: FhirEngine) :
         listOf(fhirEngine.get(ResourceType.fromCode(dataType), contextValue))
       } else {
         val search = Search(ResourceType.fromCode(dataType))
-        if (search.type != ResourceType.Group && // added special treatment for Group type
-          contextPath is String && context is String && contextValue is String
-        ) {
-          search.filter(ReferenceClientParam(contextPath), { value = "$context/$contextValue" })
-        } else {
-          if (search.type == ResourceType.Patient && // filtering active patients
-            hasField(dataType, "active")
-          ) {
-            search.filter(TokenClientParam("active"), { value = of(true) })
+
+        if (codePath is String)
+          codes?.map { Coding(it.system, it.code, it.display) }?.let {
+            // codePath is coded property name hence get the search param
+            val codeParam =
+              fhirContext.getResourceDefinition(dataType).searchParams.find {
+                  it.path.endsWith(codePath)
+                }!!
+                .name
+            search.filter(
+              TokenClientParam(codeParam),
+              { value = of(CodeableConcept().apply { coding = it }) }
+            )
           }
-        }
+
+        if (contextPath is String && context is String && contextValue is String)
+          when {
+            // in case of Task ignore the contextPath which is requester and use subject to filter
+            // by task beneficiary
+            search.type == ResourceType.Task ->
+              search.filter(
+                ReferenceClientParam(Task.SP_SUBJECT),
+                { value = "$context/$contextValue" }
+              )
+            // in case of Group contextPath is member.entity which might not be intended filter
+            search.type != ResourceType.Group ->
+              search.filter(ReferenceClientParam(contextPath), { value = "$context/$contextValue" })
+          }
+
         fhirEngine.search(search)
       }
     }
