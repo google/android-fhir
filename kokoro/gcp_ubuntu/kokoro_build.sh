6410470d986568153abbb8f9cfa1b073c97feba5
Vitor Pamplona <vitor@vitorpamplona.com>
2022-09-26 15:42:23 -0400

    Updating kokoro to test engine+sdc on 24 and workflow on min 26.

--- kokoro/gcp_ubuntu/kokoro_build.sh
+++ kokoro/gcp_ubuntu/kokoro_build.sh
@@ -107,7 +107,24 @@ function device_tests() {
       --app demo/build/outputs/apk/androidTest/debug/demo-debug-androidTest.apk \
       --test $lib_name/build/outputs/apk/androidTest/debug/$lib_name-debug-androidTest.apk \
       --timeout 30m \
-      --device model=Nexus6P,version=26,locale=en_US \
+      --device model=Nexus6P,version=24,locale=en_US \
+      --device model=Nexus6P,version=27,locale=en_US \
+      --device model=Pixel2,version=30,locale=en_US \
+      --environment-variables coverage=true,coverageFile="/sdcard/Download/coverage.ec" \
+      --directories-to-pull /sdcard/Download  \
+      --results-bucket=$GCS_BUCKET \
+      --results-dir=$KOKORO_BUILD_ARTIFACTS_SUBDIR/firebase/$lib_name \
+      --project=android-fhir-instrumeted-tests \
+      --no-use-orchestrator &
+      firebase_pids+=("$!")
+  done
+
+  local lib_names_min_sdk_26=("workflow")
+  for lib_name in "${lib_names_min_sdk_26[@]}"; do
+   gcloud firebase test android run --type instrumentation \
+      --app demo/build/outputs/apk/androidTest/debug/demo-debug-androidTest.apk \
+      --test $lib_name/build/outputs/apk/androidTest/debug/$lib_name-debug-androidTest.apk \
+      --timeout 30m \
       --device model=Nexus6P,version=27,locale=en_US \
       --device model=Pixel2,version=30,locale=en_US \
       --environment-variables coverage=true,coverageFile="/sdcard/Download/coverage.ec" \
