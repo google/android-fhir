f624f51b980a86f30437a372b8f5a39e2bfad86b
Shoaib Mushtaq <shoaib.mushtaq@venturedive.com>
2022-09-28 11:49:23 +0500

    Added units for SearchTest class

--- engine/src/test/java/com/google/android/fhir/search/SearchTest.kt
+++ engine/src/test/java/com/google/android/fhir/search/SearchTest.kt
@@ -927,6 +927,42 @@ class SearchTest {
   }
 
   @Test
+  fun search_filter_string_matches_fts() {
+    val query =
+      Search(ResourceType.Patient)
+        .apply {
+          filter(
+            Patient.NAME,
+            {
+              modifier = StringFilterModifier.MATCHES_FTS
+              value = "someValue"
+            }
+          )
+        }
+        .getQuery()
+
+    assertThat(query.query)
+      .isEqualTo(
+        """
+        SELECT a.serializedResource
+        FROM ResourceEntity a
+        WHERE a.resourceType = ?
+        AND a.resourceUuid IN (
+        SELECT resourceUuid FROM StringIndexEntity c JOIN StringIndexEntityFts d ON c.id = d.docid
+        WHERE resourceType = ? AND d.index_name = ? AND d.index_value MATCH '*' || ? || '*'
+        )
+        """.trimIndent()
+      )
+    assertThat(query.args)
+      .containsExactly(
+        ResourceType.Patient.name,
+        ResourceType.Patient.name,
+        Patient.NAME.paramName,
+        "someValue"
+      )
+  }
+
+  @Test
   fun search_filter_string_contains() {
     val query =
       Search(ResourceType.Patient)
