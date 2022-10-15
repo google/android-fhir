61b792a8662e2b4bedd14a6786252fb6f1b78a0a
Pallavi Ganorkar <ganorkar@google.com>
2022-10-15 08:01:33 +0530

    spotless check applied

--- catalog/src/main/java/com/google/android/fhir/catalog/BehaviorListViewModel.kt
+++ catalog/src/main/java/com/google/android/fhir/catalog/BehaviorListViewModel.kt
@@ -33,7 +33,7 @@ class BehaviorListViewModel(application: Application) : AndroidViewModel(applica
     val questionnaireFileName: String,
     val workFlow: WorkflowType = WorkflowType.BEHAVIOR
   ) {
-   CALCULATED_EXPRESSION(
+    CALCULATED_EXPRESSION(
       R.drawable.ic_calculations_behavior,
       R.string.behavior_name_calculated_expression,
       "calculated_expression_questionnaire.json"
