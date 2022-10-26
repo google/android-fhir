08bd5ffa1a778d8c03dde5089144083be4398f62
fikrimilano <fikrimilano1@gmail.com>
2022-10-26 10:12:15 +0700

    Create Camera launcher

---
+++ datacapture/src/main/java/com/google/android/fhir/datacapture/views/attachment/CameraLauncherFragment.kt
@@ -0,0 +1,43 @@
+/*
+ * Copyright 2022 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.google.android.fhir.datacapture.views.attachment
+
+import android.net.Uri
+import android.os.Bundle
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.os.bundleOf
+import androidx.fragment.app.DialogFragment
+import androidx.fragment.app.setFragmentResult
+import com.google.android.fhir.datacapture.views.QuestionnaireItemAttachmentViewHolderFactory
+
+class CameraLauncherFragment : DialogFragment() {
+
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    val uri = arguments?.get(QuestionnaireItemAttachmentViewHolderFactory.SAVED_PHOTO_URI) as Uri
+
+    registerForActivityResult(ActivityResultContracts.TakePicture()) { isSaved ->
+        setFragmentResult(RESULT_REQUEST_KEY, bundleOf(RESULT_REQUEST_KEY to isSaved))
+        dismiss()
+      }
+      .launch(uri)
+  }
+
+  companion object {
+    const val RESULT_REQUEST_KEY = "result"
+  }
+}
