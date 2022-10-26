3fa6bffb498385e8a6defe131a94c285dab17fad
fikrimilano <fikrimilano1@gmail.com>
2022-10-26 10:12:30 +0700

    Create Select File launcher

---
+++ datacapture/src/main/java/com/google/android/fhir/datacapture/views/attachment/SelectFileLauncherFragment.kt
@@ -0,0 +1,43 @@
+/*
+ * Copyright 2022 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.google.android.fhir.datacapture.views.attachment
+
+import android.os.Bundle
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.os.bundleOf
+import androidx.fragment.app.DialogFragment
+import androidx.fragment.app.setFragmentResult
+import com.google.android.fhir.datacapture.views.QuestionnaireItemAttachmentViewHolderFactory
+
+class SelectFileLauncherFragment : DialogFragment() {
+
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    val mimeTypes =
+      arguments?.getStringArray(QuestionnaireItemAttachmentViewHolderFactory.MIME_TYPE)
+
+    registerForActivityResult(ActivityResultContracts.OpenDocument()) { uri ->
+        setFragmentResult(RESULT_REQUEST_KEY, bundleOf(RESULT_REQUEST_KEY to uri))
+        dismiss()
+      }
+      .launch(mimeTypes)
+  }
+
+  companion object {
+    const val RESULT_REQUEST_KEY = "result"
+  }
+}
