80f1138301c708ea17da659d702feb2e1f7a5c48
omarismail <omarismail@google.com>
2022-11-11 15:36:58 +0000

    add r5 resources

--- engine/src/main/java/com/google/android/fhir/ResourceType.kt
+++ engine/src/main/java/com/google/android/fhir/ResourceType.kt
@@ -21,10 +21,12 @@ import org.hl7.fhir.exceptions.FHIRException
 enum class ResourceType {
   Account,
   ActivityDefinition,
+  AdministrableProductDefinition,
   AdverseEvent,
   AllergyIntolerance,
   Appointment,
   AppointmentResponse,
+  ArtifactAssessment,
   AuditEvent,
   Basic,
   Binary,
@@ -32,21 +34,27 @@ enum class ResourceType {
   BodyStructure,
   Bundle,
   CapabilityStatement,
+  CapabilityStatement2,
   CarePlan,
   CareTeam,
   CatalogEntry,
   ChargeItem,
   ChargeItemDefinition,
+  Citation,
   Claim,
   ClaimResponse,
   ClinicalImpression,
+  ClinicalUseDefinition,
+  ClinicalUseIssue,
   CodeSystem,
   Communication,
   CommunicationRequest,
   CompartmentDefinition,
   Composition,
   ConceptMap,
+  ConceptMap2,
   Condition,
+  ConditionDefinition,
   Consent,
   Contract,
   Coverage,
@@ -55,8 +63,10 @@ enum class ResourceType {
   DetectedIssue,
   Device,
   DeviceDefinition,
+  DeviceDispense,
   DeviceMetric,
   DeviceRequest,
+  DeviceUsage,
   DeviceUseStatement,
   DiagnosticReport,
   DocumentManifest,
@@ -69,6 +79,7 @@ enum class ResourceType {
   EpisodeOfCare,
   EventDefinition,
   Evidence,
+  EvidenceReport,
   EvidenceVariable,
   ExampleScenario,
   ExplanationOfBenefit,
@@ -79,17 +90,21 @@ enum class ResourceType {
   Group,
   GuidanceResponse,
   HealthcareService,
+  ImagingSelection,
   ImagingStudy,
   Immunization,
   ImmunizationEvaluation,
   ImmunizationRecommendation,
   ImplementationGuide,
+  Ingredient,
   InsurancePlan,
+  InventoryReport,
   Invoice,
   Library,
   Linkage,
   List,
   Location,
+  ManufacturedItemDefinition,
   Measure,
   MeasureReport,
   Media,
@@ -102,6 +117,7 @@ enum class ResourceType {
   MedicinalProduct,
   MedicinalProductAuthorization,
   MedicinalProductContraindication,
+  MedicinalProductDefinition,
   MedicinalProductIndication,
   MedicinalProductIngredient,
   MedicinalProductInteraction,
@@ -109,21 +125,26 @@ enum class ResourceType {
   MedicinalProductPackaged,
   MedicinalProductPharmaceutical,
   MedicinalProductUndesirableEffect,
+  MedicationUsage,
   MessageDefinition,
   MessageHeader,
   MolecularSequence,
   NamingSystem,
+  NutritionIntake,
   NutritionOrder,
+  NutritionProduct,
   Observation,
   ObservationDefinition,
   OperationDefinition,
   OperationOutcome,
   Organization,
   OrganizationAffiliation,
+  PackagedProductDefinition,
   Parameters,
   Patient,
   PaymentNotice,
   PaymentReconciliation,
+  Permission,
   Person,
   PlanDefinition,
   Practitioner,
@@ -132,6 +153,7 @@ enum class ResourceType {
   Provenance,
   Questionnaire,
   QuestionnaireResponse,
+  RegulatedAuthorization,
   RelatedPerson,
   RequestGroup,
   ResearchDefinition,
@@ -149,7 +171,10 @@ enum class ResourceType {
   StructureDefinition,
   StructureMap,
   Subscription,
+  SubscriptionStatus,
+  SubscriptionTopic,
   Substance,
+  SubstanceDefinition,
   SubstanceNucleicAcid,
   SubstancePolymer,
   SubstanceProtein,
@@ -171,10 +196,12 @@ enum class ResourceType {
       return when (this) {
         Account -> "account"
         ActivityDefinition -> "activitydefinition"
+        AdministrableProductDefinition -> "administrableproductdefinition"
         AdverseEvent -> "adverseevent"
         AllergyIntolerance -> "allergyintolerance"
         Appointment -> "appointment"
         AppointmentResponse -> "appointmentresponse"
+        ArtifactAssessment -> "artifactassessment"
         AuditEvent -> "auditevent"
         Basic -> "basic"
         Binary -> "binary"
@@ -182,21 +209,27 @@ enum class ResourceType {
         BodyStructure -> "bodystructure"
         Bundle -> "bundle"
         CapabilityStatement -> "capabilitystatement"
+        CapabilityStatement2 -> "capabilitystatement2"
         CarePlan -> "careplan"
         CareTeam -> "careteam"
         CatalogEntry -> "catalogentry"
         ChargeItem -> "chargeitem"
         ChargeItemDefinition -> "chargeitemdefinition"
+        Citation -> "citation"
         Claim -> "claim"
         ClaimResponse -> "claimresponse"
         ClinicalImpression -> "clinicalimpression"
+        ClinicalUseDefinition -> "clinicalusedefinition"
+        ClinicalUseIssue -> "clinicaluseissue"
         CodeSystem -> "codesystem"
         Communication -> "communication"
         CommunicationRequest -> "communicationrequest"
         CompartmentDefinition -> "compartmentdefinition"
         Composition -> "composition"
         ConceptMap -> "conceptmap"
+        ConceptMap2 -> "conceptmap2"
         Condition -> "condition"
+        ConditionDefinition -> "conditiondefinition"
         Consent -> "consent"
         Contract -> "contract"
         Coverage -> "coverage"
@@ -205,8 +238,10 @@ enum class ResourceType {
         DetectedIssue -> "detectedissue"
         Device -> "device"
         DeviceDefinition -> "devicedefinition"
+        DeviceDispense -> "devicedispense"
         DeviceMetric -> "devicemetric"
         DeviceRequest -> "devicerequest"
+        DeviceUsage -> "deviceusage"
         DeviceUseStatement -> "deviceusestatement"
         DiagnosticReport -> "diagnosticreport"
         DocumentManifest -> "documentmanifest"
@@ -219,6 +254,7 @@ enum class ResourceType {
         EpisodeOfCare -> "episodeofcare"
         EventDefinition -> "eventdefinition"
         Evidence -> "evidence"
+        EvidenceReport -> "evidencereport"
         EvidenceVariable -> "evidencevariable"
         ExampleScenario -> "examplescenario"
         ExplanationOfBenefit -> "explanationofbenefit"
@@ -229,17 +265,21 @@ enum class ResourceType {
         Group -> "group"
         GuidanceResponse -> "guidanceresponse"
         HealthcareService -> "healthcareservice"
+        ImagingSelection -> "imagingselection"
         ImagingStudy -> "imagingstudy"
         Immunization -> "immunization"
         ImmunizationEvaluation -> "immunizationevaluation"
         ImmunizationRecommendation -> "immunizationrecommendation"
         ImplementationGuide -> "implementationguide"
+        Ingredient -> "ingredient"
         InsurancePlan -> "insuranceplan"
+        InventoryReport -> "inventoryreport"
         Invoice -> "invoice"
         Library -> "library"
         Linkage -> "linkage"
         List -> "list"
         Location -> "location"
+        ManufacturedItemDefinition -> "manufactureditemdefinition"
         Measure -> "measure"
         MeasureReport -> "measurereport"
         Media -> "media"
@@ -252,6 +292,7 @@ enum class ResourceType {
         MedicinalProduct -> "medicinalproduct"
         MedicinalProductAuthorization -> "medicinalproductauthorization"
         MedicinalProductContraindication -> "medicinalproductcontraindication"
+        MedicinalProductDefinition -> "medicinalproductdefinition"
         MedicinalProductIndication -> "medicinalproductindication"
         MedicinalProductIngredient -> "medicinalproductingredient"
         MedicinalProductInteraction -> "medicinalproductinteraction"
@@ -259,21 +300,26 @@ enum class ResourceType {
         MedicinalProductPackaged -> "medicinalproductpackaged"
         MedicinalProductPharmaceutical -> "medicinalproductpharmaceutical"
         MedicinalProductUndesirableEffect -> "medicinalproductundesirableeffect"
+        MedicationUsage -> "medicationusage"
         MessageDefinition -> "messagedefinition"
         MessageHeader -> "messageheader"
         MolecularSequence -> "molecularsequence"
         NamingSystem -> "namingsystem"
+        NutritionIntake -> "nutritionintake"
         NutritionOrder -> "nutritionorder"
+        NutritionProduct -> "nutritionproduct"
         Observation -> "observation"
         ObservationDefinition -> "observationdefinition"
         OperationDefinition -> "operationdefinition"
         OperationOutcome -> "operationoutcome"
         Organization -> "organization"
         OrganizationAffiliation -> "organizationaffiliation"
+        PackagedProductDefinition -> "packagedproductdefinition"
         Parameters -> "parameters"
         Patient -> "patient"
         PaymentNotice -> "paymentnotice"
         PaymentReconciliation -> "paymentreconciliation"
+        Permission -> "permission"
         Person -> "person"
         PlanDefinition -> "plandefinition"
         Practitioner -> "practitioner"
@@ -282,6 +328,7 @@ enum class ResourceType {
         Provenance -> "provenance"
         Questionnaire -> "questionnaire"
         QuestionnaireResponse -> "questionnaireresponse"
+        RegulatedAuthorization -> "regulatedauthorization"
         RelatedPerson -> "relatedperson"
         RequestGroup -> "requestgroup"
         ResearchDefinition -> "researchdefinition"
@@ -299,7 +346,10 @@ enum class ResourceType {
         StructureDefinition -> "structuredefinition"
         StructureMap -> "structuremap"
         Subscription -> "subscription"
+        SubscriptionStatus -> "subscriptionstatus"
+        SubscriptionTopic -> "subscriptiontopic"
         Substance -> "substance"
+        SubstanceDefinition -> "substancedefinition"
         SubstanceNucleicAcid -> "substancenucleicacid"
         SubstancePolymer -> "substancepolymer"
         SubstanceProtein -> "substanceprotein"
@@ -323,10 +373,12 @@ enum class ResourceType {
     fun fromCode(code: String): ResourceType {
       if ("Account" == code) return Account
       if ("ActivityDefinition" == code) return ActivityDefinition
+      if ("AdministrableProductDefinition" == code) return AdministrableProductDefinition
       if ("AdverseEvent" == code) return AdverseEvent
       if ("AllergyIntolerance" == code) return AllergyIntolerance
       if ("Appointment" == code) return Appointment
       if ("AppointmentResponse" == code) return AppointmentResponse
+      if ("ArtifactAssessment" == code) return ArtifactAssessment
       if ("AuditEvent" == code) return AuditEvent
       if ("Basic" == code) return Basic
       if ("Binary" == code) return Binary
@@ -334,21 +386,27 @@ enum class ResourceType {
       if ("BodyStructure" == code) return BodyStructure
       if ("Bundle" == code) return Bundle
       if ("CapabilityStatement" == code) return CapabilityStatement
+      if ("CapabilityStatement2" == code) return CapabilityStatement2
       if ("CarePlan" == code) return CarePlan
       if ("CareTeam" == code) return CareTeam
       if ("CatalogEntry" == code) return CatalogEntry
       if ("ChargeItem" == code) return ChargeItem
       if ("ChargeItemDefinition" == code) return ChargeItemDefinition
+      if ("Citation" == code) return Citation
       if ("Claim" == code) return Claim
       if ("ClaimResponse" == code) return ClaimResponse
       if ("ClinicalImpression" == code) return ClinicalImpression
+      if ("ClinicalUseDefinition" == code) return ClinicalUseDefinition
+      if ("ClinicalUseIssue" == code) return ClinicalUseIssue
       if ("CodeSystem" == code) return CodeSystem
       if ("Communication" == code) return Communication
       if ("CommunicationRequest" == code) return CommunicationRequest
       if ("CompartmentDefinition" == code) return CompartmentDefinition
       if ("Composition" == code) return Composition
       if ("ConceptMap" == code) return ConceptMap
+      if ("ConceptMap2" == code) return ConceptMap2
       if ("Condition" == code) return Condition
+      if ("ConditionDefinition" == code) return ConditionDefinition
       if ("Consent" == code) return Consent
       if ("Contract" == code) return Contract
       if ("Coverage" == code) return Coverage
@@ -357,8 +415,10 @@ enum class ResourceType {
       if ("DetectedIssue" == code) return DetectedIssue
       if ("Device" == code) return Device
       if ("DeviceDefinition" == code) return DeviceDefinition
+      if ("DeviceDispense" == code) return DeviceDispense
       if ("DeviceMetric" == code) return DeviceMetric
       if ("DeviceRequest" == code) return DeviceRequest
+      if ("DeviceUsage" == code) return DeviceUsage
       if ("DeviceUseStatement" == code) return DeviceUseStatement
       if ("DiagnosticReport" == code) return DiagnosticReport
       if ("DocumentManifest" == code) return DocumentManifest
@@ -371,6 +431,7 @@ enum class ResourceType {
       if ("EpisodeOfCare" == code) return EpisodeOfCare
       if ("EventDefinition" == code) return EventDefinition
       if ("Evidence" == code) return Evidence
+      if ("EvidenceReport" == code) return EvidenceReport
       if ("EvidenceVariable" == code) return EvidenceVariable
       if ("ExampleScenario" == code) return ExampleScenario
       if ("ExplanationOfBenefit" == code) return ExplanationOfBenefit
@@ -381,17 +442,21 @@ enum class ResourceType {
       if ("Group" == code) return Group
       if ("GuidanceResponse" == code) return GuidanceResponse
       if ("HealthcareService" == code) return HealthcareService
+      if ("ImagingSelection" == code) return ImagingSelection
       if ("ImagingStudy" == code) return ImagingStudy
       if ("Immunization" == code) return Immunization
       if ("ImmunizationEvaluation" == code) return ImmunizationEvaluation
       if ("ImmunizationRecommendation" == code) return ImmunizationRecommendation
       if ("ImplementationGuide" == code) return ImplementationGuide
+      if ("Ingredient" == code) return Ingredient
       if ("InsurancePlan" == code) return InsurancePlan
+      if ("InventoryReport" == code) return InventoryReport
       if ("Invoice" == code) return Invoice
       if ("Library" == code) return Library
       if ("Linkage" == code) return Linkage
       if ("List" == code) return List
       if ("Location" == code) return Location
+      if ("ManufacturedItemDefinition" == code) return ManufacturedItemDefinition
       if ("Measure" == code) return Measure
       if ("MeasureReport" == code) return MeasureReport
       if ("Media" == code) return Media
@@ -404,6 +469,7 @@ enum class ResourceType {
       if ("MedicinalProduct" == code) return MedicinalProduct
       if ("MedicinalProductAuthorization" == code) return MedicinalProductAuthorization
       if ("MedicinalProductContraindication" == code) return MedicinalProductContraindication
+      if ("MedicinalProductDefinition" == code) return MedicinalProductDefinition
       if ("MedicinalProductIndication" == code) return MedicinalProductIndication
       if ("MedicinalProductIngredient" == code) return MedicinalProductIngredient
       if ("MedicinalProductInteraction" == code) return MedicinalProductInteraction
@@ -411,21 +477,26 @@ enum class ResourceType {
       if ("MedicinalProductPackaged" == code) return MedicinalProductPackaged
       if ("MedicinalProductPharmaceutical" == code) return MedicinalProductPharmaceutical
       if ("MedicinalProductUndesirableEffect" == code) return MedicinalProductUndesirableEffect
+      if ("MedicationUsage" == code) return MedicationUsage
       if ("MessageDefinition" == code) return MessageDefinition
       if ("MessageHeader" == code) return MessageHeader
       if ("MolecularSequence" == code) return MolecularSequence
       if ("NamingSystem" == code) return NamingSystem
+      if ("NutritionIntake" == code) return NutritionIntake
       if ("NutritionOrder" == code) return NutritionOrder
+      if ("NutritionProduct" == code) return NutritionProduct
       if ("Observation" == code) return Observation
       if ("ObservationDefinition" == code) return ObservationDefinition
       if ("OperationDefinition" == code) return OperationDefinition
       if ("OperationOutcome" == code) return OperationOutcome
       if ("Organization" == code) return Organization
       if ("OrganizationAffiliation" == code) return OrganizationAffiliation
+      if ("PackagedProductDefinition" == code) return PackagedProductDefinition
       if ("Parameters" == code) return Parameters
       if ("Patient" == code) return Patient
       if ("PaymentNotice" == code) return PaymentNotice
       if ("PaymentReconciliation" == code) return PaymentReconciliation
+      if ("Permission" == code) return Permission
       if ("Person" == code) return Person
       if ("PlanDefinition" == code) return PlanDefinition
       if ("Practitioner" == code) return Practitioner
@@ -434,6 +505,7 @@ enum class ResourceType {
       if ("Provenance" == code) return Provenance
       if ("Questionnaire" == code) return Questionnaire
       if ("QuestionnaireResponse" == code) return QuestionnaireResponse
+      if ("RegulatedAuthorization" == code) return RegulatedAuthorization
       if ("RelatedPerson" == code) return RelatedPerson
       if ("RequestGroup" == code) return RequestGroup
       if ("ResearchDefinition" == code) return ResearchDefinition
@@ -451,7 +523,10 @@ enum class ResourceType {
       if ("StructureDefinition" == code) return StructureDefinition
       if ("StructureMap" == code) return StructureMap
       if ("Subscription" == code) return Subscription
+      if ("SubscriptionStatus" == code) return SubscriptionStatus
+      if ("SubscriptionTopic" == code) return SubscriptionTopic
       if ("Substance" == code) return Substance
+      if ("SubstanceDefinition" == code) return SubstanceDefinition
       if ("SubstanceNucleicAcid" == code) return SubstanceNucleicAcid
       if ("SubstancePolymer" == code) return SubstancePolymer
       if ("SubstanceProtein" == code) return SubstanceProtein
@@ -467,7 +542,7 @@ enum class ResourceType {
       if ("ValueSet" == code) return ValueSet
       if ("VerificationResult" == code) return VerificationResult
       if ("VisionPrescription" == code) return VisionPrescription
-      throw FHIRException("Unknown resource type$code")
+      throw FHIRException("Unknown resource type $code")
     }
   }
 }
