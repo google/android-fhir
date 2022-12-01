db5b36c806b7a67ed455fcc87a1165f9d74de4fc
Kashyap Jois <kjois@iprdgroup.com>
2022-12-01 12:19:35 +0530

    Update release

--- buildSrc/src/main/kotlin/Releases.kt
+++ buildSrc/src/main/kotlin/Releases.kt
@@ -48,13 +48,13 @@ object Releases {
 
   object Engine : LibraryArtifact {
     override val artifactId = "engine"
-    override val version = "0.1.0-beta07_local"
+    override val version = "0.1.0-beta08_local"
     override val name = "Android FHIR Engine Library"
   }
 
   object DataCapture : LibraryArtifact {
     override val artifactId = "data-capture"
-    override val version = "0.1.0-beta07_local"
+    override val version = "0.1.0-beta08_local"
     override val name = "Android FHIR Structured Data Capture Library"
   }
 
