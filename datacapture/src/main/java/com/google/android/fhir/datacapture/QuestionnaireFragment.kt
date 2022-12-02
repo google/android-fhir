8c42962e9543892740b213f0380a3f74dc23623f
Jing Tang <jingtang@google.com>
2022-12-02 18:03:44 +0000

    Remove unnecessary import

--- datacapture/src/main/java/com/google/android/fhir/datacapture/QuestionnaireFragment.kt
+++ datacapture/src/main/java/com/google/android/fhir/datacapture/QuestionnaireFragment.kt
@@ -22,7 +22,6 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import androidx.appcompat.view.ContextThemeWrapper
-import androidx.core.content.res.use
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 import androidx.fragment.app.setFragmentResult
