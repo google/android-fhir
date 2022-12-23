0c9987b7ccf45140bff5003a4bb393a06f3ef24a
williamito <williamito@google.com>
2022-12-23 02:30:48 -0800

    Add version to output path for Dokka (#1772)
    * Add version to output path for Dokka
    
    * spotlessApply
    
    Co-authored-by: Jing Tang <jingtang@google.com>

--- implementationguide/build.gradle.kts
+++ implementationguide/build.gradle.kts
@@ -92,7 +92,9 @@ dependencies {
 }
 
 tasks.dokkaHtml.configure {
-  outputDirectory.set(file("../docs/${Releases.ImplmentationGuide.artifactId}"))
+  outputDirectory.set(
+    file("../docs/${Releases.ImplmentationGuide.artifactId}/${Releases.ImplmentationGuide.version}")
+  )
   suppressInheritedMembers.set(true)
   dokkaSourceSets {
     named("main") {
