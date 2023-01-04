64d9e7fc610f02fdafae76f5c1390783368960c1
owais-vd <owais.ali@venturedive.com>
2023-01-04 14:49:15 +0500

    spotless fixes

--- datacapture/src/main/java/com/google/android/fhir/datacapture/views/QuestionnaireItemViewItem.kt
+++ datacapture/src/main/java/com/google/android/fhir/datacapture/views/QuestionnaireItemViewItem.kt
@@ -20,9 +20,8 @@ import android.content.Context
 import androidx.recyclerview.widget.RecyclerView
 import com.google.android.fhir.datacapture.R
 import com.google.android.fhir.datacapture.answerExpression
-import com.google.android.fhir.datacapture.common.datatype.displayString
 import com.google.android.fhir.datacapture.candidateExpression
-import com.google.android.fhir.datacapture.displayString
+import com.google.android.fhir.datacapture.common.datatype.displayString
 import com.google.android.fhir.datacapture.validation.NotValidated
 import com.google.android.fhir.datacapture.validation.Valid
 import com.google.android.fhir.datacapture.validation.ValidationResult
