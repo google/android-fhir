4d2c62a0dde4839f7f10ebde0a0115c85268c364
omarismail <omarismail@google.com>
2023-02-16 11:20:46 +0000

    add datacapture config in demo app

--- demo/src/main/java/com/google/android/fhir/demo/FhirApplication.kt
+++ demo/src/main/java/com/google/android/fhir/demo/FhirApplication.kt
@@ -23,12 +23,14 @@ import com.google.android.fhir.FhirEngine
 import com.google.android.fhir.FhirEngineConfiguration
 import com.google.android.fhir.FhirEngineProvider
 import com.google.android.fhir.ServerConfiguration
+import com.google.android.fhir.datacapture.DataCaptureConfig
 import com.google.android.fhir.demo.data.FhirSyncWorker
+import com.google.android.fhir.search.search
 import com.google.android.fhir.sync.Sync
 import com.google.android.fhir.sync.remote.HttpLogger
 import timber.log.Timber
 
-class FhirApplication : Application() {
+class FhirApplication : Application(), DataCaptureConfig.Provider {
   // Only initiate the FhirEngine when used for the first time, not when the app is created.
   private val fhirEngine: FhirEngine by lazy { constructFhirEngine() }
 
@@ -62,4 +64,7 @@ class FhirApplication : Application() {
   companion object {
     fun fhirEngine(context: Context) = (context.applicationContext as FhirApplication).fhirEngine
   }
+
+  override fun getDataCaptureConfig(): DataCaptureConfig =
+    DataCaptureConfig(xFhirQueryResolver = { fhirEngine.search(it) })
 }
