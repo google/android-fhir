364c88edace348f72fa2fe1a43340fe7e9998855
fikrimilano <fikrimilano1@gmail.com>
2023-02-24 10:42:48 +0700

    Adjust dimens after merging latest changes

--- datacapture/src/main/java/com/google/android/fhir/datacapture/extensions/MoreQuestionnaireItemAnswerOptionComponents.kt
+++ datacapture/src/main/java/com/google/android/fhir/datacapture/extensions/MoreQuestionnaireItemAnswerOptionComponents.kt
@@ -54,7 +54,8 @@ fun Questionnaire.QuestionnaireItemAnswerOptionComponent.itemAnswerOptionImage(
         "image/jpg",
         "image/png" -> {
           val bitmap = BitmapFactory.decodeByteArray(it.data, 0, it.data.size)
-          val imageSize = context.resources.getDimensionPixelOffset(R.dimen.option_item_image_size)
+          val imageSize =
+            context.resources.getDimensionPixelOffset(R.dimen.item_answer_media_image_size)
           val drawable: Drawable = BitmapDrawable(context.resources, bitmap)
           drawable.setBounds(0, 0, imageSize, imageSize)
           drawable
