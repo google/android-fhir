acbd2335a65bf6ea4ba5d7dbc36d0a642a6872ca
stevenckngaa <68665119+stevenckngaa@users.noreply.github.com>
2023-02-24 15:55:53 +0000

    Add comments to SecurtiyRequirementViolationReceiver

--- demo/src/main/java/com/google/android/fhir/demo/security/SecurityRequirementViolationReceiver.kt
+++ demo/src/main/java/com/google/android/fhir/demo/security/SecurityRequirementViolationReceiver.kt
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2022 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.google.android.fhir.demo.security
 
 import android.app.Notification
@@ -10,19 +26,18 @@ import android.content.Context
 import android.content.Intent
 import android.os.Build
 import androidx.annotation.RequiresApi
-import com.google.android.fhir.FhirEngineProvider
 import com.google.android.fhir.demo.R
 import com.google.android.fhir.security.LockScreenRequirementViolation
-import com.google.android.fhir.security.SecurityRequirementViolation
 import com.google.android.fhir.security.SecurityRequirementViolation.EXTRA_LOCK_SCREEN_REQUIREMENT_VIOLATION
 import timber.log.Timber
 
 /** A sample receiver demonstrates the handling of password requirement violation. */
 class SecurityRequirementViolationReceiver : BroadcastReceiver() {
   override fun onReceive(context: Context, intent: Intent) {
-    val lockScreenRequirementViolation = intent.getParcelableExtra<LockScreenRequirementViolation>(
-      EXTRA_LOCK_SCREEN_REQUIREMENT_VIOLATION
-    )
+    val lockScreenRequirementViolation =
+      intent.getParcelableExtra<LockScreenRequirementViolation>(
+        EXTRA_LOCK_SCREEN_REQUIREMENT_VIOLATION
+      )
     Timber.w("Lock screen violation extra: $lockScreenRequirementViolation")
     if (lockScreenRequirementViolation == null) return
     showPasswordViolationNotification(context, lockScreenRequirementViolation.requiredComplexity)
@@ -30,18 +45,23 @@ class SecurityRequirementViolationReceiver : BroadcastReceiver() {
 
   private fun showPasswordViolationNotification(context: Context, requiredComplexity: Int) {
     val notificationManager = context.getSystemService(NotificationManager::class.java)
-    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N && !notificationManager.areNotificationsEnabled()) {
+    // We can't ask notification runtime permission from the background. Let's ask users for the
+    // notification running permission when the demo app activity is started.
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N &&
+        !notificationManager.areNotificationsEnabled()
+    ) {
       Timber.w("Can't post notification")
       return
     }
 
     createNotificationChannel(notificationManager)
 
-    val notificationBuilder = if (Build.VERSION.SDK_INT < Build.VERSION_CODES.O) {
-       Notification.Builder(context)
-    } else {
-      Notification.Builder(context, SECURITY_REQUIREMENT_VIOLATION_NOTIFICATION_CHANNEL_ID)
-    }
+    val notificationBuilder =
+      if (Build.VERSION.SDK_INT < Build.VERSION_CODES.O) {
+        Notification.Builder(context)
+      } else {
+        Notification.Builder(context, SECURITY_REQUIREMENT_VIOLATION_NOTIFICATION_CHANNEL_ID)
+      }
 
     notificationManager.notify(
       NOTIFICATION_ID,
@@ -52,7 +72,7 @@ class SecurityRequirementViolationReceiver : BroadcastReceiver() {
         .setContentIntent(
           PendingIntent.getActivity(
             context,
-            /* requestCode= */0,
+            /* requestCode= */ 0,
             Intent(DevicePolicyManager.ACTION_SET_NEW_PASSWORD)
               .putExtra(DevicePolicyManager.EXTRA_PASSWORD_COMPLEXITY, requiredComplexity),
             /* flags= */ PendingIntent.FLAG_IMMUTABLE or PendingIntent.FLAG_UPDATE_CURRENT,
