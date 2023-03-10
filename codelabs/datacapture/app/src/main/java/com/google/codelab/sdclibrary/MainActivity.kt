47c60f5e7548ff04981996be98b6db6974f2fd98
William Ito <williamito@google.com>
2023-03-09 18:30:55 -0800

    Fix codelab.

--- codelabs/datacapture/app/src/main/java/com/google/codelab/sdclibrary/MainActivity.kt
+++ codelabs/datacapture/app/src/main/java/com/google/codelab/sdclibrary/MainActivity.kt
@@ -20,7 +20,6 @@ import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
-import com.example.foodallergy.R
 
 class MainActivity : AppCompatActivity() {
 
