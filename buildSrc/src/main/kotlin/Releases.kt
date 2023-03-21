8274e4803a07f8d5003a374a20f4932ac5c1ef9b
Martin Ndegwa <mndegwa@ona.io>
2023-03-21 14:00:15 +0300

    SDK Release: SDC, Engine, Workflow - With unmerged PR https://github.com/opensrp/android-fhir/pull/1 - With unmerged PR https://github.com/google/android-fhir/pull/1669 - With unmerged PR https://github.com/google/android-fhir/pull/1927 - With unmerged PR https://github.com/google/android-fhir/pull/1917

--- buildSrc/src/main/kotlin/Releases.kt
+++ buildSrc/src/main/kotlin/Releases.kt
@@ -50,19 +50,19 @@ object Releases {
 
   object Engine : LibraryArtifact {
     override val artifactId = "engine"
-    override val version = "0.1.0-beta02-preview13-SNAPSHOT"
+    override val version = "0.1.0-beta02-preview14-SNAPSHOT"
     override val name = "Android FHIR Engine Library"
   }
 
   object DataCapture : LibraryArtifact {
     override val artifactId = "data-capture"
-    override val version = "1.0.0-preview-SNAPSHOT"
+    override val version = "1.0.0-preview2-SNAPSHOT"
     override val name = "Android FHIR Structured Data Capture Library"
   }
 
   object Workflow : LibraryArtifact {
     override val artifactId = "workflow"
-    override val version = "0.1.0-alpha02-preview8-SNAPSHOT"
+    override val version = "0.1.0-alpha02-preview9-SNAPSHOT"
     override val name = "Android FHIR Workflow Library"
   }
 
