d59c54275db03235046208b5c519b0e4cf160387
Allan Onchuru <a.onchuru@gmail.com>
2023-04-14 19:30:09 +0300

    SDK Release: Engine   - With unmerged PR https://github.com/opensrp/android-fhir/pull/1   - With unmerged PR https://github.com/google/android-fhir/pull/1917

--- buildSrc/src/main/kotlin/Releases.kt
+++ buildSrc/src/main/kotlin/Releases.kt
@@ -50,7 +50,7 @@ object Releases {
 
   object Engine : LibraryArtifact {
     override val artifactId = "engine"
-    override val version = "0.1.0-beta03"
+    override val version = "0.1.0-beta03-preview-SNAPSHOT"
     override val name = "Android FHIR Engine Library"
   }
 
