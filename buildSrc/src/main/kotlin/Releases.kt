3c2bc65fbfcc43cfa94e87998496f78e5ad20bd0
Allan Onchuru <a.onchuru@gmail.com>
2023-04-17 11:52:34 +0300

    SDK Release: SDC   - With unmerged PR https://github.com/google/android-fhir/pull/1973   - With unmerged PR https://github.com/opensrp/android-fhir/pull/1   - With unmerged PR https://github.com/google/android-fhir/pull/1917

--- buildSrc/src/main/kotlin/Releases.kt
+++ buildSrc/src/main/kotlin/Releases.kt
@@ -56,7 +56,7 @@ object Releases {
 
   object DataCapture : LibraryArtifact {
     override val artifactId = "data-capture"
-    override val version = "1.0.0-preview2-SNAPSHOT"
+    override val version = "1.0.0-preview3-SNAPSHOT"
     override val name = "Android FHIR Structured Data Capture Library"
   }
 
