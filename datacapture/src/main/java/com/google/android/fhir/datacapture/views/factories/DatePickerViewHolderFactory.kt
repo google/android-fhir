5ace3420163d9ede133359cc789a08c1ef607277
Jing Tang <jingtang@google.com>
2023-04-19 13:28:56 +0100

    Update datacapture/src/main/java/com/google/android/fhir/datacapture/views/factories/DatePickerViewHolderFactory.kt

--- datacapture/src/main/java/com/google/android/fhir/datacapture/views/factories/DatePickerViewHolderFactory.kt
+++ datacapture/src/main/java/com/google/android/fhir/datacapture/views/factories/DatePickerViewHolderFactory.kt
@@ -327,6 +327,8 @@ internal fun Int.length() =
 internal fun invalidDateErrorText(context: Context, formatPattern: String) =
   context.getString(
     R.string.date_format_validation_error_msg,
+    // Use 'mm' for month instead of 'MM' to avoid confusion.
+    // See https://developer.android.com/reference/kotlin/java/text/SimpleDateFormat.
     formatPattern.lowercase(),
     formatPattern.replace("dd", "31").replace("MM", "01").replace("yyyy", "2023")
   )
