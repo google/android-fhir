61a9d6c82fe50cbada817cf74d56a4744e17ff35
aditya <adityab@apra.in>
2023-04-19 17:22:39 +0530

    Added signature, timestamp and keyId header to the authenticator.

--- engine/src/main/java/com/google/android/fhir/sync/Authenticator.kt
+++ engine/src/main/java/com/google/android/fhir/sync/Authenticator.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright 2021 Google LLC
+ * Copyright 2022 Google LLC
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -26,4 +26,7 @@ import androidx.annotation.WorkerThread
 interface Authenticator {
   /** @return Access token for the engine to make requests on user's behalf. */
   @WorkerThread fun getAccessToken(): String
+  @WorkerThread fun getSignatureHeader(): String
+  @WorkerThread fun getTimeStampHeader(): String
+  @WorkerThread fun getKeyIdHeader(): String
 }
