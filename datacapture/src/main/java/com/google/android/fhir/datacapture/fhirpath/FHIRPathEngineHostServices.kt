6f368e272ef2ca6aa658d737435d854a0672b4f8
fikrimilano <fikrimilano1@gmail.com>
2023-05-24 10:23:53 +0700

    Use safe casting

--- datacapture/src/main/java/com/google/android/fhir/datacapture/fhirpath/FHIRPathEngineHostServices.kt
+++ datacapture/src/main/java/com/google/android/fhir/datacapture/fhirpath/FHIRPathEngineHostServices.kt
@@ -26,9 +26,7 @@ import org.hl7.fhir.r4.utils.FHIRPathEngine
  */
 internal object FHIRPathEngineHostServices : FHIRPathEngine.IEvaluationContext {
   override fun resolveConstant(appContext: Any?, name: String?, beforeContext: Boolean): Base? {
-    return if (appContext is Map<*, *> && appContext.containsKey(name) && appContext[name] != null)
-      appContext[name] as Base
-    else null
+    return (appContext as? Map<*, *>)?.get(name) as? Base
   }
 
   override fun resolveConstantType(appContext: Any?, name: String?): TypeDetails {
