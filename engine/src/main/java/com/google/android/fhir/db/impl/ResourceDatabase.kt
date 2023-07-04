b063dc66192ba4803787bf3581e3c1eab09ddd54
omarismail <omarismail@google.com>
2023-07-04 13:38:11 +0100

    use same dB version as we havent released

--- engine/src/main/java/com/google/android/fhir/db/impl/ResourceDatabase.kt
+++ engine/src/main/java/com/google/android/fhir/db/impl/ResourceDatabase.kt
@@ -50,7 +50,7 @@ import com.google.android.fhir.db.impl.entities.UriIndexEntity
       LocalChangeEntity::class,
       PositionIndexEntity::class
     ],
-  version = 4,
+  version = 3,
   exportSchema = true
 )
 @TypeConverters(DbTypeConverters::class)
@@ -70,26 +70,22 @@ val MIGRATION_2_3 =
   object : Migration(/* startVersion = */ 2, /* endVersion = */ 3) {
     override fun migrate(database: SupportSQLiteDatabase) {
       database.execSQL(
-        "CREATE INDEX IF NOT EXISTS `index_DateTimeIndexEntity_index_from` ON `DateTimeIndexEntity` (`index_from`)"
-      )
-    }
-  }
-
-val MIGRATION_3_4 =
-  object : Migration(/* startVersion = */ 3, /* endVersion = */ 4) {
-    override fun migrate(database: SupportSQLiteDatabase) {
-      database.execSQL(
         "CREATE INDEX IF NOT EXISTS `index_DateTimeIndexEntity_resourceType_index_name_resourceUuid_index_from_index_to` ON `DateTimeIndexEntity` (`resourceType`, `index_name`, `resourceUuid`, `index_from`, `index_to`)"
       )
       database.execSQL(
         "CREATE INDEX IF NOT EXISTS `index_DateIndexEntity_resourceType_index_name_resourceUuid_index_from_index_to` ON `DateIndexEntity` (`resourceType`, `index_name`, `resourceUuid`, `index_from`, `index_to`)"
       )
-      database.execSQL("DROP INDEX IF EXISTS `index_DateTimeIndexEntity_index_from`")
+      database.execSQL(
+        "CREATE INDEX IF NOT EXISTS `index_TokenIndexEntity_resourceType_index_name_index_system_index_value_resourceUuid` ON `TokenIndexEntity` (`resourceType`, `index_name`, `index_system`, `index_value`, `resourceUuid`)"
+      )
       database.execSQL(
         "DROP INDEX IF EXISTS `index_DateTimeIndexEntity_resourceType_index_name_index_from_index_to`"
       )
       database.execSQL(
         "DROP INDEX IF EXISTS `index_DateIndexEntity_resourceType_index_name_index_from_index_to`"
+      )
+      database.execSQL(
+        "DROP INDEX IF EXISTS `index_TokenIndexEntity_resourceType_index_name_index_system_index_value`"
       )
     }
   }
--- engine/src/main/java/com/google/android/fhir/db/impl/entities/TokenIndexEntity.kt
+++ engine/src/main/java/com/google/android/fhir/db/impl/entities/TokenIndexEntity.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright 2021 Google LLC
+ * Copyright 2022 Google LLC
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -28,9 +28,10 @@ import org.hl7.fhir.r4.model.ResourceType
 @Entity(
   indices =
     [
-      Index(value = ["resourceType", "index_name", "index_system", "index_value"]),
+      Index(value = ["resourceType", "index_name", "index_system", "index_value", "resourceUuid"]),
       // keep this index for faster foreign lookup
-      Index(value = ["resourceUuid"])],
+      Index(value = ["resourceUuid"])
+    ],
   foreignKeys =
     [
       ForeignKey(
@@ -40,7 +41,8 @@ import org.hl7.fhir.r4.model.ResourceType
         onDelete = ForeignKey.CASCADE,
         onUpdate = ForeignKey.NO_ACTION,
         deferred = true
-      )]
+      )
+    ]
 )
 internal data class TokenIndexEntity(
   @PrimaryKey(autoGenerate = true) val id: Long,
