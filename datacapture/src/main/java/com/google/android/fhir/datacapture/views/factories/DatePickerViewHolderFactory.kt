40b06bb0261bbc29f4619d0e9f3496dc20531304
Jing Tang <jingtang@google.com>
2023-08-17 16:52:16 +0100

    Add tests from previous pr

--- datacapture/src/main/java/com/google/android/fhir/datacapture/views/factories/DatePickerViewHolderFactory.kt
+++ datacapture/src/main/java/com/google/android/fhir/datacapture/views/factories/DatePickerViewHolderFactory.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright 2022 Google LLC
+ * Copyright 2022-2023 Google LLC
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -125,7 +125,6 @@ internal object DatePickerViewHolderFactory :
           helperText = getRequiredOrOptionalText(questionnaireViewItem, context)
         }
         textInputEditText.removeTextChangedListener(textWatcher)
-
 
         val questionnaireItemViewItemDateAnswer =
           questionnaireViewItem.answers.singleOrNull()?.valueDateType?.localDate
