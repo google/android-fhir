b7b8b980c8c2f4ccbc2976d0dc5d127db687662b
aditya-07 <adityakhajuria@google.com>
2023-08-22 00:27:18 +0530

    Allow users to request referenced resources along with the searched via inline params. (#1978)
    * Inital changes to support inline resources via revInclude
    
    * Updated test and added doc
    
    * RevInclude to return mapped response
    
    * Test code for forward include
    
    * Fixed include query
    
    * Unify search api to include include and revInclude functionality
    
    * Workflow library: Incorporated changes related to FhirEngine.search
    
    * Review comments: Changed the search result type to include the search param as well.
    
    * Updated docs
    
    * Added spotless toggle to skip indentation for specific code portions
    
    * Added individual tests for include and revInclude
    
    * Review Comments: Updated kdocs and refactored function name
    
    * Review comments: refactored type of revInclude
    
    * Fixed failing workflow compilation as workflow doesn't depends on engine project directly but released .aar
    
    * Resolved compilation errors
    
    * Reverted the engine changes

--- engine/src/test/java/com/google/android/fhir/impl/FhirEngineImplTest.kt
+++ engine/src/test/java/com/google/android/fhir/impl/FhirEngineImplTest.kt
@@ -183,7 +183,9 @@ class FhirEngineImplTest {
     val result = fhirEngine.search("Patient?gender=female")
 
     assertThat(result.size).isEqualTo(2)
-    assertThat(result.all { (it as Patient).gender == Enumerations.AdministrativeGender.FEMALE })
+    assertThat(
+        result.all { (it.resource as Patient).gender == Enumerations.AdministrativeGender.FEMALE }
+      )
       .isTrue()
   }
 
@@ -198,7 +200,7 @@ class FhirEngineImplTest {
 
     fhirEngine.create(*patients.toTypedArray())
 
-    val result = fhirEngine.search("Patient?_sort=-name").map { it as Patient }
+    val result = fhirEngine.search("Patient?_sort=-name").map { it.resource as Patient }
 
     assertThat(result.mapNotNull { it.nameFirstRep.given.firstOrNull()?.value })
       .isEqualTo(listOf("C", "B", "A"))
@@ -215,7 +217,7 @@ class FhirEngineImplTest {
 
     fhirEngine.create(*patients.toTypedArray())
 
-    val result = fhirEngine.search("Patient?_count=1").map { it as Patient }
+    val result = fhirEngine.search("Patient?_count=1").map { it.resource as Patient }
 
     assertThat(result.size).isEqualTo(1)
   }
@@ -261,7 +263,7 @@ class FhirEngineImplTest {
 
     fhirEngine.create(*patients.toTypedArray())
 
-    val result = fhirEngine.search("Patient?_tag=Tag1").map { it as Patient }
+    val result = fhirEngine.search("Patient?_tag=Tag1").map { it.resource as Patient }
 
     assertThat(result.size).isEqualTo(1)
     assertThat(result.all { patient -> patient.meta.tag.all { it.code == "Tag1" } }).isTrue()
@@ -295,7 +297,7 @@ class FhirEngineImplTest {
         .search(
           "Patient?_profile=http://fhir.org/STU3/StructureDefinition/Example-Patient-Profile-1"
         )
-        .map { it as Patient }
+        .map { it.resource as Patient }
 
     assertThat(result.size).isEqualTo(1)
     assertThat(
@@ -578,7 +580,7 @@ class FhirEngineImplTest {
       }
 
     assertThat(result).isNotEmpty()
-    assertThat(result.map { it.logicalId }).containsExactly("patient-id-create").inOrder()
+    assertThat(result.map { it.resource.logicalId }).containsExactly("patient-id-create").inOrder()
   }
 
   @Test
@@ -616,7 +618,9 @@ class FhirEngineImplTest {
       assertThat(DateTimeType(Date.from(localChangeTimestampWhenUpdated)).value)
         .isAtLeast(DateTimeType(Date.from(localChangeTimestampWhenCreated)).value)
       assertThat(result).isNotEmpty()
-      assertThat(result.map { it.logicalId }).containsExactly("patient-id-update").inOrder()
+      assertThat(result.map { it.resource.logicalId })
+        .containsExactly("patient-id-update")
+        .inOrder()
     }
 
   companion object {
