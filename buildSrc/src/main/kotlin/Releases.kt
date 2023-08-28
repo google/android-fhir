75f243da2ec22cc9b7605b25d2ad0383de39422c
Martin Ndegwa <mndegwa@ona.io>
2023-08-28 10:28:03 +0300

    Engine, SDC, Workflow, Common, Knowledge
        - With unmerged PR https://github.com/google/android-fhir/pull/2032
        - With unmerged PR https://github.com/opensrp/android-fhir/pull/1
        - With unmerged PR https://github.com/google/android-fhir/pull/1669
        - With unmerged PR https://github.com/google/android-fhir/pull/2132
        - With unmerged PR https://github.com/opensrp/android-fhir/pull/9
        - With unmerged PR https://github.com/google/android-fhir/pull/2076
        - With unmerged PR https://github.com/opensrp/android-fhir/pull/10

--- buildSrc/src/main/kotlin/Releases.kt
+++ buildSrc/src/main/kotlin/Releases.kt
@@ -54,7 +54,7 @@ object Releases {
 
   object DataCapture : LibraryArtifact {
     override val artifactId = "data-capture"
-    override val version = "1.0.0-preview15-SNAPSHOT"
+    override val version = "1.0.0-preview16-SNAPSHOT"
     override val name = "Android FHIR Structured Data Capture Library"
   }
 
