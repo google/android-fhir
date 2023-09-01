c9e82a7931f7d0ea1db0629711daec230446a21c
Aditya Khajuria <adityakhajuria@google.com>
2023-09-01 14:44:19 +0530

    Fixed demo app crash by appropriately handing ResourceNotFoundException.

--- demo/src/main/java/com/google/android/fhir/demo/FhirApplication.kt
+++ demo/src/main/java/com/google/android/fhir/demo/FhirApplication.kt
@@ -26,6 +26,7 @@ import com.google.android.fhir.NetworkConfiguration
 import com.google.android.fhir.ServerConfiguration
 import com.google.android.fhir.datacapture.DataCaptureConfig
 import com.google.android.fhir.datacapture.XFhirQueryResolver
+import com.google.android.fhir.db.ResourceNotFoundException
 import com.google.android.fhir.search.search
 import com.google.android.fhir.sync.remote.HttpLogger
 import timber.log.Timber
@@ -63,7 +64,14 @@ class FhirApplication : Application(), DataCaptureConfig.Provider {
     dataCaptureConfig =
       DataCaptureConfig().apply {
         urlResolver = ReferenceUrlResolver(this@FhirApplication as Context)
-        xFhirQueryResolver = XFhirQueryResolver { fhirEngine.search(it).map { it.resource } }
+        xFhirQueryResolver = XFhirQueryResolver {
+          try {
+            fhirEngine.search(it).map { it.resource }
+          } catch (e: ResourceNotFoundException) {
+            Timber.e(e)
+            emptyList()
+          }
+        }
       }
   }
 
