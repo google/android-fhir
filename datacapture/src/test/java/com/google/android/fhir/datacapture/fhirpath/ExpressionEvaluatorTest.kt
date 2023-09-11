36ea59eeed682cf43a0b88b7efbefc962ce62511
Jing Tang <jingtang@google.com>
2023-09-11 15:41:52 +0100

    Update datacapture/src/test/java/com/google/android/fhir/datacapture/fhirpath/ExpressionEvaluatorTest.kt

--- datacapture/src/test/java/com/google/android/fhir/datacapture/fhirpath/ExpressionEvaluatorTest.kt
+++ datacapture/src/test/java/com/google/android/fhir/datacapture/fhirpath/ExpressionEvaluatorTest.kt
@@ -786,7 +786,7 @@ class ExpressionEvaluatorTest {
         Questionnaire(),
         QuestionnaireResponse(),
         /* questionnaireItemParentMap= */ mapOf(),
-        mapOf(practitioner.resourceType.name to practitioner)
+        /* questionnaireLaunchContextMap= */ mapOf(practitioner.resourceType.name to practitioner),
       )
 
     val expressionsToEvaluate =
