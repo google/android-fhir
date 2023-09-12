26ac2358441bb5c6a11485ab4c8354d48512213c
Josh Soref <2119212+jsoref@users.noreply.github.com>
2023-09-11 21:51:40 -0400

    spelling: expected
    Signed-off-by: Josh Soref <2119212+jsoref@users.noreply.github.com>

--- workflow-testing/src/main/java/com/google/android/fhir/workflow/testing/CqlBuilder.kt
+++ workflow-testing/src/main/java/com/google/android/fhir/workflow/testing/CqlBuilder.kt
@@ -220,7 +220,7 @@ object CqlBuilder : Loadable() {
     }
 
     fun generatesFhirLibraryEqualsTo(expectedFhirAssetName: String): CompiledCql {
-      // Given the ELM is the same, builds the lib with the expented, not the new ELM to make sure
+      // Given the ELM is the same, builds the lib with the expected, not the new ELM to make sure
       // the base 64 representation of the Library matches.
       val library =
         assembleFhirLib(
