658f9c498ff4ab7d261b54d0efaadc3518b5d3a4
Martin Ndegwa <mndegwa@ona.io>
2023-09-21 09:13:34 +0300

    Engine, SDC   - With unmerged PR https://github.com/opensrp/android-fhir/pull/11   - Wup https://github.com/google/android-fhir/pull/1669   - Wup https://github.com/google/android-fhir/pull/2076   - Wup https://github.com/opensrp/android-fhir/pull/9   - Wup https://github.com/google/android-fhir/pull/2178   - Wup https://github.com/opensrp/android-fhir/pull/10

--- buildSrc/src/main/kotlin/Releases.kt
+++ buildSrc/src/main/kotlin/Releases.kt
@@ -48,13 +48,13 @@ object Releases {
 
   object Engine : LibraryArtifact {
     override val artifactId = "engine"
-    override val version = "0.1.0-beta04-preview3-SNAPSHOT"
+    override val version = "0.1.0-beta04-preview4-SNAPSHOT"
     override val name = "Android FHIR Engine Library"
   }
 
   object DataCapture : LibraryArtifact {
     override val artifactId = "data-capture"
-    override val version = "1.0.0-preview18-SNAPSHOT"
+    override val version = "1.0.0-preview19-SNAPSHOT"
     override val name = "Android FHIR Structured Data Capture Library"
   }
 
