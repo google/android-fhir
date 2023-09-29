58c3006895f83e59887eb01d5a3ba2f631269700
Hugo Miloszewski <hugomilosz@google.com>
2023-09-29 17:05:09 +0000

    Commented SHL Retrofit interface

--- document/src/main/java/com.google.android.fhir.document/interfaces/RetrofitSHLService.kt
+++ document/src/main/java/com.google.android.fhir.document/interfaces/RetrofitSHLService.kt
@@ -12,19 +12,21 @@ import retrofit2.http.Headers
 import retrofit2.http.POST
 import retrofit2.http.Url
 
+/* Interface to make HTTP requests to the SHL server */
 interface RetrofitSHLService {
-  @GET
-  suspend fun get(@Url path: String, @HeaderMap headers: Map<String, String>): JSONObject
 
+  /* Initial POST request to generate a manifest URL */
+  @Headers("Content-Type: application/json")
+  @POST("shl")
+  suspend fun getManifestUrlAndToken(@Body request: RequestBody): Response<ResponseBody>
+
+
+  /* POST request to add data to the SHL */
   @POST("file")
   @Headers("Content-Type: application/json")
   suspend fun postPayload(
     @Body contentEncrypted: String,
     @Header("Authorization") authorization: String,
   ): Response<ResponseBody>
-
-  @Headers("Content-Type: application/json")
-  @POST("shl")
-  suspend fun getManifestUrlAndToken(@Body request: RequestBody): Response<ResponseBody>
 
 }
