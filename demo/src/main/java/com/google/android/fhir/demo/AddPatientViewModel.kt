f8d541b0c332b1c15853088c55fddca1291ded04
anchita-g <109063673+anchita-g@users.noreply.github.com>
2023-10-09 13:26:23 +0530

    removing unused refs

--- demo/src/main/java/com/google/android/fhir/demo/AddPatientViewModel.kt
+++ demo/src/main/java/com/google/android/fhir/demo/AddPatientViewModel.kt
@@ -32,7 +32,6 @@ import kotlinx.coroutines.launch
 import org.hl7.fhir.r4.model.Patient
 import org.hl7.fhir.r4.model.Questionnaire
 import org.hl7.fhir.r4.model.QuestionnaireResponse
-import org.hl7.fhir.r4.model.Reference
 
 /** ViewModel for patient registration screen {@link AddPatientFragment}. */
 class AddPatientViewModel(application: Application, private val state: SavedStateHandle) :
