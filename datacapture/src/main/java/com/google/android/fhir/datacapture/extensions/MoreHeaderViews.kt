9e4cd65a14ac345c1ee74dd7af329bdeec182b54
fikrimilano <fikrimilano1@gmail.com>
2024-01-19 13:40:10 +0700

    Remove .invoke from callback

--- datacapture/src/main/java/com/google/android/fhir/datacapture/extensions/MoreHeaderViews.kt
+++ datacapture/src/main/java/com/google/android/fhir/datacapture/extensions/MoreHeaderViews.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright 2023 Google LLC
+ * Copyright 2023-2024 Google LLC
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -74,11 +74,11 @@ fun initHelpViews(
     helpCardView.visibility =
       when (helpCardView.visibility) {
         VISIBLE -> {
-          helpCardStateChangedCallback.invoke(false, helpCardItem)
+          helpCardStateChangedCallback(false, helpCardItem)
           GONE
         }
         else -> {
-          helpCardStateChangedCallback.invoke(true, helpCardItem)
+          helpCardStateChangedCallback(true, helpCardItem)
           VISIBLE
         }
       }
