f7a23c9a518e0f9626a92496a510a0ca1eb70433
Martin Ndegwa <mndegwa@ona.io>
2024-03-27 14:19:48 +0300

    SDK Release: Engine, SDC, Workflow, Barcode, Location Widget, Common, Knowledger      - With unmerged PR https://github.com/opensrp/android-fhir/pull/9      - WUP PR https://github.com/google/android-fhir/pull/2178/

--- buildSrc/src/main/kotlin/Releases.kt
+++ buildSrc/src/main/kotlin/Releases.kt
@@ -54,7 +54,7 @@ object Releases {
 
   object DataCapture : LibraryArtifact {
     override val artifactId = "data-capture"
-    override val version = "1.1.0-preview6-SNAPSHOT"
+    override val version = "1.1.0-preview7-SNAPSHOT"
     override val name = "Android FHIR Structured Data Capture Library"
   }
 
@@ -81,7 +81,7 @@ object Releases {
 
   object Knowledge : LibraryArtifact {
     override val artifactId = "knowledge"
-    override val version = "0.1.0-alpha03-preview3-SNAPSHOT"
+    override val version = "0.1.0-alpha03-preview3.1-SNAPSHOT"
     override val name = "Android FHIR Knowledge Manager Library"
   }
 
