b887f54aae916aa78bafdebad0e0901c39b9cd46
Hamza Ahmed Khan <hamza.khan@venturedive.com>
2024-05-08 11:47:34 +0500

    Update launchTimestamp Extension URL 2

--- datacapture/src/main/java/com/google/android/fhir/datacapture/QuestionnaireViewModel.kt
+++ datacapture/src/main/java/com/google/android/fhir/datacapture/QuestionnaireViewModel.kt
@@ -162,7 +162,7 @@ internal class QuestionnaireViewModel(application: Application, state: SavedStat
       }
     }
     // Add extension for questionnaire launch time stamp
-    questionnaireResponse.extension.add(Extension("https://github.com/google/android-fhir/StructureDefinition/questionnaireresponse-launchTimestamp", DateTimeType(Date())))
+    questionnaireResponse.extension.add(Extension("http://github.com/google-android/launch-timestamp", DateTimeType(Date())))
     questionnaireResponse.packRepeatedGroups()
   }
 
