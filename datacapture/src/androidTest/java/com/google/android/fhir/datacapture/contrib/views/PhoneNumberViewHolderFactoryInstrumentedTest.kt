84de4bdf378a62ad347444e45c4c1601589eba6c
Santosh Pingle <spingle@google.com>
2024-05-30 16:50:47 +0530

    review comment.

--- datacapture/src/androidTest/java/com/google/android/fhir/datacapture/contrib/views/PhoneNumberViewHolderFactoryInstrumentedTest.kt
+++ datacapture/src/androidTest/java/com/google/android/fhir/datacapture/contrib/views/PhoneNumberViewHolderFactoryInstrumentedTest.kt
@@ -24,7 +24,6 @@ import androidx.test.ext.junit.rules.ActivityScenarioRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.platform.app.InstrumentationRegistry
 import com.google.android.fhir.datacapture.QuestionnaireEditAdapter
-import com.google.android.fhir.datacapture.QuestionnaireViewHolderType
 import com.google.android.fhir.datacapture.R
 import com.google.android.fhir.datacapture.test.TestActivity
 import com.google.android.fhir.datacapture.validation.Invalid
@@ -62,20 +61,6 @@ class PhoneNumberViewHolderFactoryInstrumentedTest {
     viewHolder = PhoneNumberViewHolderFactory.create(parent)
     setTestLayout(viewHolder.itemView)
     questionnaireEditAdapter = QuestionnaireEditAdapter()
-  }
-
-  @Test
-  fun createViewHolder_shouldReturn_phoneNumberViewHolder() {
-    val viewHolderFromAdapter =
-      questionnaireEditAdapter.createViewHolder(
-        parent,
-        QuestionnaireEditAdapter.ViewType.from(
-            type = QuestionnaireEditAdapter.ViewType.Type.QUESTION,
-            subtype = QuestionnaireViewHolderType.PHONE_NUMBER.value,
-          )
-          .viewType,
-      ) as QuestionnaireEditAdapter.ViewHolder.QuestionHolder
-    assertThat(viewHolderFromAdapter.holder).isInstanceOf(viewHolder::class.java)
   }
 
   @Test
