70fd2aec1e6bf04b75cb95fb6ffe31ba848c6500
aditya-07 <adityakhajuria@google.com>
2024-06-14 01:35:09 +0530

    Added rules for required for engine and datacapture libraries. (#1916)
    * Added rules for required for engine and datacatpure
    
    * Added consumerProguardFile for engine and catalog library
    
    * Review comments: Removed unnecessary rules
    
    * Review comments: Added comments
    
    * Enabled minfying for catalog and demo release apps. Also removed unnecessary keep rule from catalog's rule file
    
    * updated rules
    
    * Added comments about new rules

--- datacapture/build.gradle.kts
+++ datacapture/build.gradle.kts
@@ -21,6 +21,7 @@ android {
     testInstrumentationRunner = Dependencies.androidJunitRunner
     // Need to specify this to prevent junit runner from going deep into our dependencies
     testInstrumentationRunnerArguments["package"] = "com.google.android.fhir.datacapture"
+    consumerProguardFile("proguard-rules.pro")
   }
 
   buildFeatures { viewBinding = true }
