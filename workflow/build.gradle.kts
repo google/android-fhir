89e208959eb924c63351ae419fcfaa9f72384609
Jing Tang <jingtang@google.com>
2024-07-25 17:53:06 +0100

    Run spotless

--- workflow/build.gradle.kts
+++ workflow/build.gradle.kts
@@ -103,9 +103,7 @@ dependencies {
   implementation(libs.kotlin.stdlib)
   implementation(libs.kotlinx.coroutines.android)
   implementation(libs.kotlinx.coroutines.core)
-  implementation(libs.opencds.cqf.fhir.cr) {
-    exclude("ch.qos.logback", "logback-classic")
-  }
+  implementation(libs.opencds.cqf.fhir.cr) { exclude("ch.qos.logback", "logback-classic") }
   implementation(libs.opencds.cqf.fhir.jackson)
 
   testImplementation(Dependencies.jsonAssert)
