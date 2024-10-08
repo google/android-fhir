e5b65fc35d2da87d222f55c2abf46cc39a8af6b7
Santosh Pingle <spingle@google.com>
2024-10-08 18:26:58 +0530

    update last sync label.

--- demo/src/main/java/com/google/android/fhir/demo/ManualSyncFragment.kt
+++ demo/src/main/java/com/google/android/fhir/demo/ManualSyncFragment.kt
@@ -97,7 +97,7 @@ class ManualSyncFragment : Fragment() {
 
   private fun observeLastSyncTime() {
     mainActivityViewModel.lastSyncTimestampLiveData.observe(viewLifecycleOwner) {
-      requireView().findViewById<TextView>(R.id.lastSyncTime).text = it
+      requireView().findViewById<TextView>(R.id.lastSyncTime).text = getString(R.string.last_sync_timestamp,it)
     }
   }
 }
