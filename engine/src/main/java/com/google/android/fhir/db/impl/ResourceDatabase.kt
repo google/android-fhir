6ad4424bfd3201a789bd7c9b1e849cdaa774d205
L≡ZRS <12814349+LZRS@users.noreply.github.com>
2024-11-20 17:40:20 +0300

    Update columns' order in TokenIndexEntity multi-column index

--- engine/src/main/java/com/google/android/fhir/db/impl/ResourceDatabase.kt
+++ engine/src/main/java/com/google/android/fhir/db/impl/ResourceDatabase.kt
@@ -54,7 +54,7 @@ import org.json.JSONObject
       PositionIndexEntity::class,
       LocalChangeResourceReferenceEntity::class,
     ],
-  version = 8,
+  version = 9,
   exportSchema = true,
 )
 @TypeConverters(DbTypeConverters::class)
@@ -205,6 +205,24 @@ internal val MIGRATION_7_8 =
           }
           continueIterating = it.moveToNext()
         }
+      }
+    }
+  }
+
+internal val Migration_8_9 =
+  object : Migration(8, 9) {
+    override fun migrate(database: SupportSQLiteDatabase) {
+      database.beginTransaction()
+      try {
+        database.execSQL(
+          "DROP INDEX IF EXISTS `index_TokenIndexEntity_resourceType_index_name_index_system_index_value_resourceUuid`;",
+        )
+        database.execSQL(
+          "CREATE INDEX IF NOT EXISTS `index_TokenIndexEntity_index_value_resourceType_index_name_resourceUuid` ON `TokenIndexEntity` (`index_value`, `resourceType`, `index_name`, `resourceUuid`);",
+        )
+        database.setTransactionSuccessful()
+      } finally {
+        database.endTransaction()
       }
     }
   }
