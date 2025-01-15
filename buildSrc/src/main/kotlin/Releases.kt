175212191937aea24d9b135a1f3f2023be621cab
Martin Ndegwa <mndegwa@ona.io>
2025-01-15 17:49:45 +0300

    SDK Release - SDC, Engine
    FORK
        - With unmerged PR https://github.com/opensrp/android-fhir/pull/9
        - WUP  https://github.com/opensrp/android-fhir/pull/13
    
    SDK
        - WUP https://github.com/google/android-fhir/pull/2178
        - WUP https://github.com/google/android-fhir/pull/2650
        - WUP https://github.com/google/android-fhir/pull/2645
        - WUP https://github.com/google/android-fhir/pull/2678
        - WUP https://github.com/google/android-fhir/pull/2755

--- buildSrc/src/main/kotlin/Releases.kt
+++ buildSrc/src/main/kotlin/Releases.kt
@@ -50,19 +50,19 @@ object Releases {
 
   object Engine : LibraryArtifact {
     override val artifactId = "engine"
-    override val version = "1.1.0-preview2-SNAPSHOT"
+    override val version = "1.1.0-preview3-SNAPSHOT"
     override val name = "Android FHIR Engine Library"
   }
 
   object DataCapture : LibraryArtifact {
     override val artifactId = "data-capture"
-    override val version = "1.2.0-preview7-SNAPSHOT"
+    override val version = "1.2.0-preview8-SNAPSHOT"
     override val name = "Android FHIR Structured Data Capture Library"
   }
 
   object Workflow : LibraryArtifact {
     override val artifactId = "workflow"
-    override val version = "0.1.0-beta01-preview-SNAPSHOT"
+    override val version = "0.1.0-beta01-preview1-SNAPSHOT"
     override val name = "Android FHIR Workflow Library"
   }
 
@@ -83,7 +83,7 @@ object Releases {
 
   object Knowledge : LibraryArtifact {
     override val artifactId = "knowledge"
-    override val version = "0.1.0-beta01-preview-SNAPSHOT"
+    override val version = "0.1.0-beta01-preview1-SNAPSHOT"
     override val name = "Android FHIR Knowledge Manager Library"
   }
 
