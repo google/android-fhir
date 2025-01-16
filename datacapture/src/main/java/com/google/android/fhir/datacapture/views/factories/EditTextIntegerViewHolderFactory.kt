f0fde4b8499c867c1a0e1ff1f69ef7678342cfcc
chungwwei <chungwwei223@gmail.com>
2025-01-16 08:26:08 -0500

    Throw IllegalArgumentException when minValue is larger than maxValue

--- datacapture/src/main/java/com/google/android/fhir/datacapture/views/factories/EditTextIntegerViewHolderFactory.kt
+++ datacapture/src/main/java/com/google/android/fhir/datacapture/views/factories/EditTextIntegerViewHolderFactory.kt
@@ -39,6 +39,17 @@ internal object EditTextIntegerViewHolderFactory :
         InputType.TYPE_CLASS_NUMBER or InputType.TYPE_NUMBER_FLAG_SIGNED,
       ) {
 
+      override fun bind(questionnaireViewItem: QuestionnaireViewItem) {
+        super.bind(questionnaireViewItem)
+
+        val minValue = (questionnaireViewItem.minAnswerValue as? IntegerType)?.value
+        val maxValue = (questionnaireViewItem.maxAnswerValue as? IntegerType)?.value
+
+        if (minValue != null && maxValue != null && minValue > maxValue) {
+          throw IllegalArgumentException("minValue cannot be greater than maxValue")
+        }
+      }
+
       override suspend fun handleInput(
         editable: Editable,
         questionnaireViewItem: QuestionnaireViewItem,
