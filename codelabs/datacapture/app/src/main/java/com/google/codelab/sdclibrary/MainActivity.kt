4b5af1d4519bd02a70d20d9cb51004bcd61fe789
Thomas Niccolo Reyes <niccoreyes@gmail.com>
2025-02-05 10:38:10 +0800

    Add lifecycle coroutine support to MainActivity

--- codelabs/datacapture/app/src/main/java/com/google/codelab/sdclibrary/MainActivity.kt
+++ codelabs/datacapture/app/src/main/java/com/google/codelab/sdclibrary/MainActivity.kt
@@ -18,6 +18,8 @@ package com.google.codelab.sdclibrary
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.lifecycleScope
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
 
