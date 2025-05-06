3eb08705ee6761c0dd8efadb66b2bae1605c5e06
L≡ZRS <12814349+LZRS@users.noreply.github.com>
2025-05-06 15:16:56 +0300

    Replace depracted toUpperCase() with uppercase()

--- engine/src/test/java/com/google/android/fhir/index/SearchParameterRepositoryGeneratedTest.kt
+++ engine/src/test/java/com/google/android/fhir/index/SearchParameterRepositoryGeneratedTest.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright 2023 Google LLC
+ * Copyright 2023-2025 Google LLC
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -43,7 +43,7 @@ class SearchParameterRepositoryGeneratedTest(private val resource: Resource) {
         .map {
           SearchParamDefinition(
             it.name,
-            Enumerations.SearchParamType.valueOf(it.type.toUpperCase()),
+            Enumerations.SearchParamType.valueOf(it.type.uppercase()),
             it.path,
           )
         }
