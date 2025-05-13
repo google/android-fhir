37b72122a47f46ce505e5b2db9176717c62fc4ac
L≡ZRS <12814349+LZRS@users.noreply.github.com>
2025-05-14 02:25:03 +0300

    Update FhirEngineCrudBenchmark tracing to use testTags

--- engine/benchmarks/macrobenchmark/src/main/java/com/google/android/fhir/engine/macrobenchmark/Utils.kt
+++ engine/benchmarks/macrobenchmark/src/main/java/com/google/android/fhir/engine/macrobenchmark/Utils.kt
@@ -16,5 +16,21 @@
 
 package com.google.android.fhir.engine.macrobenchmark
 
+import androidx.benchmark.macro.MacrobenchmarkScope
+import androidx.test.uiautomator.By
+import androidx.test.uiautomator.Until
+import org.junit.Assert.fail
+
 const val TARGET_PACKAGE = "com.google.android.fhir.engine.benchmarks.app"
 const val DEFAULT_ITERATIONS = 10
+
+fun MacrobenchmarkScope.clickOnTestTag(testTag: String) {
+  val selector = By.res(testTag)
+  if (!device.wait(Until.hasObject(selector), 2_500)) {
+    fail("Did not find object with testTag $testTag")
+  }
+
+  device.findObject(selector).click()
+  // Chill to ensure we capture the end of the click span in the trace.
+  Thread.sleep(100)
+}
