c57e3cd051021b01cdabb405d0b400a957774e7b
L≡ZRS <12814349+LZRS@users.noreply.github.com>
2025-05-17 16:54:13 +0300

    Register generateSynthea task to run Synthea through gradle

--- generate_synthea.sh
+++ generate_synthea.sh
@@ -1,22 +1,25 @@
 #!/usr/bin/env bash
 
 # Parameters
-POPULATION="${1:-3}"
+POPULATION="${1}"
+OUTPUT_DIR="${2}"
 SCRIPT_DIR=$(dirname "$0")
 SYNTHEA_DIR="$SCRIPT_DIR"/synthea
-GIT_ROOT_DIR=$(git rev-parse --show-toplevel)
 
 if [ ! -d "$SYNTHEA_DIR" ]; then
   # build Synthea according to https://github.com/synthetichealth/synthea
   git clone https://github.com/synthetichealth/synthea.git "$SYNTHEA_DIR"
 fi
+
+rm -rf "$SYNTHEA_DIR"/output
+
 pushd "$SYNTHEA_DIR" || exit
 # generate valid R4 resources in output/fhir
 ./run_synthea -m pregnancy -p "$POPULATION" --exporter.fhir.included_resources Patient --exporter.fhir.transaction_bundle false --exporter.years_of_history 1 --exporter.fhir.use_us_core_ig false --exporter.fhir.bulk_data true
 popd || exit
 
-# Move to benchmark assets dir
-mkdir -p "$GIT_ROOT_DIR"/engine/benchmarks/app/src/main/assets/bulk_data
-cp "$SYNTHEA_DIR"/output/fhir/Patient*.ndjson "$GIT_ROOT_DIR"/engine/benchmarks/app/src/main/assets/bulk_data
-
-rm -rf "$SYNTHEA_DIR"/output
+# Move to output dir
+if [ -n "$OUTPUT_DIR" ]; then
+  mkdir -p "$OUTPUT_DIR"
+  cp "$SYNTHEA_DIR"/output/fhir/Patient*.ndjson "$OUTPUT_DIR"
+fi
