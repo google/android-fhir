6e18563dd80d474db8e5f3f00996ba19112fc2d8
L≡ZRS <12814349+LZRS@users.noreply.github.com>
2025-05-20 16:30:42 +0300

    Update reading of resources data from assets

--- engine/benchmarks/app/src/main/java/com/google/android/fhir/engine/benchmarks/app/data/ResourcesDataProvider.kt
+++ engine/benchmarks/app/src/main/java/com/google/android/fhir/engine/benchmarks/app/data/ResourcesDataProvider.kt
@@ -18,52 +18,48 @@ package com.google.android.fhir.engine.benchmarks.app.data
 
 import android.content.res.AssetManager
 import ca.uhn.fhir.parser.IParser
-import java.io.BufferedReader
 import kotlinx.coroutines.currentCoroutineContext
 import kotlinx.coroutines.withContext
+import org.hl7.fhir.r4.model.Patient
 import org.hl7.fhir.r4.model.Resource
+import org.hl7.fhir.r4.model.ResourceType
+
+private typealias AssetPath = String
+
+private typealias AssetJsonString = String
 
 class ResourcesDataProvider(
   private val assetManager: AssetManager,
   private val fhirR4JsonParser: IParser,
 ) {
-  suspend fun provideResources(onResourcesProvided: suspend (List<Resource>) -> Unit) {
-    try {
-      readFileResources()
-        .chunked(READ_CHUNK_SIZE)
-        .map { it.map { resourceStr -> fhirR4JsonParser.parseResource(resourceStr) as Resource } }
-        .forEach { onResourcesProvided(it) }
-    } finally {
-      closeReaders()
-    }
+  suspend fun collectResources(block: suspend (List<Resource>) -> Unit) {
+    assetFilePaths()
+      .map { assetManager.open(it).bufferedReader() }
+      .forEach { it.useLines { line -> line.consume(block) } }
   }
 
-  private suspend fun readFileResources(): Sequence<String> =
+  suspend fun collectPatientResources(block: (List<Resource>) -> Unit) {
+    assetFilePaths({ it.contains(ResourceType.Patient.name, ignoreCase = true) })
+      .map { assetManager.open(it).bufferedReader() }
+      .forEach { it.useLines { line -> line.consume(block) } }
+  }
+
+  private suspend fun Sequence<AssetJsonString>.consume(block: suspend (List<Resource>) -> Unit) =
     withContext(currentCoroutineContext()) {
-      val ndjsonFiles =
-        assetManager.list(BULK_DATA_DIR)?.filter { it.endsWith(".ndjson") } ?: emptyList()
-      return@withContext ndjsonFiles
-        .asSequence()
-        .map { assetManager.open("$BULK_DATA_DIR/$it") }
-        .flatMap {
-          val reader = it.bufferedReader()
-          openReaders += reader
-          reader.lineSequence()
-        }
+      chunked(READ_CHUNK_SIZE)
+        .map { chunk -> chunk.map { res -> fhirR4JsonParser.parseResource(res) as Resource } }
+        .forEach { block(it) }
     }
 
-  private suspend fun closeReaders() =
+  private suspend fun assetFilePaths(filter: (String) -> Boolean = { true }): Sequence<AssetPath> =
     withContext(currentCoroutineContext()) {
-      openReaders.forEach {
-        try {
-          it.close()
-        } finally {
-          openReaders -= it
-        }
-      }
+      return@withContext assetManager
+        .list(BULK_DATA_DIR)
+        ?.filter { it.endsWith(".ndjson") && filter(it) }
+        ?.map { "$BULK_DATA_DIR/$it" }
+        ?.asSequence()
+        ?: emptySequence()
     }
-
-  private val openReaders = mutableListOf<BufferedReader>()
 
   companion object {
     private const val READ_CHUNK_SIZE = 250
