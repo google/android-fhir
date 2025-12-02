07a21cd8669ca135acd8b7052686e8f34651e6d1
L≡ZRS <12814349+LZRS@users.noreply.github.com>
2025-12-03 00:41:58 +0300

    Fix QuestionnaireViewItem answers change not updating state
    By implement custom equals and hashcode, that on top of structural equality
    also checks that two questionnaireViewItems have the same response

--- datacapture/src/androidTest/java/com/google/android/fhir/datacapture/test/views/EditTextSingleLineViewHolderFactoryInstrumentedTest.kt
+++ datacapture/src/androidTest/java/com/google/android/fhir/datacapture/test/views/EditTextSingleLineViewHolderFactoryInstrumentedTest.kt
@@ -378,4 +378,41 @@ class EditTextSingleLineViewHolderFactoryInstrumentedTest {
 
     composeTestRule.onNodeWithText("Optional").assertDoesNotExist()
   }
+
+  @Test
+  fun displaysCorrectTextOnQuestionnaireViewItemAnswerUpdate() {
+    val questionnaireViewItem =
+      QuestionnaireViewItem(
+        Questionnaire.QuestionnaireItemComponent().apply { text = "First Name" },
+        QuestionnaireResponse.QuestionnaireResponseItemComponent().apply {
+          answer =
+            listOf(
+              QuestionnaireResponse.QuestionnaireResponseItemAnswerComponent().apply {
+                value = StringType("Jane")
+              },
+            )
+        },
+        validationResult = NotValidated,
+        answersChangedCallback = { _, _, _, _ -> },
+      )
+
+    viewHolder.bind(questionnaireViewItem)
+    composeTestRule.onNodeWithTag(EDIT_TEXT_FIELD_TEST_TAG).assertTextEquals("Jane")
+
+    val questionnaireViewItemUpdatedAnswer =
+      questionnaireViewItem.copy(
+        questionnaireResponseItem =
+          questionnaireViewItem.getQuestionnaireResponseItem().apply {
+            answer =
+              listOf(
+                QuestionnaireResponse.QuestionnaireResponseItemAnswerComponent().apply {
+                  value = StringType("Janette")
+                },
+              )
+          },
+      )
+    viewHolder.bind(questionnaireViewItemUpdatedAnswer)
+
+    composeTestRule.onNodeWithTag(EDIT_TEXT_FIELD_TEST_TAG).assertTextEquals("Janette")
+  }
 }
